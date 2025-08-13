8442098fed9ee7248dc25bb63fca06a6b12c678f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:05:18 +0000

    Add package netbox-branch-review/0.1.7/netbox_branch_review-0.1.7-py3-none-any.whl

---
+++ packages/netbox-branch-review/netbox_branch_review-0.1.7-py3-none-any.whl/netbox_branch_review/migrations/0003_merge_0002_add_description_0002_repair_ticket_column.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("netbox_branch_review", "0002_add_description"),
+        ("netbox_branch_review", "0002_repair_ticket_column"),
+    ]
+
+    operations = []
