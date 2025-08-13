bf07aee619aa23e55faf5d08771ec669ccf16c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 16:05:19 +0000

    Add package imgs/0.1.3/imgs-0.1.3-py3-none-any.whl

---
+++ packages/imgs/imgs-0.1.3-py3-none-any.whl/imgs/__main__.py
@@ -0,0 +1,10 @@
+"""
+Main entry point for the imgs package.
+
+This allows the package to be run as a module: python -m imgs
+"""
+
+from .imgs import main
+
+if __name__ == "__main__":
+    main()
