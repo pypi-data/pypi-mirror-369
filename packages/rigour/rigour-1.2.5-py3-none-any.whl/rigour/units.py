382df0be5c279e4a42bede16593f57a30dee3eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 18:05:31 +0000

    Add package rigour/1.2.5/rigour-1.2.5-py3-none-any.whl

---
+++ packages/rigour/rigour-1.2.5-py3-none-any.whl/rigour/units.py
@@ -0,0 +1,98 @@
+UNITS = {
+    # Fractions
+    "percent": "%",
+    "pct": "%",
+    "pct.": "%",
+    "percentage": "%",
+    "per cent": "%",
+    "per-cent": "%",
+    # Length
+    "centimeters": "cm",  # en-US
+    "centimetres": "cm",
+    "centimeter": "cm",  # en-US
+    "centimetre": "cm",
+    "meters": "m",  # en-US
+    "metres": "m",
+    "meter": "m",  # en-US
+    "metre": "m",
+    "kilometers": "km",  # en-US
+    "kilometres": "km",
+    "kilometer": "km",  # en-US
+    "kilometre": "km",
+    "inches": "in",
+    "inch": "in",
+    "″": "in",  # double prime symbol (unicode: U+2033) — the correct unicode symbol for inches (in practice people may use double quotes etc)
+    "feet": "ft",
+    "foot": "ft",
+    "′": "ft",  # prime symbol (unicode: U+2032) — the correct unicode symbol for feet (in practice people may use single quote etc)
+    # Weight/mass
+    "grams": "g",
+    "grammes": "g",  # en-GB
+    "lbs.": "lbs",
+    "pound": "lbs",
+    "pounds": "lbs",
+    "kg": "kg",
+    "kilograms": "kg",
+    "kilogrammes": "kg",  # en GB
+    "kilos": "kg",
+    "ton": "t",
+    "tons": "t",
+    "tonne": "t",
+    "tonnes": "t",
+    "metric ton": "t",
+    "metric tonne": "t",
+    "metric tons": "t",
+    "metric tonnes": "t",
+    # Area
+    "m2": "m²",
+    "square meters": "m²",  # en-US
+    "square metres": "m²",
+    "sqm": "m²",
+    "hectares": "ha",
+    "hectare": "ha",
+    # Volume
+    "cubic feet": "cf",
+    "cubic ft": "cf",
+    # Time/frequency
+    "seconds": "s",
+    "secs": "s",
+    "millisecond": "ms",
+    "milliseconds": "ms",
+    "hertz": "Hz",
+    # Currency
+    # (i) Currency — whole units
+    "eur": "€",
+    "euros": "€",
+    "euro": "€",
+    "dollars": "$",
+    "dollar": "$",
+    "British pounds": "£",  # Note: 'pounds' can also be a unit of weight
+    "British pound": "£",
+    "pounds sterling": "£",
+    "pound sterling": "£",
+    "rub": "₽",
+    "rubles": "₽",
+    "ruble": "₽",
+    "russian rubles": "₽",
+    "russian ruble": "₽",
+    # (ii) Currency — fractions of units
+    "cents": "¢",
+    "cent": "¢",
+    "centimes": "¢",
+    "centime": "¢",
+    "pence": "p",
+    "penny": "p",
+}
+
+
+def normalize_unit(unit: str) -> str:
+    """
+    Normalize a unit string to its standard abbreviation.
+
+    Args:
+        unit (str): The unit string to normalize.
+
+    Returns:
+        str: The normalized unit abbreviation.
+    """
+    return UNITS.get(unit.lower(), unit)
