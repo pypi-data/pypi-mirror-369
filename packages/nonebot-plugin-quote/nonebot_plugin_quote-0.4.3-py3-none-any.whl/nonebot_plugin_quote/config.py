87e99924fc079446788758b46207f7108f3e5f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 15:04:05 +0000

    Add package nonebot-plugin-quote/0.4.3/nonebot_plugin_quote-0.4.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-quote/nonebot_plugin_quote-0.4.3-py3-none-any.whl/nonebot_plugin_quote/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Extra
+from typing import List, Dict
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    record_path: str = 'record.json'
+    inverted_index_path: str = 'inverted_index.json'
+    quote_superuser: Dict[str, List[str]] = {}
+    global_superuser: List[str] = []
+    superusers: set[str]
+    quote_needat: bool = True
+    quote_startcmd: str = ''
+    quote_path: str = 'quote'
+    font_path: str = 'font1'
+    author_font_path: str = 'font2'
+
+def check_font(font_path, author_font_path):
+    # 判断字体是否配置
+    return not (font_path == 'font1' or author_font_path == 'font2')
