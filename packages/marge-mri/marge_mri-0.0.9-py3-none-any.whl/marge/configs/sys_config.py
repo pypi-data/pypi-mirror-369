d93e803626b6c21e665853c1f721045eab3606cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 16:06:42 +0000

    Add package marge-mri/0.0.9/marge_mri-0.0.9-py3-none-any.whl

---
+++ packages/marge-mri/marge_mri-0.0.9-py3-none-any.whl/marge/configs/sys_config.py
@@ -0,0 +1,5 @@
+side = ["None", "Left", "Right"]
+orientation = ["Feet First Supine (FFS)", "Head First Supine (HFS)", "Feet First Prono (FFP)", "Head First Prono(HFP)"]
+screenshot_folder = "experiments/screenshots"
+projects = ["None"]
+study_case = ["None"]
