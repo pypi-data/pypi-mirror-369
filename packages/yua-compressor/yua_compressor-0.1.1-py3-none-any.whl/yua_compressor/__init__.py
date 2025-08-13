1e3010d634110065c0bd1550bc6456709bb887f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 14:05:46 +0000

    Add package yua-compressor/0.1.1/yua_compressor-0.1.1-py3-none-any.whl

---
+++ packages/yua-compressor/yua_compressor-0.1.1-py3-none-any.whl/yua_compressor/__init__.py
@@ -0,0 +1,8 @@
+from .core import compress_bytes, decompress_bytes, compress_file, decompress_file
+
+__all__ = [
+    "compress_bytes",
+    "decompress_bytes",
+    "compress_file",
+    "decompress_file",
+]
