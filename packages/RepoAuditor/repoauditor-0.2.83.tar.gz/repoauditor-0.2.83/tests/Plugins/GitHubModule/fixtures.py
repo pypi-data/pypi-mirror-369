44ca9d23b0ac84f0a3fc1b914a0ba04d45c32efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:28:01 +0000

    Add package RepoAuditor/0.2.83/repoauditor-0.2.83.tar.gz

---
+++ packages/RepoAuditor/repoauditor-0.2.83.tar.gz/repoauditor-0.2.83/tests/Plugins/GitHubModule/fixtures.py
@@ -0,0 +1,28 @@
+# -------------------------------------------------------------------------------
+# |
+# |  Copyright (c) 2024 Scientific Software Engineering Center at Georgia Tech
+# |  Distributed under the MIT License.
+# |
+# -------------------------------------------------------------------------------
+"""Common fixtures for GitHubModule unit tests."""
+
+import pytest
+from dataclasses import dataclass
+
+
+@pytest.fixture
+def session():
+    """Create a dummy GitHub API session object."""
+
+    @dataclass
+    class _GithubSession_:
+        github_url: str
+        pat: str
+        is_enterprise: bool
+
+    s = _GithubSession_(
+        "https://github.com/gt-sse-center/RepoAuditor",
+        pat="github_pat_dummy",
+        is_enterprise=False,
+    )
+    return s
