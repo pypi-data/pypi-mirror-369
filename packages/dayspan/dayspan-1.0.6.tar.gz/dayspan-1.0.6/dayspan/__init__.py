427c646ce730d9785840da2303b609cf8e294453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 14:04:38 +0000

    Add package dayspan/1.0.6/dayspan-1.0.6.tar.gz

---
+++ packages/dayspan/dayspan-1.0.6.tar.gz/dayspan-1.0.6/dayspan/__init__.py
@@ -0,0 +1,7 @@
+"""DaySpan - A simple tool to count days between two dates."""
+
+from .calculator import calculate_days, parse_date
+
+__version__ = '1.0.6'
+__author__ = 'Cong Gao'
+__all__ = ['calculate_days', 'parse_date']
