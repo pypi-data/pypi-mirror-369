1140d492e6f4c8c5e5d6ecac2f4dc62edc23b7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 16:04:57 +0000

    Add package randomity/0.3.0/randomity-0.3.0.tar.gz

---
+++ packages/randomity/randomity-0.3.0.tar.gz/randomity-0.3.0/package.sh
@@ -0,0 +1,23 @@
+set -e
+
+if [ -z "$1" ]; then
+    echo "Error: Please provide a new version number (./package.sh x.y.z)"
+    exit 1
+fi
+
+NEW_VERSION=$1
+PYPROJECT_FILE="pyproject.toml"
+
+echo "Updating version in $PYPROJECT_FILE to $NEW_VERSION..."
+sed -i '' -e "s/^version = \"[0-9.]*\"/version = \"$NEW_VERSION\"/" "$PYPROJECT_FILE"
+
+echo "Cleaning previous build..."
+rm -rf dist/*
+
+echo "Building the package..."
+/opt/homebrew/opt/python@3.11/bin/python3.11 -m build
+
+echo "Publishing the package to PyPI..."
+twine upload dist/*
+
+echo "Successfully published version $NEW_VERSION!"
