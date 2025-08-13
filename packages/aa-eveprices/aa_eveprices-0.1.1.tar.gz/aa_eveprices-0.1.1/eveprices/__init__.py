60fe51c6f39b4f2880a814309bd10761bd30ec79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 18:05:40 +0000

    Add package aa-eveprices/0.1.1/aa_eveprices-0.1.1.tar.gz

---
+++ packages/aa-eveprices/aa_eveprices-0.1.1.tar.gz/aa_eveprices-0.1.1/eveprices/__init__.py
@@ -0,0 +1,8 @@
+"""Alliance Auth application providing a common backend for managing type prices"""
+
+# pylint: disable = invalid-name
+default_app_config = "example.apps.ExampleConfig"
+
+app_name = "AA-Eveprices"
+__version__ = "0.1.1"
+__url__ = "http://gitlab.com/r0kym/aa-eveprices"
