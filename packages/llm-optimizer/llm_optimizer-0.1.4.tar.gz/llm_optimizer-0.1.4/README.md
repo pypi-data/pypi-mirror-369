bdf7375c81930a31074643241304aa58a54aa9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 18:06:14 +0000

    Add package llm-optimizer/0.1.4/llm_optimizer-0.1.4.tar.gz

---
+++ packages/llm-optimizer/llm_optimizer-0.1.4.tar.gz/llm_optimizer-0.1.4/README.md
@@ -0,0 +1,11 @@
+# LLM Optimizer
+
+*Optimize and track emissions while running Large Language Models*
+
+## 🚀 Features
+
+- Load and run text generation with Hugging Face Transformers.
+- Track energy consumption and carbon emissions using CodeCarbon.
+
+
+## 📦 Installation
---
+++ packages/llm-optimizer/llm_optimizer-0.1.4.tar.gz/llm_optimizer-0.1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llm_optimizer',
+    version='0.1.4',
+    description='Optimize LLM outputs and track emissions',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Vijayalakshmi',
+    author_email='viji814881@email.com',
+    url='https://github.com/viji123450/llm_optimizer',  # <-- OPTIONAL: link to your repo
+    license='MIT',  # <-- or any license you prefer
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'transformers',
+        'codecarbon',
+        'evaluate',
+        'numpy',
+        'rouge_score',
+        'nltk',
+        'absl-py'
+    ],
+    entry_points={
+        'console_scripts': [
+            'llm-optimize=llm_optimizer.main:load_and_generate',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
