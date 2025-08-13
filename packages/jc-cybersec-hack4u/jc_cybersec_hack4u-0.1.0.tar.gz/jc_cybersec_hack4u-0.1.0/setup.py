2115d4f9fa475d9b7c6f562a077018f94b94e58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 14:05:57 +0000

    Add package jc-cybersec-hack4u/0.1.0/jc_cybersec_hack4u-0.1.0.tar.gz

---
+++ packages/jc-cybersec-hack4u/jc_cybersec_hack4u-0.1.0.tar.gz/jc_cybersec_hack4u-0.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+#Leer el contenido del archivo README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "jc_cybersec_hack4u",
+    version = "0.1.0",
+    packages = find_packages(),
+    install_requires = [],
+    author = "Javier Criado",
+    description = "Una biblioteca para consultar cursos de hack4u.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://hack4u.io",
+)
+
