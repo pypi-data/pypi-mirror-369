97d9b1886b0f8d44ad3a03a10fe6c17fa52cc8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:10:19 +0000

    Add package greetme-rj-june10/0.1/greetme_rj_june10-0.1.tar.gz

---
+++ packages/greetme-rj-june10/greetme_rj_june10-0.1.tar.gz/greetme_rj_june10-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='greetme_rj_june10',  # <-- new unique name
+    version='0.1',
+    packages=find_packages(),
+    description='A simple greeting package',
+    author='Rajeendran U',
+    author_email='your.email@example.com',
+    install_requires=[],
+)
