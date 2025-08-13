30e04bde657ac8dfa8dd2a4984c38cddd8549c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 14:04:35 +0000

    Add package pvevti/2025.8.12.0/pvevti-2025.8.12.0-py3-none-any.whl

---
+++ packages/pvevti/pvevti-2025.8.12.0-py3-none-any.whl/pvevti/__init__.py
@@ -0,0 +1,20 @@
+# Define the __all__ variable
+__all__ = ["csvutil", "genutil", "jsonutil", "pdfutil", "mf4util"]
+__version__ = "2025.8.12.0"
+
+#s = time.time()
+#print('[pvevti] Import csvutil module')
+#from . import csvutil
+#print('       Done ({:.0f}ms)'.format(1000*(time.time()-s)))
+#s = time.time()
+#print('[pvevti] Import genutil module')
+#from . import genutil
+#print("       Done ({:.0f}ms)".format(1000*(time.time()-s)))
+#s = time.time()
+#print('[pvevti] Import jsonutil module')
+#from . import jsonutil
+#print("       Done ({:.0f}ms)".format(1000*(time.time()-s)))
+#s = time.time()
+#print('[pvevti] Import pdfutil module')
+#from . import pdfutil
+#print("       Done ({:.0f}ms)".format(1000*(time.time()-s)))
