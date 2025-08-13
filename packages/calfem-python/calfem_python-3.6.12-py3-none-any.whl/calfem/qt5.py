74a985da4a2eb14626387fda806df5ff9f4067ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:05:15 +0000

    Add package calfem-python/3.6.12/calfem_python-3.6.12-py3-none-any.whl

---
+++ packages/calfem-python/calfem_python-3.6.12-py3-none-any.whl/calfem/qt5.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 23:17:26 2017
+
+@author: jonas_000
+"""
+
+from qtpy.QtCore import Slot, Signal, QMetaObject
+from qtpy.QtWidgets import *
+from qtpy.QtGui import QPixmap
+from qtpy.uic import loadUi
