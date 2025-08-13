04c0d4d0f1fcd39b6dd7351e446fc884ade4056a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 15:04:18 +0000

    Add package gllm-inference-binary/0.4.55/gllm_inference_binary-0.4.55-cp313-cp313-win_amd64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.4.55-cp313-cp313-win_amd64.whl/gllm_inference/lm_invoker/schema/openai_compatible.pyi
@@ -0,0 +1,35 @@
+class Key:
+    """Defines valid keys in OpenAI."""
+    ARGUMENTS: str
+    CONTENT: str
+    DATA: str
+    DEFS: str
+    FINISH_REASON: str
+    FORMAT: str
+    FUNCTION: str
+    ID: str
+    IMAGE_URL: str
+    INPUT_AUDIO: str
+    JSON_SCHEMA: str
+    NAME: str
+    RESPONSE_FORMAT: str
+    ROLE: str
+    SCHEMA: str
+    STRICT: str
+    TEXT: str
+    TITLE: str
+    TOOLS: str
+    TOOL_CALLS: str
+    TOOL_CALL_ID: str
+    TYPE: str
+    URL: str
+    USAGE: str
+
+class InputType:
+    """Defines valid input types in OpenAI."""
+    FUNCTION: str
+    IMAGE_URL: str
+    INPUT_AUDIO: str
+    JSON_SCHEMA: str
+    TEXT: str
+    TOOL: str
