a30dfebcad489e26b20835d98866b7f7a8926607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 15:04:24 +0000

    Add package leaguemanager/0.11.3/leaguemanager-0.11.3-py3-none-any.whl

---
+++ packages/leaguemanager/leaguemanager-0.11.3-py3-none-any.whl/leaguemanager/services/account/__init__.py
@@ -0,0 +1,11 @@
+from .role import RoleAsyncService, RoleSyncService
+from .user import UserAsyncService, UserRoleAsyncService, UserRoleSyncService, UserSyncService
+
+__all__ = [
+    "RoleAsyncService",
+    "RoleSyncService",
+    "UserAsyncService",
+    "UserRoleAsyncService",
+    "UserRoleSyncService",
+    "UserSyncService",
+]
