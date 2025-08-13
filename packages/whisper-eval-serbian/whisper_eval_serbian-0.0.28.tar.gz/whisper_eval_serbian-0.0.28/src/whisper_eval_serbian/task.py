36faae2a0246d91b56d99bbd0ae24f9c10a2a68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:05:56 +0000

    Add package whisper-eval-serbian/0.0.28/whisper_eval_serbian-0.0.28.tar.gz

---
+++ packages/whisper-eval-serbian/whisper_eval_serbian-0.0.28.tar.gz/whisper_eval_serbian-0.0.28/src/whisper_eval_serbian/task.py
@@ -0,0 +1,51 @@
+# whisper_evaluator/task.py
+from datasets import load_dataset, Audio
+from .utils import to_latin_serbian
+
+class ASRTask:
+    """
+    Handles loading and preparing a dataset for Automatic Speech Recognition evaluation.
+    """
+    def __init__(
+        self,
+        dataset_name: str,
+        dataset_subset: str,
+        dataset_split: str,
+        audio_column: str,
+        text_column: str,
+        hf_token: str | None = None
+    ):
+        print(f"--- [DEBUG] ASRTask received dataset_split: '{dataset_split}' ---")
+
+        # Load dataset
+        self.dataset = load_dataset(
+            dataset_name,
+            dataset_subset,
+            split=dataset_split,
+            trust_remote_code=True,
+            token=hf_token
+        )
+
+        # Resample audio column to 16 kHz
+        self.dataset = self.dataset.cast_column(audio_column, Audio(sampling_rate=16000))
+
+        # Standardize column names
+        self.dataset = self.dataset.rename_columns({
+            audio_column: "audio",
+            text_column: "reference_text"
+        })
+
+        # Apply transliteration
+        def transliterate_to_latin(text):
+            text["reference_text"] = to_latin_serbian(text["reference_text"])
+            return text
+
+        self.dataset = self.dataset.map(transliterate_to_latin)
+
+
+
+    def __iter__(self):
+        return iter(self.dataset)
+    
+    def __len__(self):
+        return len(self.dataset)
