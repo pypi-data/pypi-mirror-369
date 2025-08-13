8d6d7c6a190feddff47d1b7abbe7d9d821821f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:28:13 +0000

    Add package text-encryption-decryption/1.0.0/text_encryption_decryption-1.0.0-py3-none-any.whl

---
+++ packages/text-encryption-decryption/text_encryption_decryption-1.0.0-py3-none-any.whl/tests/test_encrypt.py
@@ -0,0 +1,14 @@
+
+def encryption(string="Success"):
+    encrypted=""
+    numbers=""
+    twos = []
+    for ch in string:
+        b=ord(ch)
+        ones=(~b) & 0xFF
+        tw=(ones+1) & 0xFF
+        twos.append(tw)
+    for num in twos:
+        numbers+=str(num)
+    encrypted = (numbers[int(len(numbers)/2):])[::-1] + (numbers[:int(len(numbers)/2)])[::-1]
+    assert encrypted == "141141551751751931371"
