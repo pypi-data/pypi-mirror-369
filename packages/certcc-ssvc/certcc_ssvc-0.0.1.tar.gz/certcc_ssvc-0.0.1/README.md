45da1b82570b36f7bbe406b98925a23f7fa16b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:05:30 +0000

    Add package certcc-ssvc/0.0.1/certcc_ssvc-0.0.1.tar.gz

---
+++ packages/certcc-ssvc/certcc_ssvc-0.0.1.tar.gz/certcc_ssvc-0.0.1/README.md
@@ -0,0 +1,7 @@
+# CERTCC SSVC
+
+**Stakeholder-Specific Vulnerability Categorization**
+
+This is a placeholder for the official CERTCC SSVC package. The complete tooling and documentation is planned to be released by **September 2025**.
+
+Stay tuned.
---
+++ packages/certcc-ssvc/certcc_ssvc-0.0.1.tar.gz/certcc_ssvc-0.0.1/ssvc/__init__.py
@@ -0,0 +1,12 @@
+"""
+SSVC (Stakeholder-Specific Vulnerability Categorization)
+
+This is a placeholder package. The official SSVC tools and documentation
+will be released by September 2025.
+"""
+
+__version__ = "0.0.1"
+__author__ = "CERTCC"
+
+def coming_soon():
+    return "SSVC package will be published by September 2025. Stay tuned!"
