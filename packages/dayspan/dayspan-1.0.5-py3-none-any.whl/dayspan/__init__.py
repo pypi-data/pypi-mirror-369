39d70456850dab72842321cdf6443ee9ac6d0d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:06:10 +0000

    Add package dayspan/1.0.5/dayspan-1.0.5-py3-none-any.whl

---
+++ packages/dayspan/dayspan-1.0.5-py3-none-any.whl/dayspan/__init__.py
@@ -0,0 +1,7 @@
+"""DaySpan - A simple tool to calculate days between dates."""
+
+from .calculator import calculate_days, parse_date
+
+__version__ = '1.0.5'
+__author__ = 'Cong Gao'
+__all__ = ['calculate_days', 'parse_date']
