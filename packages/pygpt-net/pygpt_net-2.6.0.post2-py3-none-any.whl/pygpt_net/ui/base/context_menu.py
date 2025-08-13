fab710313f64f9bbcd7a072df112fdaf4ae4ebde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 14:06:00 +0000

    Add package pygpt-net/2.6.0.post2/pygpt_net-2.6.0.post2-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.6.0.post2-py3-none-any.whl/pygpt_net/ui/base/context_menu.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2025.08.08 05:00:00                  #
+# ================================================== #
+
+from PySide6.QtWidgets import QMenu
+from PySide6.QtGui import QAction, QIcon
+
+from pygpt_net.core.tabs.tab import Tab
+from pygpt_net.utils import trans
+import pygpt_net.icons_rc
+
+
+class ContextMenu:
+    def __init__(self, window=None):
+        """
+        Context menu common
+
+        :param window: main window
+        """
+        self.window = window
+
+    def get_copy_to_menu(self, parent, selected_text: str = None, excluded: list = None) -> QMenu:
+        """
+        Get copy to menu
+
+        :param parent: Parent widget
+        :param selected_text: Selected text
+        :param excluded: Excluded items
+        :return: Menu
+        """
+        if excluded is None:
+            excluded = []
+        menu = QMenu(trans('text.context_menu.copy_to'), parent)
+
+        # input
+        if 'input' not in excluded:
+            action = QAction(QIcon(":/icons/input.svg"), trans('text.context_menu.copy_to.input'), parent)
+            action.triggered.connect(
+                lambda: self.window.controller.chat.common.append_to_input(selected_text))
+            menu.addAction(action)
+
+        # calendar
+        if 'calendar' not in excluded:
+            action = QAction(QIcon(":/icons/schedule.svg"), trans('text.context_menu.copy_to.calendar'), parent)
+            action.triggered.connect(
+                lambda: self.window.controller.calendar.note.append_text(selected_text))
+            menu.addAction(action)
+
+        # notepad
+        if 'notepad' not in excluded:
+            tabs = self.window.core.tabs.get_tabs_by_type(Tab.TAB_NOTEPAD)
+            if len(tabs) > 0:
+                for tab in tabs:
+                    action = QAction(QIcon(":/icons/paste.svg"), tab.title, parent)
+                    action.triggered.connect(lambda checked=False, tab=tab:
+                                             self.window.controller.notepad.append_text(selected_text, tab.data_id))
+                    menu.addAction(action)
+
+        # python interpreter
+        if 'interpreter' not in excluded:
+            menu.addSeparator()
+            if 'interpreter_edit' not in excluded:
+                action = QAction(QIcon(":/icons/code.svg"), trans('text.context_menu.copy_to.python.code'), parent)
+                action.triggered.connect(
+                    lambda: self.window.tools.get("interpreter").append_to_edit(selected_text))
+                menu.addAction(action)
+
+            if 'interpreter_input' not in excluded:
+                action = QAction(QIcon(":/icons/code.svg"), trans('text.context_menu.copy_to.python.input'), parent)
+                action.triggered.connect(
+                    lambda: self.window.tools.get("interpreter").append_to_input(selected_text))
+                menu.addAction(action)
+
+        # translator
+        if 'translator' not in excluded:
+            menu.addSeparator()
+            if 'translator_left' not in excluded:
+                action = QAction(QIcon(":/icons/text.svg"), trans('text.context_menu.copy_to.translator_left'),
+                                 parent)
+                action.triggered.connect(
+                    lambda: self.window.tools.get("translator").append_content("left", selected_text))
+                menu.addAction(action)
+
+            if 'translator_right' not in excluded:
+                action = QAction(QIcon(":/icons/text.svg"), trans('text.context_menu.copy_to.translator_right'),
+                                 parent)
+                action.triggered.connect(
+                    lambda: self.window.tools.get("translator").append_content("right", selected_text))
+                menu.addAction(action)
+
+        return menu
---
+++ packages/pygpt-net/pygpt_net-2.6.0.post2-py3-none-any.whl/pygpt_net/ui/layout/toolbox/presets.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.11.21 20:00:00                  #
+# ================================================== #
+
+from PySide6 import QtCore
+from PySide6.QtGui import QStandardItemModel, Qt, QIcon
+from PySide6.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton, QSplitter, QWidget, QSizePolicy
+
+from pygpt_net.core.types import (
+    MODE_AGENT,
+    MODE_EXPERT,
+)
+from pygpt_net.ui.widget.element.labels import HelpLabel, TitleLabel
+from pygpt_net.ui.widget.lists.preset import PresetList
+from pygpt_net.ui.layout.toolbox.footer import Footer
+from pygpt_net.utils import trans
+import pygpt_net.icons_rc
+
+class Presets:
+    def __init__(self, window=None):
+        """
+        Toolbox UI
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.footer = Footer(window)
+        self.id = 'preset.presets'
+
+    def setup(self) -> QSplitter:
+        """
+        Setup presets
+
+        :return: QSplitter
+        """
+        presets = self.setup_presets()
+
+        self.window.ui.models['preset.presets'] = self.create_model(self.window)
+        self.window.ui.nodes['preset.presets'].setModel(self.window.ui.models['preset.presets'])
+
+        self.window.ui.nodes['presets.widget'] = QWidget()
+        self.window.ui.nodes['presets.widget'].setLayout(presets)
+        self.window.ui.nodes['presets.widget'].setMinimumHeight(180)
+        self.window.ui.nodes['presets.widget'].setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        return self.window.ui.nodes['presets.widget']
+
+    def setup_presets(self) -> QVBoxLayout:
+        """
+        Setup list
+
+        :return: QVBoxLayout
+        """
+        self.window.ui.nodes['preset.presets.new'] = QPushButton(QIcon(":/icons/add.svg"), "")
+        self.window.ui.nodes['preset.presets.new'].clicked.connect(
+            lambda: self.window.controller.presets.editor.edit()
+        )
+
+        self.window.ui.nodes['preset.presets.label'] = TitleLabel(trans("toolbox.presets.label"))
+        self.window.ui.nodes['preset.agents.label'] = TitleLabel(trans("toolbox.agents.label"))
+        self.window.ui.nodes['preset.experts.label'] = TitleLabel(trans("toolbox.experts.label"))
+        self.window.ui.nodes['preset.presets.label'].setVisible(False)
+        self.window.ui.nodes['preset.agents.label'].setVisible(False)
+        self.window.ui.nodes['preset.experts.label'].setVisible(False)
+
+        header = QHBoxLayout()
+        header.addWidget(self.window.ui.nodes['preset.presets.label'])
+        header.addWidget(self.window.ui.nodes['preset.agents.label'])
+        header.addWidget(self.window.ui.nodes['preset.experts.label'])
+        header.addWidget(self.window.ui.nodes['preset.presets.new'], alignment=Qt.AlignRight)
+        header.setContentsMargins(5, 0, 0, 0)
+
+        header_widget = QWidget()
+        header_widget.setLayout(header)
+
+        self.window.ui.nodes[self.id] = PresetList(self.window, self.id)
+        self.window.ui.nodes[self.id].selection_locked = self.window.controller.presets.preset_change_locked
+        self.window.ui.nodes[self.id].setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        self.window.ui.nodes['tip.toolbox.presets'] = HelpLabel(trans('tip.toolbox.presets'), self.window)
+        self.window.ui.nodes['tip.toolbox.presets'].setAlignment(Qt.AlignCenter)
+
+        layout = QVBoxLayout()
+        layout.addStretch()
+        layout.addWidget(header_widget)
+        layout.addWidget(self.window.ui.nodes[self.id], 1)
+        layout.addWidget(self.window.ui.nodes['tip.toolbox.presets'])
+        layout.setContentsMargins(2, 5, 5, 5)
+
+        self.window.ui.models[self.id] = self.create_model(self.window)
+        self.window.ui.nodes[self.id].setModel(self.window.ui.models[self.id])
+
+        return layout
+
+    def create_model(self, parent) -> QStandardItemModel:
+        """
+        Create list model
+        :param parent: parent widget
+        :return: QStandardItemModel
+        """
+        return QStandardItemModel(0, 1, parent)
+
+    def update_presets(self, data):
+        """
+        Update presets list
+
+        :param data: Data to update
+        """
+        mode = self.window.core.config.get('mode')
+        self.window.ui.nodes[self.id].backup_selection()
+        old_model = self.window.ui.models[self.id]
+        if old_model is not None:
+            old_model.clear()
+            old_model.deleteLater()
+
+        new_model = self.create_model(self.window)
+        self.window.ui.models[self.id] = new_model
+        self.window.ui.nodes[self.id].setModel(new_model)
+
+        i = 0
+        for key in data:
+            new_model.insertRow(i)
+
+            name = data[key].name
+            if mode == MODE_EXPERT and not key.startswith("current.") and data[key].enabled:
+                name = "[x] " + name
+            elif mode == MODE_AGENT:
+                num_experts = self.window.core.experts.count_experts(key)
+                if num_experts > 0:
+                    name = f"{name} ({num_experts} experts)"
+
+            prompt = str(data[key].prompt)
+            if len(prompt) > 80:
+                prompt = prompt[:80] + '...'
+            tooltip = prompt
+
+            index = new_model.index(i, 0)
+            new_model.setData(index, tooltip, QtCore.Qt.ToolTipRole)
+            new_model.setData(index, name)
+            i += 1
+
+        self.window.ui.nodes[self.id].restore_selection()
