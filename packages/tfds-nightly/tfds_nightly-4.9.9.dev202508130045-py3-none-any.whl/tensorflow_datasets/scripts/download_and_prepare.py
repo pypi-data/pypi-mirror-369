57782ef1f4da477d869db6a7fd304f32561ad541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 14:04:36 +0000

    Add package tfds-nightly/4.9.9.dev202508130045/tfds_nightly-4.9.9.dev202508130045-py3-none-any.whl

---
+++ packages/tfds-nightly/tfds_nightly-4.9.9.dev202508130045-py3-none-any.whl/tensorflow_datasets/scripts/download_and_prepare.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+# Copyright 2025 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+r"""Wrapper around `tfds build`."""
+
+import typing
+
+from absl import app
+from absl import flags
+from absl import logging
+from tensorflow_datasets.scripts.cli import build
+from tensorflow_datasets.scripts.cli import main as main_cli
+
+module_import = flags.DEFINE_string('module_import', None, '`--imports` flag.')
+dataset = flags.DEFINE_string('dataset', None, 'singleton `--datasets` flag.')
+
+builder_config_id = flags.DEFINE_integer(
+    'builder_config_id', None, '`--config_idx` flag'
+)
+
+
+
+def _parse_flags(argv: list[str]) -> main_cli.Args:
+  """Command lines flag parsing."""
+  return main_cli._parse_flags([argv[0], 'build'] + argv[1:])  # pylint: disable=protected-access
+
+
+_display_warning = True
+
+
+def main(args: main_cli.Args) -> None:
+  if _display_warning:
+    logging.warning(
+        '***`tfds build` should be used instead of `download_and_prepare`.***'
+    )
+  cmd_args = typing.cast(build.Args, args.command)
+  if module_import.value:
+    cmd_args.generation.imports = module_import.value
+  if dataset.value:
+    cmd_args.datasets = [dataset.value]
+  if builder_config_id.value is not None:
+    cmd_args.generation.config_idx = builder_config_id.value
+  main_cli.main(args)
+
+
+if __name__ == '__main__':
+  app.run(main, flags_parser=_parse_flags)
