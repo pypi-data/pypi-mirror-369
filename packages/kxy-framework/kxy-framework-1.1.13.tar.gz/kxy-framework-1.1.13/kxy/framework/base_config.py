7c049ec0dbcd05017ceb6b1e7763f9bb61afa8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 16:04:38 +0000

    Add package kxy-framework/1.1.13/kxy-framework-1.1.13.tar.gz

---
+++ packages/kxy-framework/kxy-framework-1.1.13.tar.gz/kxy-framework-1.1.13/kxy/framework/base_config.py
@@ -0,0 +1,24 @@
+import os
+from abc import ABC, abstractmethod
+
+class BaseConfig(ABC):
+    BussinessLog = False
+    @abstractmethod
+    def SSO_URL(self):
+        pass
+
+    @abstractmethod
+    def SystemCode(self):
+        pass
+    @abstractmethod
+    def JWT_SECRET_KEY(self):
+        pass
+    @abstractmethod
+    def JWT_ALGORITHM(self):
+        pass
+
+    def env_first(self):
+        for name in [f for f in dir(self) if not callable(f) and not f.startswith('__')]:
+            v=os.environ.get(name)
+            if v:
+                setattr(self, name, v)
