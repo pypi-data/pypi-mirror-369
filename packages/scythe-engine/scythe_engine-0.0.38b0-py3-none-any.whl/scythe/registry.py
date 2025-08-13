2b52a0cad7abfe14c28446f7f79b69141e3752d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:28:01 +0000

    Add package scythe-engine/0.0.38b0/scythe_engine-0.0.38b0-py3-none-any.whl

---
+++ packages/scythe-engine/scythe_engine-0.0.38b0-py3-none-any.whl/scythe/registry.py
@@ -0,0 +1,205 @@
+"""Register experiments with Scythe."""
+
+import inspect
+import tempfile
+from pathlib import Path
+from typing import ClassVar, Literal, Protocol, TypeVar, cast, get_type_hints
+
+from hatchet_sdk import Context, Worker
+from hatchet_sdk.labels import DesiredWorkerLabel
+from hatchet_sdk.runnables.workflow import Standalone
+from hatchet_sdk.utils.timedelta_to_expression import Duration
+
+from scythe.base import ExperimentInputSpec, ExperimentOutputSpec
+from scythe.hatchet import hatchet
+from scythe.settings import timeout_settings
+from scythe.utils.s3 import s3_client
+
+ExperimentStandaloneType = Standalone[ExperimentInputSpec, ExperimentOutputSpec]
+
+TInput = TypeVar("TInput", bound=ExperimentInputSpec, contravariant=True)
+TOutput = TypeVar("TOutput", bound=ExperimentOutputSpec, covariant=True)
+
+
+class ExperimentTypeNotFound(Exception):
+    """An experiment was not found."""
+
+    def __init__(self, name: str):
+        """Initialize the exception."""
+        self.name = name
+        super().__init__(f"Experiment {name} not found")
+
+
+class ExperimentTypeExists(Exception):
+    """An experiment type already exists."""
+
+    def __init__(self, name: str):
+        """Initialize the exception."""
+        self.name = name
+        super().__init__(f"Experiment {name} already exists")
+
+
+class ExperimentFunctionWithoutTempdir(Protocol[TInput, TOutput]):
+    """A function that can run experiments, with or without tempdir support."""
+
+    def __call__(self, input_spec: TInput) -> TOutput:
+        """Invoke the experiment function with flexible signature."""
+        ...
+
+    def __name__(self) -> str:
+        """The name of the experiment function."""
+        ...
+
+
+class ExperimentFunctionWithTempdir(Protocol[TInput, TOutput]):
+    """A function that can run experiments, with tempdir support."""
+
+    def __call__(self, input_spec: TInput, tempdir: Path) -> TOutput:
+        """Invoke the experiment function with flexible signature."""
+        ...
+
+    def __name__(self) -> str:
+        """The name of the experiment function."""
+        ...
+
+
+ExperimentFunction = (
+    ExperimentFunctionWithoutTempdir[TInput, TOutput]
+    | ExperimentFunctionWithTempdir[TInput, TOutput]
+)
+
+
+def _function_accepts_tempdir(fn_: ExperimentFunction) -> bool:
+    """Check if a function accepts a tempdir parameter."""
+    sig = inspect.signature(fn_)
+    return ("tempdir" in sig.parameters) and (
+        sig.parameters["tempdir"].annotation is Path
+    )
+
+
+class ExperimentRegistry:
+    """An experiment registry for standalone task steps."""
+
+    _experiments_dict: ClassVar[dict[str, ExperimentStandaloneType]] = {}
+
+    @classmethod
+    def Include(
+        cls,
+        task: Standalone[TInput, TOutput],
+    ) -> ExperimentStandaloneType:
+        """Register an experiment whose input and output schemas conform to the base types."""
+        task_safe = cast(ExperimentStandaloneType, task)
+        if task_safe.name in cls._experiments_dict:
+            raise ExperimentTypeExists(task_safe.name)
+        cls._experiments_dict[task_safe.name] = task_safe
+        return task_safe
+
+    @classmethod
+    def Register(
+        cls,
+        *,
+        worker: Worker | None = None,
+        description: str | None = None,
+        name: str | None = None,
+        desired_worker_labels: dict[str, DesiredWorkerLabel] | None = None,
+        schedule_timeout: Duration = timeout_settings.EXPERIMENT_SCHEDULE,
+        execution_timeout: Duration = timeout_settings.EXPERIMENT_EXECUTION,
+        retries: int = 1,
+        overwrite_log_method: bool = True,
+        inject_workflow_run_id: bool = True,
+        auto_fetch_files: bool = True,
+        local_file_location: Literal["cache", "copied-to-tempdir"] = "cache",
+        **task_config,
+    ):
+        """Decorator to make a standalone experiment from a function.
+
+        Usage:
+            @ExperimentRegistry.Register(description="desc", ...)
+            def my_experiment(input_spec: MyInput) -> MyOutput:
+                ...
+        """
+
+        def decorator(
+            fn: ExperimentFunction[TInput, TOutput],
+        ) -> ExperimentStandaloneType:
+            input_type = cast(type[TInput], get_type_hints(fn)["input_spec"])
+            return_type = cast(type[TOutput], get_type_hints(fn)["return"])
+
+            fn_name = fn.__name__
+            fn_doc = fn.__doc__
+            fn_accepts_tempdir = _function_accepts_tempdir(fn)
+            if (
+                auto_fetch_files
+                and local_file_location == "copied-to-tempdir"
+                and not fn_accepts_tempdir
+            ):
+                msg = "Function does not accept tempdir, so local_file_location must be 'cache'"
+                raise ValueError(msg)
+
+            @cls.Include
+            @hatchet.task(
+                name=name or f"scythe_experiment_{fn_name}",
+                description=description or f"{fn_doc}",
+                input_validator=input_type,
+                desired_worker_labels=desired_worker_labels,
+                schedule_timeout=schedule_timeout,
+                execution_timeout=execution_timeout,
+                retries=retries,
+                **task_config,
+            )
+            def task(input_: input_type, context: Context) -> return_type:  # pyright: ignore [reportInvalidTypeForm]
+                """The task implementation."""
+                if overwrite_log_method:
+                    input_.log = lambda msg: context.log(msg)
+                if inject_workflow_run_id:
+                    input_.workflow_run_id = context.workflow_run_id
+
+                input_to_call_with = input_
+                if auto_fetch_files:
+                    input_to_call_with = input_._fetch_and_replace_file_references()
+                    input_to_call_with._original_spec = input_
+
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    tdir = Path(temp_dir)
+                    if fn_accepts_tempdir:
+                        if (
+                            local_file_location == "copied-to-tempdir"
+                            and auto_fetch_files
+                        ):
+                            input_to_call_with = (
+                                input_to_call_with._copy_local_files_to_and_reference(
+                                    tdir
+                                )
+                            )
+                        fn_ = cast(ExperimentFunctionWithTempdir[TInput, TOutput], fn)
+                        output = fn_(input_to_call_with, tempdir=tdir)
+                    else:
+                        fn_ = cast(
+                            ExperimentFunctionWithoutTempdir[TInput, TOutput], fn
+                        )
+                        output = fn_(input_to_call_with)
+                    output._add_scalars(input_)
+                    output._transfer_files(
+                        input_,
+                        input_.storage_settings,
+                        s3_client,
+                    )
+                    return output
+
+            if worker:
+                worker.register_workflow(task)
+            return task
+
+        return decorator
+
+    @classmethod
+    def get_experiment(cls, name: str) -> ExperimentStandaloneType:
+        """Get an experiment's Hatchet Stanadalone."""
+        if name not in cls._experiments_dict:
+            raise ExperimentTypeNotFound(name)
+        return cls._experiments_dict[name]
+
+    @classmethod
+    def experiments(cls) -> list[ExperimentStandaloneType]:
+        """Get all experiments."""
+        return list(cls._experiments_dict.values())
---
+++ packages/scythe-engine/scythe_engine-0.0.38b0-py3-none-any.whl/scythe/utils/filesys.py
@@ -0,0 +1,137 @@
+"""Filesystem utilities."""
+
+import logging
+import shutil
+from collections.abc import Callable
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+try:
+    # python 3.11+
+    from typing import Self  # pyright: ignore [reportAttributeAccessIssue]
+except ImportError:
+    # python 3.10
+    from typing_extensions import Self  # noqa: UP035
+
+import requests
+from pydantic import AnyUrl, BaseModel, FilePath, HttpUrl, UrlConstraints
+
+from scythe.utils.s3 import s3_client as s3
+
+if TYPE_CHECKING:
+    from mypy_boto3_s3.client import S3Client as S3ClientType
+else:
+    S3ClientType = object
+
+logger = logging.getLogger(__name__)
+
+
+def fetch_uri(  # noqa: C901
+    uri: AnyUrl | str,
+    local_path: Path,
+    use_cache: bool = True,
+    logger_fn: Callable = logger.info,
+    s3: S3ClientType = s3,
+) -> Path:
+    """Fetch a file from a uri and return the local path.
+
+    Caching is enabled by default and works by
+    checking if the file exists locally before downloading it
+    to avoid downloading the same file multiple times.
+
+    Args:
+        uri (AnyUrl): The uri to fetch
+        local_path (Path): The local path to save the fetched file
+        use_cache (bool): Whether to use the cache
+        logger_fn (Callable): The logger function to use
+        s3 (S3Client): The S3 client to use
+
+    Returns:
+        local_path (Path): The local path of the fetched file
+    """
+    if isinstance(uri, str):
+        uri = AnyUrl(uri)
+    if uri.scheme == "s3":
+        bucket = uri.host
+        if not uri.path:
+            raise ValueError(f"S3URI:NO_PATH:{uri}")
+        if not bucket:
+            raise ValueError(f"S3URI:NO_BUCKET:{uri}")
+        path = uri.path[1:]
+        if not local_path.exists() or not use_cache:
+            logger_fn(f"Downloading {uri}...")
+            local_path.parent.mkdir(parents=True, exist_ok=True)
+            s3.download_file(bucket, path, str(local_path))
+        else:
+            logger_fn(f"File {local_path} already exists, skipping download.")
+    elif uri.scheme == "http" or uri.scheme == "https":
+        if not local_path.exists() or not use_cache:
+            logger_fn(f"Downloading {uri}...")
+            local_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(local_path, "wb") as f:
+                f.write(requests.get(str(uri), timeout=60).content)
+        else:
+            logger_fn(f"File {local_path} already exists, skipping download.")
+    elif uri.scheme == "file":
+        if not local_path.exists() or not use_cache:
+            logger_fn(f"Copying {uri} to {local_path}...")
+            local_path.parent.mkdir(parents=True, exist_ok=True)
+            if uri.path:
+                shutil.copy(uri.path, local_path.as_posix())
+            else:
+                msg = f"File URI:NO_PATH:{uri}"
+                logger_fn(msg)
+                raise ValueError(msg)
+        else:
+            logger_fn(f"File {local_path} already exists, skipping copy.")
+    else:
+        raise NotImplementedError(f"URI:SCHEME:{uri.scheme}")
+    return local_path
+
+
+class S3Url(AnyUrl):
+    """A URL for an S3 object."""
+
+    _constraints = UrlConstraints(allowed_schemes=["s3"], host_required=True)
+
+
+FileReference = S3Url | HttpUrl | Path | FilePath
+
+
+class FileReferenceMixin(BaseModel):
+    """A mixin for file reference fields."""
+
+    @classmethod
+    def _file_reference_fields(cls) -> list[str]:
+        """Get the file reference fields."""
+        annotations = cls.model_fields
+        return [k for k, v in annotations.items() if v.annotation is FileReference]
+
+    @property
+    def _local_artifact_file_paths(self) -> dict[str, Path]:
+        """Get the local source file paths."""
+        data = self.model_dump()
+        return {
+            k: data[k]
+            for k in self._file_reference_fields()
+            if isinstance(data[k], Path)
+        }
+
+    @property
+    def remote_artifact_file_paths(self) -> dict[str, HttpUrl | S3Url]:
+        """Get the remote source file paths."""
+        data = self.model_dump()
+        return {
+            k: data[k]
+            for k in self._file_reference_fields()
+            if not isinstance(data[k], Path)
+        }
+
+    def _copy_local_files_to_and_reference(self, pth: Path) -> Self:
+        """Copy the local files to a given path."""
+        local_paths = self._local_artifact_file_paths
+        new_self = self.model_copy(deep=True)
+        for k, v in local_paths.items():
+            shutil.copy(v, pth / f"{k}{v.suffix}")
+            setattr(new_self, k, pth / f"{k}{v.suffix}")
+        return new_self
