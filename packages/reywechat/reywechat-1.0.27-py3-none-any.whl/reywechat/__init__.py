ee15a6a2e8f4b87f877a1dbfae10169ef69cb927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:10:49 +0000

    Add package reywechat/1.0.27/reywechat-1.0.27-py3-none-any.whl

---
+++ packages/reywechat/reywechat-1.0.27-py3-none-any.whl/reywechat/__init__.py
@@ -0,0 +1,26 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-10-17 20:24:04
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : WeChat method set.
+
+Folders
+-------
+data : Data file.
+
+Modules
+-------
+rall : All methods.
+rbase : Base methods.
+rclient : Client methods.
+rdb : WeChatDatabase methods.
+rlog : Log methods.
+rreceive : Receive methods.
+rschedule : Schedule methods.
+rsend : Send methods.
+rtrigger : Trigger methods.
+rwechat : WeChat methods.
+"""
