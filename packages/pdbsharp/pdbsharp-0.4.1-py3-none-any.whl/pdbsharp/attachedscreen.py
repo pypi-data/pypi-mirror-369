9bc518c9e2be112dffd2cc59e362eab8469a6907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:10:19 +0000

    Add package pdbsharp/0.4.1/pdbsharp-0.4.1-py3-none-any.whl

---
+++ packages/pdbsharp/pdbsharp-0.4.1-py3-none-any.whl/pdbsharp/attachedscreen.py
@@ -0,0 +1,67 @@
+import json
+from typing import TYPE_CHECKING
+
+from textual.app import ComposeResult
+from textual.binding import Binding
+from textual.containers import Horizontal, Vertical
+from textual.message import Message
+from textual.reactive import reactive
+from textual.screen import Screen
+from textual.widgets import Footer, Header, Input
+
+from .debuginputarea import DebugInputArea
+from .debugresponsearea import DebugResponseArea
+from .IOarea import IOArea
+from .messages import MessageFromRepl
+from .process_utils import get_process_name
+
+if TYPE_CHECKING:
+    from .pdbsharp import Pdbsharp
+
+
+class AttachedScreen(Screen):
+    prompt: reactive[str] = reactive("", init=False)
+    show_io_screen: reactive[bool] = reactive(True)
+
+    BINDINGS = [
+        Binding("ctrl+d", "detach", "Detach from Process", priority=True),
+    ]
+
+    CSS_PATH = "attachedscreen.tcss"
+
+    def compose(self) -> ComposeResult:
+        self.sub_title = f"{get_process_name(self.app._server_pid) if self.app._server_pid else 'process'} (pid {self.app._server_pid if self.app._server_pid else '*unknown*'})"
+
+        yield Header()
+        with Horizontal():
+            with Vertical(id="primary"):
+                yield DebugResponseArea()
+                yield DebugInputArea().data_bind(AttachedScreen.prompt)
+            if self.show_io_screen:
+                yield IOArea()
+        yield Footer()
+
+    def on_message_from_repl(self, message: MessageFromRepl):
+        payload: dict[str, str | list[str]] = json.loads(message.text)
+        match payload:
+            case {"type": "pdbsharp", "message": str(msg)}:
+                self.screen.query_one(DebugResponseArea).write(f"{self.prompt}{msg}")
+            case {"type": "info", "message": str(msg)}:
+                self.screen.query_one(DebugResponseArea).write(msg)
+            case {"type": "error", "message": str(msg)}:
+                self.screen.query_one(DebugResponseArea).write("ERROR FROM PDB: " + msg)
+            case {"command_list": list(_command_list)}:
+                self.app.command_list = _command_list[:]
+            case {"state": str(state), "prompt": str(prompt)}:
+                self.prompt = prompt
+            case _:
+                raise ValueError(
+                    f"Could not determine how to handle message from remote pdb: {payload}"
+                )
+
+    def on_screen_resume(self, *args) -> None:
+        self.query_one(DebugInputArea).query_one(Input).focus()
+
+    def action_detach(self) -> None:
+        self.app: Pdbsharp
+        self.app.detach()
