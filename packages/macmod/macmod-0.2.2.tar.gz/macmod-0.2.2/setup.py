aa04b3b346d485cd483301bd2cd7bef6331a8150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:05:15 +0000

    Add package macmod/0.2.2/macmod-0.2.2.tar.gz

---
+++ packages/macmod/macmod-0.2.2.tar.gz/macmod-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='macmod',
+    version='0.2.2',
+    description='A simple command-line tool to format MAC addresses in standard styles.',
+    author='Maksym Ototiuk',
+    author_email='mac@masik.slmail.me', 
+    url='https://github.com/Maksym-Ototiuk/macmod', 
+    packages=find_packages(), 
+    install_requires=[
+        'pyperclip',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mac = macmod.macmod:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
