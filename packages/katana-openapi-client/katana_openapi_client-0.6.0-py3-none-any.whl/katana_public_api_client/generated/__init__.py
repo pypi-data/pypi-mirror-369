e9792952cdabc0778c596c92ee7264dfb6f6f8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:05:23 +0000

    Add package katana-openapi-client/0.6.0/katana_openapi_client-0.6.0-py3-none-any.whl

---
+++ packages/katana-openapi-client/katana_openapi_client-0.6.0-py3-none-any.whl/katana_public_api_client/generated/__init__.py
@@ -0,0 +1,10 @@
+"""Generated OpenAPI client code - do not edit manually."""
+
+from .client import AuthenticatedClient, Client
+from .errors import *  # noqa: F403
+from .types import *  # noqa: F403
+
+__all__ = [
+    "AuthenticatedClient",
+    "Client",
+]
