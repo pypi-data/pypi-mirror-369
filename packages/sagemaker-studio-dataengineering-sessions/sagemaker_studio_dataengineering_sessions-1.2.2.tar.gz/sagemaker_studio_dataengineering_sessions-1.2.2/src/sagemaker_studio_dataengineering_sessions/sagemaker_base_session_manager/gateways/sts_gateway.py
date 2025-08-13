df32cb9422566dc02947adc70d0269b1e688c7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 16:05:30 +0000

    Add package sagemaker-studio-dataengineering-sessions/1.2.2/sagemaker_studio_dataengineering_sessions-1.2.2.tar.gz

---
+++ packages/sagemaker-studio-dataengineering-sessions/sagemaker_studio_dataengineering_sessions-1.2.2.tar.gz/sagemaker_studio_dataengineering_sessions-1.2.2/src/sagemaker_studio_dataengineering_sessions/sagemaker_base_session_manager/gateways/sts_gateway.py
@@ -0,0 +1,18 @@
+import boto3
+import logging
+
+from sagemaker_studio_dataengineering_sessions.sagemaker_base_session_manager.common.constants import DATAZONE_DOMAIN_REGION
+
+
+class StsGateway:
+    def __init__(self, sts_client):
+        self.sts_client = sts_client
+        self.logger = logging.getLogger(__name__)
+
+    def get_source_identity(self):
+        try:
+            response = self.sts_client.get_caller_identity()
+            return response['Arn'].split('/')[-1].split('@')[0]
+        except Exception as e:
+            self.logger.error("Failed to retrieve source identity.", e)
+            raise e
