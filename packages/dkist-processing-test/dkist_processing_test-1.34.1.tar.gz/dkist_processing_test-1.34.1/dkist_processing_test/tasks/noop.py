40c74d2fa6dde74e86022724e6cc5dd10dc36e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:27:36 +0000

    Add package dkist-processing-test/1.34.1/dkist_processing_test-1.34.1.tar.gz

---
+++ packages/dkist-processing-test/dkist_processing_test-1.34.1.tar.gz/dkist_processing_test-1.34.1/dkist_processing_test/tasks/noop.py
@@ -0,0 +1,15 @@
+"""
+Test task for infrastructure integration
+"""
+from dkist_processing_core import TaskBase
+
+__all__ = ["NoOpTask", "NoOpTask2"]
+
+
+class NoOpTask(TaskBase):
+    def run(self) -> None:
+        pass
+
+
+class NoOpTask2(NoOpTask):
+    pass
