0528c96cbcdf4af226da8e3fff743e2785efa727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 14:05:55 +0000

    Add package holamundoplayerabelgp/0.0.1/holamundoplayerabelgp-0.0.1.tar.gz

---
+++ packages/holamundoplayerabelgp/holamundoplayerabelgp-0.0.1.tar.gz/holamundoplayerabelgp-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from pathlib import Path
+
+long_desc = Path("README.md").read_text()
+
+setuptools.setup(
+    name="holamundoplayerabelgp",
+    version="0.0.1",
+    long_description=long_desc,
+    packages=setuptools.find_packages(
+        exclude=["mocks", "tests"]
+    ))
