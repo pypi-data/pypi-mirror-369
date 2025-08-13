7516a3950218ef6a9136985d8c86bae5f42a031a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 15:04:05 +0000

    Add package maleo-metadata/0.2.11/maleo_metadata-0.2.11-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.2.11-py3-none-any.whl/maleo/metadata/schemas/data/system_role.py
@@ -0,0 +1,61 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from maleo.soma.mixins.data import DataIdentifier, DataStatus, DataTimestamp
+from maleo.soma.mixins.general import Order
+from maleo.metadata.enums.system_role import SystemRole
+from maleo.metadata.mixins.system_role import Key, Name
+
+
+class SystemRoleDataSchema(
+    Name,
+    Key,
+    Order,
+    DataTimestamp,
+    DataStatus,
+    DataIdentifier,
+):
+    pass
+
+
+class SimpleSystemRoleMixin(BaseModel):
+    system_role: SystemRole = Field(..., description="Single System Role")
+
+
+class OptionalSimpleSystemRoleMixin(BaseModel):
+    system_role: Optional[SystemRole] = Field(
+        None, description="Single System Role. (Optional)"
+    )
+
+
+class ListOfSimpleSystemRolesMixin(BaseModel):
+    system_roles: List[SystemRole] = Field(..., description="Multiple System Roles")
+
+
+class OptionalListOfSimpleSystemRolesMixin(BaseModel):
+    system_roles: Optional[List[SystemRole]] = Field(
+        None, description="Multiple System Roles. (Optional)"
+    )
+
+
+class ExpandedSystemRoleMixin(BaseModel):
+    system_role_details: SystemRoleDataSchema = Field(
+        ..., description="Single System Role Data"
+    )
+
+
+class OptionalExpandedSystemRoleMixin(BaseModel):
+    system_role_details: Optional[SystemRoleDataSchema] = Field(
+        None, description="Single System Role Data. (Optional)"
+    )
+
+
+class ListOfExpandedSystemRolesMixin(BaseModel):
+    system_roles_details: List[SystemRoleDataSchema] = Field(
+        ..., description="Multiple System Roles Data"
+    )
+
+
+class OptionalListOfExpandedSystemRolesMixin(BaseModel):
+    system_roles_details: Optional[List[SystemRoleDataSchema]] = Field(
+        None, description="Multiple System Roles Data. (Optional)"
+    )
