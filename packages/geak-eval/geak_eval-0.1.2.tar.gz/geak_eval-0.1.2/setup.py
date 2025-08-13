14d654cb8a93dc7dd0f332d3fa5ee5adbeb45102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 16:05:13 +0000

    Add package geak-eval/0.1.2/geak_eval-0.1.2.tar.gz

---
+++ packages/geak-eval/geak_eval-0.1.2.tar.gz/geak_eval-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geak-eval',  # Replace with your module/package name
+    version='0.1.2',
+    author='Vinay Joshi',
+    author_email='vinajosh@amd.com',
+    description='An evaluation framework for Triton kernels',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # Add your dependencies here
+        "triton==3.3.0",
+        "parse_llm_code",
+        "pandas",
+        "numpy==1.26",
+        "openai==0.28"
+    ],
+    entry_points={
+        'console_scripts': [
+            'geak-eval=geak_eval.run:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
