d0660b9354636762cc353e2f6791ee18b088b3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 07:06:22 +0000

    Add package ya-json-server-pd/0.8.0/ya_json_server_pd-0.8.0.tar.gz

---
+++ packages/ya-json-server-pd/ya_json_server_pd-0.8.0.tar.gz/ya_json_server_pd-0.8.0/src/app/api.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+from app.handlers.commons import init_db_json
+from app.handlers.exceptions import APIException, api_exception_handler
+from app.views.routes import router
+
+init_db_json()
+
+description = """
+A mock REST API with zero coding.
+
+Technologies::
+* Python 3.13
+* FastAPI 0.116
+"""
+app = FastAPI(
+    title="Yet Another JSON Server",
+    description=description,
+    version="1.0.0",
+    openapi_tags=[{"name": "API"}],
+)
+
+app.add_exception_handler(APIException, api_exception_handler)
+app.add_exception_handler(FileNotFoundError, api_exception_handler)
+app.add_exception_handler(Exception, api_exception_handler)
+
+app.include_router(router)
