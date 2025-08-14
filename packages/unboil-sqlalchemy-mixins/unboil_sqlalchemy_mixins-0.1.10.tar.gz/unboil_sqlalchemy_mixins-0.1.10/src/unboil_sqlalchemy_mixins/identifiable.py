8d086fd10888dd018dccae0a2bd6f813510c7161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 15:06:26 +0000

    Add package unboil-sqlalchemy-mixins/0.1.10/unboil_sqlalchemy_mixins-0.1.10.tar.gz

---
+++ packages/unboil-sqlalchemy-mixins/unboil_sqlalchemy_mixins-0.1.10.tar.gz/unboil_sqlalchemy_mixins-0.1.10/src/unboil_sqlalchemy_mixins/identifiable.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Mapped, mapped_column, MappedAsDataclass
+from sqlalchemy import String
+import uuid
+
+
+class IdentifiableMixin(MappedAsDataclass):
+    """
+    Mixin for SQLAlchemy models to add a string 'id' primary key with optional user-specified prefix.
+    Usage:
+        class MyModel(IdentifiableMixin, Base, id_prefix="user_"):
+            ...
+    """
+
+    id: Mapped[str] = mapped_column(
+        String(32),
+        primary_key=True,
+        default_factory=lambda: uuid.uuid4().hex,
+        nullable=False,
+        init=False,
+    )
+    
+    def __init_subclass__(cls, id_prefix: str = "", **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+        cls.id = mapped_column(
+            String(32),
+            primary_key=True,
+            default_factory=lambda: f"{id_prefix}{uuid.uuid4().hex}",
+            nullable=False,
+            init=False,
+        )
