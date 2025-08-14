436f39b2f7fe62bc4a6b2ef4532ac481573fe583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 07:06:15 +0000

    Add package pyorbx/0.2.0/pyorbx-0.2.0.tar.gz

---
+++ packages/pyorbx/pyorbx-0.2.0.tar.gz/pyorbx-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="pyorbx",
+    version="0.2.0",
+    py_modules=["pyorbx"],
+    description="Cinematic legacy module with branded aliases and procedural clarity.",
+    author="Anonymous",
+    author_email="anon@legacy.dev",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
