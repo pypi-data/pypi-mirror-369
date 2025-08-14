6bd0eea0d08d63d4abc053d7b8101c2ab5ff8df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 07:06:20 +0000

    Add package hypothesis-crosshair/0.0.25/hypothesis_crosshair-0.0.25.tar.gz

---
+++ packages/hypothesis-crosshair/hypothesis_crosshair-0.0.25.tar.gz/hypothesis_crosshair-0.0.25/README.md
@@ -0,0 +1,128 @@
+# hypothesis-crosshair
+
+[![Downloads](https://pepy.tech/badge/hypothesis-crosshair)](https://pepy.tech/project/hypothesis-crosshair)
+
+
+Add the power of solver-based symbolic execution to your
+[Hypothesis](https://hypothesis.readthedocs.io/en/latest/index.html)
+tests with
+[CrossHair](https://github.com/pschanely/CrossHair).
+
+Just 
+```
+pip install hypothesis-crosshair
+```
+
+and then add a backend="crosshair" setting, like so:
+
+```
+from hypothesis import given, settings, strategies as st
+
+@settings(backend="crosshair")
+@given(st.integers())
+def test_needs_solver(x):
+    assert x != 123456789
+```
+
+
+Docs hopefully coming soon. In the meantime, start a
+[discussion](https://github.com/pschanely/hypothesis-crosshair/discussions)
+or file an [issue](https://github.com/pschanely/hypothesis-crosshair/issues).
+
+
+## FAQ
+
+### Can I try using crosshair for ALL my hypothesis tests?
+
+Yes! Create or edit your pytest
+[conftest.py](https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files)
+file to register a profile like the following:
+
+```
+from hypothesis import settings
+
+settings.register_profile(
+    "crosshair",
+    backend="crosshair",
+)
+```
+
+And then run pytest using the profile you've defined:
+```
+pytest . --hypothesis-profile=crosshair 
+```
+
+
+## Changelog
+
+### Next Version
+* Nothing yet
+
+### 0.0.25
+* Prevent internal crosshair errors from getting exposed while trying to 
+  recover from a user-level exception.
+* Synchronize with recent hypothesis API updates.
+  (fixes [#40](https://github.com/pschanely/hypothesis-crosshair/issues/40)
+  and [#42](https://github.com/pschanely/hypothesis-crosshair/issues/42))
+
+### 0.0.24
+* Do not attempt to capture and retry hypothesis internal exceptions.
+  (fixes [#34](https://github.com/pschanely/hypothesis-crosshair/issues/34))
+
+### 0.0.23
+* Prevent an incorrect verified claim under SMT-heavy analysis.
+  (fixes [pschanely/CrossHair#354](https://github.com/pschanely/CrossHair/issues/354))
+
+### 0.0.22
+* Abort concrete executions with invalid draws.
+  (fixes [#29](https://github.com/pschanely/hypothesis-crosshair/issues/29))
+* Adjust how the preventative measures in v0.0.21 work for recursive datastructures.
+
+### 0.0.21
+* Avoid occasional unexpected errors when stopping a test run with Ctrl-C.
+* Prevent over-expansion when generating recursive datastructures. (fixes [#27](https://github.com/pschanely/hypothesis-crosshair/issues/27))
+
+### 0.0.20
+* Avoid potential import warning when registering ourself with hypothesis.
+* Skip constraint checking when performing a concrete re-execution.
+
+### 0.0.19
+* Limit the re-thow behavior in 0.0.17 to Unsatisfiable errors exclusively
+* Change default path timeout to 2.5 seconds
+* Prevent false positives by ensuring user exceptions are only exposed under concrete executions.
+
+### 0.0.18
+* Ensure drawn floats respect hypothesis signed-zero semantics for min_value/max_value.
+
+### 0.0.17
+* Do not interpret Unsatisfiable errors as user exceptions; just re-throw, so that hypothesis can act appropriately.
+* Report CrossHair path abortions to hypothesis as `discard_test_case` instead of `verified`.
+  This lets Hypothesis report unsatisfiable strategies correctly when run under crosshair.
+
+### 0.0.16
+* Integrate hypothesis's new BackCannotProceed exception, which will reduce the likelihood of FlakeyReplay errors.
+* Validate suspected counterexamples with concrete executions.
+* Treat nondeterminism as an unexplored path rather than a user error. (though we might change this back later)
+* Ensure realization logic called by hypothesis cannot grow the path tree.
+* Allow for collapsing more SMT expressions when drawing strings and floats.
+
+### 0.0.15
+* (was never released)
+
+### 0.0.14
+* Support the revised hypothesis provider draw interfaces as of hypothesis `v6.112.0`.
+
+### 0.0.13
+* Integrate with the hypothesis [observability system](https://hypothesis.readthedocs.io/en/latest/observability.html).
+
+### 0.0.12
+* Error early when trying to nest hypothesis tests. (which will otherwise put CrossHair into a bad state)
+
+### 0.0.11
+* Address errors when the solver can't keep up (fixes [#20](https://github.com/pschanely/hypothesis-crosshair/issues/20))
+
+### 0.0.10
+* Reduce the numebr of iterations required to generate valid datetimes
+
+### 0.0.9
+* Quietly ignore iterations that appear to be failing due to symbolic intolerance.
---
+++ packages/hypothesis-crosshair/hypothesis_crosshair-0.0.25.tar.gz/hypothesis_crosshair-0.0.25/hypothesis_crosshair_provider/crosshair_provider_test.py
@@ -0,0 +1,149 @@
+from unittest.mock import patch
+
+import pytest
+from crosshair.util import (
+    IgnoreAttempt,
+    NotDeterministic,
+    UnexploredPath,
+    UnknownSatisfiability,
+)
+from hypothesis import settings
+from hypothesis import strategies as st
+from hypothesis.errors import BackendCannotProceed
+from hypothesis.internal.conjecture.provider_conformance import run_conformance_test
+from hypothesis.internal.intervalsets import IntervalSet
+
+from hypothesis_crosshair_provider.crosshair_provider import CrossHairPrimitiveProvider
+
+
+class TargetException(Exception):
+    pass
+
+
+def _example_user_code(s_bool, s_int, s_float, s_str, s_bytes):
+    if s_bool:
+        if s_int == 120:
+            if s_float < 2.0:
+                if s_str == "foo":
+                    if s_bytes == b"b":
+                        raise TargetException
+
+
+def test_basic_loop():
+    provider = CrossHairPrimitiveProvider()
+    found_ct = 0
+    for _ in range(30):
+        try:
+            with provider.per_test_case_context_manager():
+                s_bool = provider.draw_boolean()
+                s_int = provider.draw_integer()
+                s_float = provider.draw_float()
+                s_str = provider.draw_string(
+                    IntervalSet.from_string("abcdefghijklmnopqrstuvwxyz")
+                )
+                s_bytes = provider.draw_bytes(1, 1)
+                assert type(s_bool) == bool
+                assert type(s_int) == int
+                assert type(s_float) == float
+                assert type(s_str) == str
+                assert type(s_bytes) == bytes
+                _example_user_code(s_bool, s_int, s_float, s_str, s_bytes)
+            # assert provider.completion == "completed normally"
+            assert type(provider.export_value(s_bool)) == bool
+            assert type(provider.export_value(s_int)) == int
+            assert type(provider.export_value(s_float)) == float
+            assert type(provider.export_value(s_str)) == str
+            # NOTE: draw_bytes can raise IgnoreAttempt, which will leave the bytes
+            # symbolic without a concrete value:
+            assert type(provider.export_value(s_bytes)) in (bytes, type(None))
+        except BackendCannotProceed:
+            pass
+        except TargetException:
+            found_ct += 1
+    assert found_ct > 0, "CrossHair could not find the exception"
+
+
+def test_post_run_value_export():
+    provider = CrossHairPrimitiveProvider()
+    with provider.per_test_case_context_manager():
+        s_int = provider.draw_integer()
+        if s_int > 10:
+            pass
+    assert provider.completion == "completed normally"
+    assert type(provider.export_value(s_int)) is int
+    assert type(provider.export_value([s_int])[0]) is int
+
+
+def test_post_run_decisions_do_not_grow_the_search_tree():
+    provider = CrossHairPrimitiveProvider()
+    # There should only be one real branch; so it will take 2 iterations to exhaust
+    for _ in range(2):
+        with provider.per_test_case_context_manager():
+            s_int = provider.draw_integer()
+            if s_int > 10:
+                pass
+        with provider.post_test_case_context_manager():
+            if s_int + 1 > 100:
+                pass
+        assert not provider.exhausted
+        assert provider.completion == "completed normally"
+    provider.bubble_status()
+    assert provider.exhausted
+
+
+def test_export_mid_run_does_not_grow_the_search_tree():
+    provider = CrossHairPrimitiveProvider()
+    # There should only be one real branch; so it will take 2 iterations to exhaust
+    for _ in range(2):
+        with provider.per_test_case_context_manager():
+            s_int = provider.draw_integer()
+            if s_int > 10:
+                pass
+            provider.export_value(s_int)
+        assert not provider.exhausted
+        assert provider.completion == "completed normally"
+    provider.bubble_status()
+    assert provider.exhausted
+
+
+def test_value_export_with_no_decisions():
+    provider = CrossHairPrimitiveProvider()
+    with provider.per_test_case_context_manager():
+        s_int = provider.draw_integer()
+    assert type(provider.export_value(s_int)) is int
+
+
+def test_provider_conformance_crosshair():
+    # Hypothesis can in theory pass values of any type to `realize`,
+    # but the default strategy in the conformance test here acts too much like a
+    # fuzzer for crosshair internals here and finds very strange errors.
+    _realize_objects = (
+        st.integers() | st.floats() | st.booleans() | st.binary() | st.text()
+    )
+    run_conformance_test(
+        CrossHairPrimitiveProvider,
+        context_manager_exceptions=(IgnoreAttempt, UnexploredPath, NotDeterministic),
+        settings=settings(max_examples=5, stateful_step_count=10),
+        _realize_objects=_realize_objects,
+    )
+
+
+@patch("crosshair.statespace.solver_is_sat", side_effect=UnknownSatisfiability)
+def test_unsat_during_realization(solver_is_sat_mock):
+    provider = CrossHairPrimitiveProvider()
+    with provider.per_test_case_context_manager():
+        s_int = provider.draw_integer()
+    with provider.post_test_case_context_manager():
+        with pytest.raises(BackendCannotProceed):
+            provider.export_value(s_int)
+    assert solver_is_sat_mock.call_count == 1
+
+
+@patch("crosshair.statespace.solver_is_sat", side_effect=UnknownSatisfiability)
+def test_unsat_during_user_exception_realization(solver_is_sat_mock):
+    provider = CrossHairPrimitiveProvider()
+    with pytest.raises(BackendCannotProceed):
+        with provider.per_test_case_context_manager():
+            s_int = provider.draw_integer()
+            raise TargetException
+    assert solver_is_sat_mock.call_count == 1
