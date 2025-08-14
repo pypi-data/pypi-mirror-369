408ed189d52c83fef2676905cf06d7bc54d25131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 04:11:20 +0000

    Add package boitrann/0.1.0/boitrann-0.1.0.tar.gz

---
+++ packages/boitrann/boitrann-0.1.0.tar.gz/boitrann-0.1.0/lists/module.py
@@ -0,0 +1,7 @@
+
+
+def grouping(l: list|tuple, n: int) -> list[list|tuple]:
+    if not isinstance(l, (list, tuple)):
+        raise TypeError(f"l must be a list or tuple, instead {type(l).__name__}")
+    
+    return [l[i:i+n] for i in range(0, len(l), n)]
---
+++ packages/boitrann/boitrann-0.1.0.tar.gz/boitrann-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='boitrann',
+    version='0.1.0',
+    packages=find_packages(),
+    description="This is Boi Tran's repository",
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    author='Tran Quoc Dat',
+    author_email='dat.tq6141@gmail.com',
+    # url='https://github.com/yourusername/my_package',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
