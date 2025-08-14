9f95257efd8151ac10796adb64978e6e583baf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:06:15 +0000

    Add package lithic/0.102.0/lithic-0.102.0.tar.gz

---
+++ packages/lithic/lithic-0.102.0.tar.gz/lithic-0.102.0/src/lithic/types/book_transfer_response.py
@@ -0,0 +1,120 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+from .external_resource import ExternalResource
+
+__all__ = ["BookTransferResponse", "Event", "TransactionSeries"]
+
+
+class Event(BaseModel):
+    token: str
+    """Globally unique identifier."""
+
+    amount: int
+    """
+    Amount of the financial event that has been settled in the currency's smallest
+    unit (e.g., cents).
+    """
+
+    created: datetime
+    """Date and time when the financial event occurred. UTC time zone."""
+
+    detailed_results: List[Literal["APPROVED", "FUNDS_INSUFFICIENT"]]
+    """Detailed Results"""
+
+    memo: str
+    """Memo for the transfer."""
+
+    result: Literal["APPROVED", "DECLINED"]
+    """
+    APPROVED financial events were successful while DECLINED financial events were
+    declined by user, Lithic, or the network.
+    """
+
+    subtype: str
+    """The program specific subtype code for the specified category/type."""
+
+    type: str
+    """Type of the book transfer"""
+
+
+class TransactionSeries(BaseModel):
+    related_transaction_event_token: Optional[str] = None
+
+    related_transaction_token: Optional[str] = None
+
+    type: str
+
+
+class BookTransferResponse(BaseModel):
+    token: str
+    """Customer-provided token that will serve as an idempotency token.
+
+    This token will become the transaction token.
+    """
+
+    category: Literal["ADJUSTMENT", "BALANCE_OR_FUNDING", "DERECOGNITION", "DISPUTE", "FEE", "REWARD", "TRANSFER"]
+    """Category of the book transfer"""
+
+    created: datetime
+    """Date and time when the transfer occurred. UTC time zone."""
+
+    currency: str
+    """
+    3-character alphabetic ISO 4217 code for the settling currency of the
+    transaction.
+    """
+
+    events: List[Event]
+    """A list of all financial events that have modified this transfer."""
+
+    external_resource: Optional[ExternalResource] = None
+    """External resource associated with the management operation"""
+
+    from_financial_account_token: str
+    """
+    Globally unique identifier for the financial account or card that will send the
+    funds. Accepted type dependent on the program's use case.
+    """
+
+    pending_amount: int
+    """
+    Pending amount of the transaction in the currency's smallest unit (e.g., cents),
+    including any acquirer fees. The value of this field will go to zero over time
+    once the financial transaction is settled.
+    """
+
+    result: Literal["APPROVED", "DECLINED"]
+    """
+    APPROVED transactions were successful while DECLINED transactions were declined
+    by user, Lithic, or the network.
+    """
+
+    settled_amount: int
+    """
+    Amount of the transaction that has been settled in the currency's smallest unit
+    (e.g., cents).
+    """
+
+    status: Literal["DECLINED", "REVERSED", "SETTLED"]
+    """Status types: \\** `DECLINED` - The transfer was declined.
+
+    - `REVERSED` - The transfer was reversed \\** `SETTLED` - The transfer is
+      completed.
+    """
+
+    to_financial_account_token: object
+    """
+    Globally unique identifier for the financial account or card that will receive
+    the funds. Accepted type dependent on the program's use case.
+    """
+
+    transaction_series: Optional[TransactionSeries] = None
+    """A series of transactions that are grouped together."""
+
+    updated: datetime
+    """Date and time when the financial transaction was last updated. UTC time zone."""
