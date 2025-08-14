8c05ab02012f3d51fe56de2fe3ce63b35b4a2a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 05:05:05 +0000

    Add package mini-swe-agent/1.8.0/mini_swe_agent-1.8.0.tar.gz

---
+++ packages/mini-swe-agent/mini_swe_agent-1.8.0.tar.gz/mini_swe_agent-1.8.0/src/minisweagent/run/extra/swebench_single.py
@@ -0,0 +1,57 @@
+"""Run on a single SWE-Bench instance."""
+
+from pathlib import Path
+
+import typer
+import yaml
+from datasets import load_dataset
+
+from minisweagent.agents.interactive import InteractiveAgent
+from minisweagent.config import builtin_config_dir, get_config_path
+from minisweagent.models import get_model
+from minisweagent.run.extra.swebench import (
+    DATASET_MAPPING,
+    get_sb_environment,
+)
+
+app = typer.Typer(add_completion=False)
+
+
+# fmt: off
+@app.command()
+def main(
+    subset: str = typer.Option("lite", "--subset", help="SWEBench subset to use or path to a dataset", rich_help_panel="Data selection"),
+    split: str = typer.Option("dev", "--split", help="Dataset split", rich_help_panel="Data selection"),
+    instance_spec: str = typer.Option(0, "-i", "--instance", help="SWE-Bench instance ID or index", rich_help_panel="Data selection"),
+    model_name: str | None = typer.Option(None, "-m", "--model", help="Model to use", rich_help_panel="Basic"),
+    config_path: Path = typer.Option( builtin_config_dir / "extra" / "swebench.yaml", "-c", "--config", help="Path to a config file", rich_help_panel="Basic"),
+    environment_class: str | None = typer.Option(None, "--environment-class", rich_help_panel="Advanced"),
+    exit_immediately: bool = typer.Option( False, "--exit-immediately", help="Exit immediately when the agent wants to finish instead of prompting.", rich_help_panel="Basic"),
+) -> None:
+    # fmt: on
+    """Run on a single SWE-Bench instance."""
+    dataset_path = DATASET_MAPPING.get(subset, subset)
+    print(f"Loading dataset from {dataset_path}, split {split}...")
+    instances = {
+        inst["instance_id"]: inst  # type: ignore
+        for inst in load_dataset(dataset_path, split=split)
+    }
+    if instance_spec.isnumeric():
+        instance_spec = sorted(instances.keys())[int(instance_spec)]
+    instance: dict = instances[instance_spec]  # type: ignore
+
+    config = yaml.safe_load(get_config_path(config_path).read_text())
+    config.setdefault("environment", {}).setdefault("environment_class", environment_class)
+    if exit_immediately:
+        config.setdefault("agent", {})["confirm_exit"] = False
+    env = get_sb_environment(config, instance)
+    agent = InteractiveAgent(
+        get_model(model_name, config.get("model", {})),
+        env,
+        **(config.get("agent", {}) | {"mode": "yolo"}),
+    )
+    agent.run(instance["problem_statement"])
+
+
+if __name__ == "__main__":
+    app()
