4170add4b8b69e523bc12cfd0d55d5a9566c94b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:43 +0000

    Add package omdev/0.0.0.dev395/omdev-0.0.0.dev395-py3-none-any.whl

---
+++ packages/omdev/omdev-0.0.0.dev395-py3-none-any.whl/omdev/precheck/scripts.py
@@ -0,0 +1,47 @@
+import dataclasses as dc
+import os
+import stat
+import typing as ta
+
+from .. import magic
+from ..py import findimports
+from .base import Precheck
+from .base import PrecheckContext
+
+
+##
+
+
+class ScriptDepsPrecheck(Precheck['ScriptDepsPrecheck.Config']):
+    @dc.dataclass(frozen=True)
+    class Config(Precheck.Config):
+        pass
+
+    def __init__(
+            self,
+            context: PrecheckContext,
+            config: Config = Config(),
+    ) -> None:
+        super().__init__(config)
+
+        self._context = context
+
+    async def run(self) -> ta.AsyncGenerator[Precheck.Violation]:
+        for fp in sorted(magic.find_magic_files(
+                magic.PY_MAGIC_STYLE,
+                self._context.src_roots,
+                keys=['@omlish-script'],
+        )):
+            if not (stat.S_IXUSR & os.stat(fp).st_mode):
+                yield Precheck.Violation(self, f'script {fp} is not executable')
+
+            with open(fp) as f:  # noqa  # FIXME
+                src = f.read()
+
+            if not src.startswith('#!/usr/bin/env python3\n'):
+                yield Precheck.Violation(self, f'script {fp} lacks correct shebang')
+
+            imps = findimports.find_imports(fp)
+            deps = findimports.get_import_deps(imps)
+            if deps:
+                yield Precheck.Violation(self, f'script {fp} has deps: {deps}')
