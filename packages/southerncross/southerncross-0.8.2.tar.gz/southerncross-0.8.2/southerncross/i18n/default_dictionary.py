7bffc2e8c11c7de335930a43944e350f9ccad53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 16:06:03 +0000

    Add package southerncross/0.8.2/southerncross-0.8.2.tar.gz

---
+++ packages/southerncross/southerncross-0.8.2.tar.gz/southerncross-0.8.2/southerncross/i18n/default_dictionary.py
@@ -0,0 +1,10 @@
+default_dictionary = {
+    "ja": {
+        "this field is required.": "この項目は必須です。",
+        "invalid crontab expression.": "crontabのフォーマットが不正です。",
+        "invalid email address.": "メールアドレスが不正です。",
+        "number must be at least %(min)s.": "最小値は %(min)s です。",
+        "number must be between %(min)s and %(max)s.":
+            "最小値と最大値はそれぞれ %(min)s と %(max)s です。"
+    }
+}
