6e965673420e514786fd800594820d0657dca1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 03:29:55 +0000

    Add package css-for-ctk/0.0.2/css_for_ctk-0.0.2-py3-none-any.whl

---
+++ packages/css-for-ctk/css_for_ctk-0.0.2-py3-none-any.whl/css_for_ctk/example.py
@@ -0,0 +1,21 @@
+import customtkinter
+from css_for_ctk.apply import apply_styles
+
+with open("style.css", "r") as f:
+    css_data = f.read()
+
+@apply_styles(css_data)
+class MyApp(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.button = customtkinter.CTkButton(self)
+        self.entry = customtkinter.CTkEntry(self)
+        self.otherentry = customtkinter.CTkEntry(self)
+
+    def button_callback(self):
+        print("button clicked")
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.mainloop()
+    
