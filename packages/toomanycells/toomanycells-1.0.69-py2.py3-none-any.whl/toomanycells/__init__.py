ad0b13153ae8c981206348cae85b9536773e2e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 07:06:37 +0000

    Add package toomanycells/1.0.69/toomanycells-1.0.69-py2.py3-none-any.whl

---
+++ packages/toomanycells/toomanycells-1.0.69-py2.py3-none-any.whl/toomanycells/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for toomanycells."""
+
+__author__ = """Javier Ruiz-Ramirez"""
+__email__ = "javier.ruizramirez@uhn.ca"
+__version__ = "1.0.69"
+
+from .toomanycells import TooManyCells
+from .common import load_metadata_for_demo
