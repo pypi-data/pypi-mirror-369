9eab75c01b8edd257e6fa360395689130360fe85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 04:11:03 +0000

    Add package nuspacesim/2.1.0/nuspacesim-2.1.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nuspacesim/nuspacesim-2.1.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nuspacesim/apps/__init__.py
@@ -0,0 +1,20 @@
+r"""Command line client apps
+
+.. _apps:
+
+******
+ Apps
+******
+
+.. currentmodule:: nuspacesim.apps
+
+.. autosummary::
+   :toctree:
+
+   cli
+
+"""
+
+__all__ = ["cli", "run", "create_config", "show_plot", "utils"]
+
+from . import cli, create_config, run, show_plot, utils
