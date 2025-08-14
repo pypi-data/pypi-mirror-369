70d14a5675caab94c6d1effdfa20ee67eb82adda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:34 +0000

    Add package neosmartblue.py/0.1.1/neosmartblue_py-0.1.1.tar.gz

---
+++ packages/neosmartblue.py/neosmartblue_py-0.1.1.tar.gz/neosmartblue_py-0.1.1/src/neosmartblue/py/const.py
@@ -0,0 +1,3 @@
+# BLE characteristic UUIDs (Nordic UART Service)
+_RX_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"  # Device receives commands
+_TX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"  # Device transmits notifications
