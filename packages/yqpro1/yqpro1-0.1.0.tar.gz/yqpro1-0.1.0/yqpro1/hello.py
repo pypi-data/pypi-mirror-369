f97f55870bb5b14f5d25cb4839a64325d9883dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:05:00 +0000

    Add package yqpro1/0.1.0/yqpro1-0.1.0.tar.gz

---
+++ packages/yqpro1/yqpro1-0.1.0.tar.gz/yqpro1-0.1.0/yqpro1/hello.py
@@ -0,0 +1,13 @@
+import requests
+import datetime
+from greetsub import greetinner
+
+
+def hello_new_world():
+    print("I am in new world")
+
+
+if __name__ == "__main__":
+    hello_new_world()
+
+
