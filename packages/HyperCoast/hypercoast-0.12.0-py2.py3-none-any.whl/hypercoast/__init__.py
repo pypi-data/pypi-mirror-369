8e120a82d42b003e2e2afcc080d9e6070f844e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 05:05:24 +0000

    Add package HyperCoast/0.12.0/hypercoast-0.12.0-py2.py3-none-any.whl

---
+++ packages/HyperCoast/hypercoast-0.12.0-py2.py3-none-any.whl/hypercoast/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024 Qiusheng Wu <giswqs@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+"""Top-level package for HyperCoast."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = "giswqs@gmail.com"
+__version__ = "0.12.0"
+
+
+from .hypercoast import *
