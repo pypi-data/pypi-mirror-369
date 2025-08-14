d45b26d0b39e8a305acf408b76dde7cc32b7d6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 04:11:38 +0000

    Add package gbrl/1.1.1/gbrl-1.1.1-cp39-cp39-manylinux_2_31_x86_64.whl

---
+++ packages/gbrl/gbrl-1.1.1-cp39-cp39-manylinux_2_31_x86_64.whl/gbrl/src/cpp/node.h
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024, NVIDIA Corporation. All rights reserved.
+//
+// This work is made available under the Nvidia Source Code License-NC.
+// To view a copy of this license, visit
+// https://nvlabs.github.io/gbrl/license.html
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef NODE_HEADER
+#define NODE_HEADER
+
+#include <iostream>
+#include <string>
+#include <cstdint>
+
+#include "split_candidate_generator.h"
+#include "types.h"
+
+class TreeNode {
+    public:
+        TreeNode(int *sample_indices, const int n_samples, const int n_num_features, const int n_cat_features, const int output_dim, const int depth, const int node_idx);
+        ~TreeNode();
+        int splitNode(const float *obs, const char *categorical_obs, const int _node_idx, const splitCandidate &split_candidate);
+        float getSplitScore(dataSet *dataset, const float *feature_weights, scoreFunc split_score_func, const splitCandidate &split_candidate, const int min_data_in_leaf);
+        float splitScoreCosine(const float *obs, const float *feature_weights, const float *grads, const splitCandidate &split_candidate, const int min_data_in_leaf);
+        float splitScoreCosineCategorical(const char *obs, const float *feature_weights, const float *grads, const splitCandidate &split_candidate, const int min_data_in_leaf);
+        float splitScoreL2(const float *obs, const float *feature_weights, const float *grads, const splitCandidate &split_candidate, const int min_data_in_leaf);
+        float splitScoreL2Categorical(const char *obs, const float *feature_weights, const float *grads, const splitCandidate &split_candidate, const int min_data_in_leaf);
+        bool isLeaf() const;
+        static void printTree(TreeNode *node);
+        friend std::ostream& operator<<(std::ostream& os, const TreeNode& obj);
+
+        int *sample_indices = nullptr;
+        int n_samples;
+        int n_num_features;
+        int n_cat_features;
+        int output_dim;
+        int depth;
+        int node_idx;
+
+        float feature_value;
+        int feature_idx;
+        splitCondition *split_conditions = nullptr;
+        
+        TreeNode *left_child = nullptr;
+        TreeNode *right_child = nullptr;
+};
+
+std::ostream& operator<<(std::ostream& os, const TreeNode& obj);
+
+void print_leaf(const int global_leaf_idx, const int leaf_idx, const int tree_idx, const ensembleData *edata, const ensembleMetaData *metadata);
+
+#endif 
---
+++ packages/gbrl/gbrl-1.1.1-cp39-cp39-manylinux_2_31_x86_64.whl/gbrl/src/cpp/utils.h
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024, NVIDIA Corporation. All rights reserved.
+//
+// This work is made available under the Nvidia Source Code License-NC.
+// To view a copy of this license, visit
+// https://nvlabs.github.io/gbrl/license.html
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <omp.h>
+
+#include "types.h"
+
+std::string VectoString(const float* vec, const int vec_size);
+int binaryToDecimal(const BoolVector& binaryPath);
+
+inline int calculate_num_threads(int total_elements, int min_elements_per_thread) {
+    int max_threads = omp_get_max_threads();
+    int n_threads = total_elements / min_elements_per_thread;
+    if (n_threads <= 1) {
+        return 1; // At least one thread
+    } else if (n_threads > max_threads) {
+        return max_threads; // Do not exceed the maximum available threads
+    } else {
+        return n_threads; // Number of threads based on workload
+    }
+}
+
+template<typename T>
+int count_distinct(T *arr, int n);
+
+inline void valid_tree_idx(const int tree_idx, const ensembleMetaData* metadata){
+    if (tree_idx < 0 || tree_idx > metadata->n_trees){
+        std::cerr << "ERROR: invalid tree_idx " << tree_idx << " in ensemble with ntrees = " << metadata->n_trees <<std::endl;
+        throw std::runtime_error("Invalid tree index");
+    }
+}
+
+inline void valid_tree_range(const int start_tree_idx, const int stop_tree_idx, const ensembleMetaData* metadata){
+    if (start_tree_idx < 0 || stop_tree_idx > metadata->n_trees || start_tree_idx >= stop_tree_idx){
+        std::cerr << "ERROR: invalid tree range [" <<  start_tree_idx << ", " << stop_tree_idx << "] in ensemble with ntrees = " << metadata->n_trees <<std::endl;
+        throw std::runtime_error("Invalid tree range");
+    }
+}
+
+
+template<typename T>
+inline T* reallocate_and_copy(T* original, size_t new_n_elements, size_t old_n_elements) {
+    // Allocate new memory block with the given size
+    T* new_block = new T[new_n_elements];
+    // Copy data from the original to the new block
+    memcpy(new_block, original, old_n_elements * sizeof(T));
+    // Delete the original block
+    delete[] original;
+    // Return the new memory block pointer
+    return new_block;
+}
+
+void write_header(std::ofstream& file, const serializationHeader& header);
+serializationHeader create_header();
+serializationHeader read_header(std::ifstream& file);
+
+void display_header(serializationHeader header);
+#endif 
