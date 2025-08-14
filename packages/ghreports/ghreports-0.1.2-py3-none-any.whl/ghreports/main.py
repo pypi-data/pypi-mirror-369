385be391fdf142b3694c9a1698a411f516b9695e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 05:06:11 +0000

    Add package ghreports/0.1.2/ghreports-0.1.2-py3-none-any.whl

---
+++ packages/ghreports/ghreports-0.1.2-py3-none-any.whl/ghreports/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Read information from Github API using GraphQL GitHubAPI.
+"""
+
+from __future__ import annotations
+
+from ghreports.cli import app
+
+
+def main() -> None:
+    app()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
