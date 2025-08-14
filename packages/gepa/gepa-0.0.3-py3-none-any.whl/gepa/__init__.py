d9dffcd85bc9830ba3bbab846a0a4c625dc64b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:05:32 +0000

    Add package gepa/0.0.3/gepa-0.0.3-py3-none-any.whl

---
+++ packages/gepa/gepa-0.0.3-py3-none-any.whl/gepa/__init__.py
@@ -0,0 +1,5 @@
+from .adapters import default_adapter
+from .api import optimize
+from .core.adapter import EvaluationBatch, GEPAAdapter
+from .core.result import GEPAResult
+from .examples import aime
