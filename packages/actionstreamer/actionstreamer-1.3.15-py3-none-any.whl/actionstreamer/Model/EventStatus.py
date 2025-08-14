44d07c435513257d6c71fb13eac78fcceb28b6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:06:01 +0000

    Add package actionstreamer/1.3.15/actionstreamer-1.3.15-py3-none-any.whl

---
+++ packages/actionstreamer/actionstreamer-1.3.15-py3-none-any.whl/actionstreamer/Model/EventStatus.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class EventStatus(Enum):
+
+    Checked_out = 2
+    Complete = 4
+    Error = 5
+    Pending = 1
+    Processing = 3
+    Timed_out = 6
---
+++ packages/actionstreamer/actionstreamer-1.3.15-py3-none-any.whl/actionstreamer/__init__.py
@@ -0,0 +1,7 @@
+from .CommonFunctions import *
+from .WebService import *
+from .Config import *
+from .Device import *
+from .Model import *
+
+__all__ = ['CommonFunctions', 'WebService', 'Config', 'Device', 'Model']
