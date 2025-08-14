41eb3c71300156cf389c56b57f29a12ada34cdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 05:05:20 +0000

    Add package nonebot-plugin-kawaii-robot/4.1.4/nonebot_plugin_kawaii_robot-4.1.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-kawaii-robot/nonebot_plugin_kawaii_robot-4.1.4-py3-none-any.whl/nonebot_plugin_kawaii_robot/__main__.py
@@ -0,0 +1,232 @@
+import asyncio
+import random
+from dataclasses import dataclass, field
+
+from nonebot import logger, on_command
+from nonebot.adapters import Event as BaseEvent, Message as BaseMessage
+from nonebot.permission import SUPERUSER
+from nonebot.plugin.on import on_message, on_notice
+from nonebot.rule import Rule, to_me
+from nonebot_plugin_alconna.uniseg import UniMessage
+from nonebot_plugin_uninfo import GROUP, GUILD, Session, Uninfo
+
+from .config import config
+from .data_source import (
+    LOADED_HELLO_REPLY,
+    LOADED_INTERRUPT_MSG,
+    LOADED_POKE_REPLY,
+    LOADED_REPLY_DICT,
+    LOADED_UNKNOWN_REPLY,
+    clear_replies,
+    reload_replies,
+)
+from .utils import (
+    check_percentage,
+    choice_reply_from_ev,
+    finish_multi_msg,
+    search_reply_dict,
+)
+
+# region 词库回复
+
+
+async def ignore_rule(event: BaseEvent) -> bool:
+    msg = event.get_plaintext().strip()
+
+    # 消息以忽略词开头
+    if next(
+        (x for x in config.leaf_ignore if msg.startswith(x)),
+        None,
+    ):
+        return False
+
+    # at 始终触发
+    if event.is_tome():
+        return True
+
+    # 没 at，启用非 at 回复，并且概率满足
+    return (
+        # (not event.is_tome()) and
+        (not config.leaf_need_at) and check_percentage(config.leaf_trigger_percent)
+    )
+
+
+async def talk_matcher_handler(event: BaseEvent, ss: Uninfo):
+    msg = event.get_plaintext().strip()
+
+    # 如果是光艾特 bot (没消息返回)，就回复以下内容
+    if (not msg) and event.is_tome():
+        await finish_multi_msg(await choice_reply_from_ev(ss, LOADED_HELLO_REPLY))
+
+    # 从词库中获取回复
+    if reply_list := search_reply_dict(LOADED_REPLY_DICT, msg):
+        await finish_multi_msg(await choice_reply_from_ev(ss, reply_list))
+
+    # 不明白的内容，开启所有回复并 @bot 才会回复
+    if event.is_tome() and config.leaf_reply_type == 1:
+        await finish_multi_msg(await choice_reply_from_ev(ss, LOADED_UNKNOWN_REPLY))
+
+
+if config.leaf_reply_type >= 0:
+    DICT_REPLY_PERM = GROUP | GUILD if config.leaf_permission == "GROUP" else None
+    talk = on_message(
+        rule=Rule(ignore_rule) & (to_me() if config.leaf_need_at else None),
+        permission=DICT_REPLY_PERM,
+        priority=99,
+        block=False,
+    )
+    talk.handle()(talk_matcher_handler)
+
+
+# endregion
+
+
+# region 戳一戳
+
+try:
+    from nonebot.adapters.onebot.v11 import (
+        Bot as OBV11Bot,
+        MessageSegment as OBV11Seg,
+        PokeNotifyEvent,
+    )
+except ImportError:
+    pass
+else:
+
+    async def send_poke(bot: OBV11Bot, event: PokeNotifyEvent):
+        if config.is_lagrange:
+            if event.group_id:
+                await bot.group_poke(group_id=event.group_id, user_id=event.user_id)
+            else:
+                await bot.friend_poke(user_id=event.user_id)
+        else:
+            await bot.send(event, OBV11Seg("poke", {"qq": event.user_id}))
+
+    async def poke_matcher_handler(bot: OBV11Bot, event: PokeNotifyEvent, ss: Uninfo):
+        await asyncio.sleep(random.uniform(*config.leaf_poke_action_delay))
+
+        if check_percentage(config.leaf_poke_rand):
+            await finish_multi_msg(
+                await choice_reply_from_ev(ss, LOADED_POKE_REPLY),
+            )
+
+        await send_poke(bot, event)
+
+    if config.leaf_poke_rand >= 0:
+        poke_matcher = on_notice(rule=to_me(), priority=10, block=False)
+        poke_matcher.handle()(poke_matcher_handler)
+
+
+# endregion
+
+
+# region 打断/复读姬
+
+repeat_infos: dict[str, "RepeatInfo"] = {}
+
+
+def random_repeat_limit():
+    return random.randint(*config.leaf_repeater_limit)
+
+
+@dataclass
+class RepeatInfo:
+    limit: int = field(default_factory=random_repeat_limit)  # 复读次数上限
+    last_msg: str | None = None  # 正在复读的消息
+    repeated: int = 0  # 已经复读过的次数
+    users: set[str] = field(default_factory=set)  # 参与复读的用户
+
+    @classmethod
+    def get(cls, scene_id: str) -> "RepeatInfo":
+        if scene_id not in repeat_infos:
+            repeat_infos[scene_id] = (x := cls())
+            return x
+        return repeat_infos[scene_id]
+
+    def count(self, user_id: str, message: str):
+        if self.last_msg != message:
+            self.last_msg = message
+            self.repeated = 1
+            self.users.clear()
+            return False
+
+        if self.repeated == 0:
+            # 在 Bot 复读 或 打断复读 后仍在复读
+            # Bot 复读后只把 repeated 设为了 0，没有把 last_msg 清空，
+            # 代表 interrupt / repeat continue 为 False
+            return False
+
+        if config.leaf_force_different_user:
+            if user_id in self.users:
+                return False
+            self.users.add(user_id)
+
+        self.repeated += 1
+        if self.repeated >= self.limit:
+            self.limit = random_repeat_limit()
+            self.repeated = 0
+            self.users.clear()
+            return True
+
+        return False
+
+    async def do_interrupt(self, ss: Session):  # noqa: ARG002
+        if config.leaf_interrupt_continue:
+            self.last_msg = None
+        await finish_multi_msg(await choice_reply_from_ev(ss, LOADED_INTERRUPT_MSG))
+
+    async def do_repeat(self, msg: BaseMessage):
+        if config.leaf_repeat_continue:
+            self.last_msg = None
+        await (await UniMessage.of(msg).attach_reply()).send()
+
+
+async def repeat_rule(event: BaseEvent, ss: Uninfo) -> bool:
+    try:
+        raw = event.get_message()
+    except ValueError:
+        return False
+    msg = repr(UniMessage.of(raw))
+    return RepeatInfo.get(ss.scene_path).count(event.get_user_id(), msg)
+
+
+async def repeater_matcher_handler(event: BaseEvent, ss: Uninfo):
+    info = RepeatInfo.get(ss.id)
+    if check_percentage(config.leaf_interrupt):
+        await info.do_interrupt(ss)
+    else:
+        await info.do_repeat(event.get_message())
+
+
+if config.leaf_interrupt >= 0:
+    repeater = on_message(
+        rule=repeat_rule,
+        permission=GROUP | GUILD,
+        priority=99,
+        block=False,
+    )
+    repeater.handle()(repeater_matcher_handler)
+
+# endregion
+
+
+# region 重载
+
+
+async def reload_replies_handler():
+    try:
+        await reload_replies()
+    except Exception:
+        clear_replies()
+        logger.exception("Error when reload replies")
+        await UniMessage.text("重载失败了呜喵，看起来咱被玩坏了喵").send()
+    else:
+        await UniMessage.text("重载回复词库完成了喵~").send()
+
+
+if config.leaf_register_reload_command:
+    matcher_reload_data = on_command("重载词库", permission=SUPERUSER)
+    matcher_reload_data.handle()(reload_replies_handler)
+
+
+# endregion
---
+++ packages/nonebot-plugin-kawaii-robot/nonebot_plugin_kawaii_robot-4.1.4-py3-none-any.whl/nonebot_plugin_kawaii_robot/config.py
@@ -0,0 +1,101 @@
+from collections.abc import Iterable
+from typing import Any, Literal
+
+from cookit.pyd import field_validator
+from nonebot import get_plugin_config
+from pydantic import BaseModel, Field
+
+ReplyPermType = Literal["ALL", "GROUP"]
+
+
+class ConfigModel(BaseModel):
+    nickname: set[str]
+
+    is_lagrange: bool = False
+
+    leaf_permission: ReplyPermType = "ALL"
+    """词库回复权限，`ALL` 就是全部聊天都会触发回复，`GROUP` 就是仅群聊"""
+
+    leaf_ignore: set[str] = set()
+    """忽略词，指令以本 Set 中的元素开头不会触发词库回复"""
+
+    leaf_reply_type: Literal[-1, 0, 1] = 1
+    """回复模式，`-1` 关闭全部 at 回复，`0` 仅启用词库回复，`1` 开启所有回复"""
+
+    leaf_poke_rand: int = Field(20, ge=-1, le=100)
+    """戳一戳回复文字概率，范围 `0` ~ `100`，`-1` 关闭戳一戳回复"""
+
+    leaf_force_different_user: bool = True
+    """复读、打断复读时是否按复读的用户数计算次数"""
+
+    leaf_repeater_limit: tuple[int, int] = (2, 6)
+    """触发复读或打断次数，群内复读 `{0}` ~ `{1}` 次数后触发复读或打断"""
+
+    leaf_repeat_continue: bool = False
+    """复读后，群友继续复读达到指定次数时是否继续参与复读或打断"""
+
+    leaf_interrupt: int = Field(20, ge=0, le=100)
+    """打断复读概率，范围 `0` ~ `100`"""
+
+    leaf_interrupt_continue: bool = True
+    """打断复读后，群友继续复读达到指定次数时是否继续参与复读或打断"""
+
+    leaf_match_pattern: Literal[0, 1, 2] = 1
+    """
+    词库回复匹配模式，
+    `0` 是整句话精确匹配关键词（不推荐）；
+    `1` 是按从长到短的顺序遍历词库中的关键词，遇到匹配的就停止遍历并选取对应回复；
+    `2` 与 `1` 的遍历方式相同，但是会遍历所有词库中的关键词，然后从匹配到的所有项目中随机选取一个回复
+    """
+
+    leaf_search_max: int = 20
+    """当 `LEAF_MATCH_PATTERN` >= 1 时，消息长度大于此数值则不从词库中匹配回复，设为 `0` 则禁用此功能"""
+
+    leaf_need_at: bool = True
+    """词库回复是否需要 `to_me`"""
+
+    leaf_trigger_percent: int = Field(5, ge=0, le=100)
+    """词库回复非 `to_me` 时的触发概率，范围 `0` ~ `100`"""
+
+    leaf_poke_action_delay: tuple[float, float] = (0.5, 1.5)
+    """戳一戳回复延时，单位秒"""
+
+    leaf_multi_reply_delay: tuple[float, float] = (1.0, 3.0)
+    """当回复存在多条消息时，发送消息的间隔时间，单位秒"""
+
+    leaf_load_builtin_dict: bool = True
+    """是否载入内置回复词库"""
+
+    leaf_load_builtin_special: bool = True
+    """是否载入内置特殊回复词库"""
+
+    leaf_register_reload_command: bool = True
+    """是否注册 重载词库 指令"""
+
+    @field_validator(
+        "leaf_repeater_limit",
+        "leaf_poke_action_delay",
+        "leaf_multi_reply_delay",
+        mode="before",
+    )
+    def check_interval(cls, v: Any):  # noqa: N805
+        if isinstance(v, int | float):
+            v = (v, v)
+        else:
+            if not isinstance(v, Iterable):
+                raise TypeError("区间必须是 int, float 或 Iterable")
+            v = tuple(v)
+            if len(v) != 2:
+                raise ValueError("区间长度必须为 2")
+            if v[0] > v[1]:
+                raise ValueError("区间左边界必须小于或等于右边界")
+        return v
+
+    @field_validator("leaf_repeater_limit")
+    def check_repeater_limit(cls, v: tuple[int, int]):  # noqa: N805
+        if v[0] < 2:
+            raise ValueError("触发复读或打断次数左边界必须大于 2")
+        return v
+
+
+config = get_plugin_config(ConfigModel)
