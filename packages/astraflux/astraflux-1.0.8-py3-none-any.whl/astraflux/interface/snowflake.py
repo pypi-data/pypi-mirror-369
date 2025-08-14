e4fd27a4b41b813442426c83da858d1443d6594c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:27 +0000

    Add package astraflux/1.0.8/astraflux-1.0.8-py3-none-any.whl

---
+++ packages/astraflux/astraflux-1.0.8-py3-none-any.whl/astraflux/interface/snowflake.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+__all__ = ['snowflake_id']
+
+
+def snowflake_id() -> str:
+    """
+    Returns a Snowflake ID generator function.
+    Returns:
+        function: A function that generates Snowflake IDs.
+    """
+    return snowflake_id()
