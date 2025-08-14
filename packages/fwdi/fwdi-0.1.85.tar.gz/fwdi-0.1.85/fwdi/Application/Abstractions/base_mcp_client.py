b101f3d3c62848314cbc5c552c800b23b7b57124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:46 +0000

    Add package fwdi/0.1.85/fwdi-0.1.85.tar.gz

---
+++ packages/fwdi/fwdi-0.1.85.tar.gz/fwdi-0.1.85/fwdi/Application/Abstractions/base_mcp_client.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from mcp.types import CallToolResult, GetPromptResult, TextResourceContents, BlobResourceContents
+
+class BaseMCPClient(ABC):
+    def __init__(self):
+        super().__init__()
+        self.tools:list[Tool] = []
+        self.prompts:list[Prompt] = []
+        self.resources:list[Resource] = []
+
+    @classmethod
+    async def create(cls, host:str='127.0.0.1', port:int=5000, path:str='/mcp'):
+        ...
+
+    @abstractmethod
+    async def check_avaible(self):
+        ...
+    
+    @abstractmethod
+    async def load_env(self)->bool:
+        ...
+
+    @abstractmethod
+    async def call_tool(self, name_fn:str, param:dict)->CallToolResult:
+        ...
+
+    @abstractmethod
+    async def read_resource(self, uri:str)->list[TextResourceContents | BlobResourceContents]:
+        ...
+
+    @abstractmethod
+    async def get_prompt(self, name:str, param:dict)->GetPromptResult:
+        ...
