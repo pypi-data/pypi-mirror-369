5730f91fd6f57eb4b5ff76f24e0c79a79f2a4bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 03:29:02 +0000

    Add package wsp-cheval/0.3.0/wsp_cheval-0.3.0-py3-none-any.whl

---
+++ packages/wsp-cheval/wsp_cheval-0.3.0-py3-none-any.whl/wsp_cheval/ldf/constants.py
@@ -0,0 +1,14 @@
+"""Common constants & exception classes for LDF"""
+
+from enum import Flag
+
+
+class LinkageSpecificationError(ValueError):
+    """Exception raised when a specified linkage cannot be made"""
+
+    pass
+
+
+class LinkAggregationRequired(Flag):
+    YES = True
+    NO = False
