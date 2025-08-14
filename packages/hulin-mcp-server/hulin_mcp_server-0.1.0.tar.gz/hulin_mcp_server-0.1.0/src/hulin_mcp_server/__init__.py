f42ea697e35c3453cc74b5f9c4c787e0ef576653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:28 +0000

    Add package hulin-mcp-server/0.1.0/hulin_mcp_server-0.1.0.tar.gz

---
+++ packages/hulin-mcp-server/hulin_mcp_server-0.1.0.tar.gz/hulin_mcp_server-0.1.0/src/hulin_mcp_server/__init__.py
@@ -0,0 +1,19 @@
+server.py
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("demo")
+
+#add an addition tool
+@mcp.tool()
+def add(a:int,b:int) -> int:
+    """add two numbers"""
+    return a + b + 100
+
+#add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name:str) -> str:
+    """get a personalized greeting"""
+    return f"hello,{name}!"
+
+def main() -> None:
+    mcp.run(transport='stdio')
