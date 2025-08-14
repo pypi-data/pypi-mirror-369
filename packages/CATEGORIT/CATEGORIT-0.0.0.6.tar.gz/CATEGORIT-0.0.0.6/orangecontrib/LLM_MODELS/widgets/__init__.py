57c71a2107e4d995f074305ff4dc99a592544cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 03:29:55 +0000

    Add package CATEGORIT/0.0.0.6/CATEGORIT-0.0.0.6.tar.gz

---
+++ packages/CATEGORIT/CATEGORIT-0.0.0.6.tar.gz/CATEGORIT-0.0.0.6/orangecontrib/LLM_MODELS/widgets/__init__.py
@@ -0,0 +1,19 @@
+"""
+Widgets from Development workflow category
+
+"""
+# ID = "orangecontrib.AAIT"
+
+NAME = "AAIT - LLM MODELS"
+
+# Category icon show in the menu
+ICON = "icons/models.png"
+
+BACKGROUND = "light-purple"
+
+DESCRIPTION = ("AAIT - Algorithm is a package meant to develop and enable advanced AI "
+               "functionalities in Orange Data Mining.")
+
+
+
+# PRIORITY = 6
