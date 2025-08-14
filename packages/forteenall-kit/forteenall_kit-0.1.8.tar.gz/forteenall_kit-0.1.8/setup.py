24c01f1f258e78f4d50f3ebf9ec38f116b329c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:05:08 +0000

    Add package forteenall-kit/0.1.8/forteenall_kit-0.1.8.tar.gz

---
+++ packages/forteenall-kit/forteenall_kit-0.1.8.tar.gz/forteenall_kit-0.1.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="forteenall_kit",
+version='0.1.8',
+    author="Mahdi Nouri",
+    author_email="algo.mahdi.nouri@gmail.com",
+    description="all agents and AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/algonouir/forteenall_kit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
