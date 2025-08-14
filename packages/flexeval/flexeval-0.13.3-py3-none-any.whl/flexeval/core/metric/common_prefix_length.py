de4592128e24c30bf9789ad207724e18d5de4710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:36 +0000

    Add package flexeval/0.13.3/flexeval-0.13.3-py3-none-any.whl

---
+++ packages/flexeval/flexeval-0.13.3-py3-none-any.whl/flexeval/core/metric/common_prefix_length.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from .base import Metric, MetricResult
+from .utils import validate_inputs
+
+
+def get_longest_common_prefix(s1: str, s2: str) -> str:
+    """Find the longest common prefix between two strings."""
+    length = min(len(s1), len(s2))
+
+    for i in range(length):
+        if s1[i] != s2[i]:
+            return s1[:i]
+    return s1[:length]
+
+
+class CommonPrefixLength(Metric):
+    """
+    A metric that calculates the length of the longest common prefix between the model output and the reference.
+
+    Examples:
+        >>> from flexeval import CommonPrefixLength
+        >>> common_prefix_length = CommonPrefixLength()
+        >>> lm_outputs = ["ABCDEFG"]
+        >>> references_list = [["ABCdefg"]]
+        >>> result = common_prefix_length.evaluate(lm_outputs, references_list)
+        >>> print(result)
+        MetricResult(
+            summary={"average_common_prefix_length": 3.0, "longest_common_prefix_length": 3},
+            instance_details=[{"common_prefix_length": 3}],
+        )
+    """
+
+    def evaluate(
+        self,
+        lm_outputs: list[str],
+        references_list: list[list[str]],
+        extra_info_list: list[dict[str, str]] | None = None,
+    ) -> MetricResult:
+        validate_inputs(lm_outputs, references_list, extra_info_list)
+
+        # Compute metrics
+        common_prefix_length_list: list[int] = []
+        for lm_output, references in zip(lm_outputs, references_list):
+            common_prefix_length = max(len(get_longest_common_prefix(lm_output, gt)) for gt in references)
+            common_prefix_length_list.append(common_prefix_length)
+
+        return MetricResult(
+            {
+                "average_common_prefix_length": sum(common_prefix_length_list) / len(common_prefix_length_list),
+                "longest_common_prefix_length": max(common_prefix_length_list),
+            },
+            instance_details=[{"common_prefix_length": s} for s in common_prefix_length_list],
+        )
---
+++ packages/flexeval/flexeval-0.13.3-py3-none-any.whl/flexeval/core/metric/output_length_stats.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from .base import Metric, MetricResult
+
+
+class OutputLengthStats(Metric):
+    """
+    Compute statistics on the length of the outputs.
+
+    Examples:
+        >>> from flexeval import OutputLengthStats
+        >>> output_length_stats = OutputLengthStats()
+        >>> lm_outputs = ["123456", "123456789"]
+        >>> result = output_length_stats.evaluate(lm_outputs)
+        >>> print(result)
+        MetricResult(
+            summary={'avg_output_length': 7.5, 'max_output_length': 9, 'min_output_length': 6},
+            instance_details=[{'output_length': 6}, {'output_length': 9}]
+        )
+    """
+
+    def evaluate(
+        self,
+        lm_outputs: list[str],
+        references_list: list[list[str]] | None = None,
+        extra_info_list: list[dict[str, str]] | None = None,
+    ) -> MetricResult:
+        # Compute metrics
+        output_length_list = [len(output) for output in lm_outputs]
+        return MetricResult(
+            {
+                "avg_output_length": sum(output_length_list) / len(output_length_list),
+                "max_output_length": max(output_length_list),
+                "min_output_length": min(output_length_list),
+            },
+            instance_details=[{"output_length": s} for s in output_length_list],
+        )
