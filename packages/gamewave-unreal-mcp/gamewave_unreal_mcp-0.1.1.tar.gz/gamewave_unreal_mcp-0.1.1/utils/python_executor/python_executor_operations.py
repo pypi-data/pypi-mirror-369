bcf30b87b824c5434ef8b2271ceecf5bef56ae2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 05:05:22 +0000

    Add package gamewave-unreal-mcp/0.1.1/gamewave_unreal_mcp-0.1.1.tar.gz

---
+++ packages/gamewave-unreal-mcp/gamewave_unreal_mcp-0.1.1.tar.gz/gamewave_unreal_mcp-0.1.1/utils/python_executor/python_executor_operations.py
@@ -0,0 +1,24 @@
+"""
+Blueprint Action Operations for Unreal MCP.
+
+This module provides utility functions for discovering Blueprint actions using the FBlueprintActionDatabase.
+"""
+
+import logging
+import json
+from typing import Dict, Any, List
+from mcp.server.fastmcp import Context
+from utils.unreal_connection_utils import send_unreal_command
+
+# Get logger
+logger = logging.getLogger("UnrealMCP")
+
+def execute_python_code(
+    ctx: Context,
+    code: str
+) -> Dict[str, Any]:
+    """Implementation for executin python code in unreal engine."""
+    params = {
+        "code": code
+    }
+    return send_unreal_command("execute_python_code", params)
