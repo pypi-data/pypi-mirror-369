70355ee30016b011bbe50b114c49d8c19a6f24a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:17 +0000

    Add package samplestring/0.1.0/samplestring-0.1.0.tar.gz

---
+++ packages/samplestring/samplestring-0.1.0.tar.gz/samplestring-0.1.0/src/samplestring/__init__.py
@@ -0,0 +1,6 @@
+#def main() -> None:
+    #print("Hello from mystring-utils!")
+
+from .stringutil import reverse_string, count_words, count_vowels, max_char_count
+
+__all__ = ['reverse_string', 'max_char_count', 'count_words', 'count_vowels']
