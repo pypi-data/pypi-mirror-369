0f40df13c1a1b1e0ef661527531197a7b8840923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 04:11:46 +0000

    Add package universal-publisher/1.1.2/universal_publisher-1.1.2.tar.gz

---
+++ packages/universal-publisher/universal_publisher-1.1.2.tar.gz/universal_publisher-1.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="universal-publisher",
+    version="1.1.2",
+    author="Akshay",
+    description="A universal ROS2 publisher for text, numbers, booleans, images, videos, and multi-float arrays.",
+    packages=find_packages(),
+    install_requires=[
+        "cv_bridge",
+        "opencv-python"
+    ],
+    python_requires=">=3.6",
+)
