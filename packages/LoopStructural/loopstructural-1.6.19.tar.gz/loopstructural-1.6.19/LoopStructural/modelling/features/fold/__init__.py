f5094f68d1ccdd858a107278f3db9993ada44752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 15:05:54 +0000

    Add package LoopStructural/1.6.19/loopstructural-1.6.19.tar.gz

---
+++ packages/LoopStructural/loopstructural-1.6.19.tar.gz/loopstructural-1.6.19/LoopStructural/modelling/features/fold/__init__.py
@@ -0,0 +1,7 @@
+""" """
+
+from ._fold import FoldEvent
+from ._svariogram import SVariogram
+from ._fold_rotation_angle_feature import FoldRotationAngleFeature
+from ._foldframe import FoldFrame
+from ._fold import FoldEvent
