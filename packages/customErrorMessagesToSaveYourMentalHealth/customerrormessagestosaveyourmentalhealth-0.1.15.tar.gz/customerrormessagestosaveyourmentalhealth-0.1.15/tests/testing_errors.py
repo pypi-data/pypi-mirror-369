cf4e05e278967b1c3cdd58d892fe8e0789b3d61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:47 +0000

    Add package customErrorMessagesToSaveYourMentalHealth/0.1.15/customerrormessagestosaveyourmentalhealth-0.1.15.tar.gz

---
+++ packages/customErrorMessagesToSaveYourMentalHealth/customerrormessagestosaveyourmentalhealth-0.1.15.tar.gz/customerrormessagestosaveyourmentalhealth-0.1.15/tests/testing_errors.py
@@ -0,0 +1,16 @@
+# testing_errors.py (in the tests folder)
+import sys
+import os
+
+# Add the parent directory (custom_error_msg) to Python path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parent_dir)
+
+from customErrorMessagesToSaveYourMentalHealth import error_handler
+
+sys.excepthook = lambda exc_type, exc_value, exc_tb: error_handler(
+    exc_type, exc_value, exc_tb, theme_name="motivate me"
+)
+
+# Trigger an error
+1/0
