5d902b88f70967d5a3922fedc07dc0bae44ec563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 05:05:05 +0000

    Add package gbrl/1.1.1/gbrl-1.1.1-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/gbrl/gbrl-1.1.1-cp310-cp310-macosx_14_0_arm64.whl/gbrl/src/cpp/config.h
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024, NVIDIA Corporation. All rights reserved.
+//
+// This work is made available under the Nvidia Source Code License-NC.
+// To view a copy of this license, visit
+// https://nvlabs.github.io/gbrl/license.html
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef VERSION_CONFIG_H
+#define VERSION_CONFIG_H
+
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 1
+#define PATCH_VERSION 1
+
+#endif // VERSION_CONFIG_H
