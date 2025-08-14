22119313e8c9c516ddb5ccb9b8750f7840602e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:55 +0000

    Add package unboil-sqlalchemy-types/0.1.4/unboil_sqlalchemy_types-0.1.4-py3-none-any.whl

---
+++ packages/unboil-sqlalchemy-types/unboil_sqlalchemy_types-0.1.4-py3-none-any.whl/unboil_sqlalchemy_types/__init__.py
@@ -0,0 +1,30 @@
+from typing import Any
+from sqlalchemy import Dialect, TypeDecorator, JSON
+
+
+class PydanticJSON(TypeDecorator):
+    
+    impl = JSON
+    cache_ok = True  # Performance hint
+
+    def __init__(self, pydantic_type: type[Any]):
+        from pydantic import TypeAdapter
+        super().__init__()
+        self.pydantic_type = pydantic_type
+        self.adapter = TypeAdapter(pydantic_type)
+
+    def process_bind_param(self, value: Any, dialect: Dialect):
+        if isinstance(value, self.pydantic_type):
+            return self.adapter.dump_python(value)
+        elif isinstance(value, dict):
+            return value  # Already a dict, possibly from a deserialized source
+        raise TypeError(f"Expected {self.pydantic_type} or dict, got {type(value)}")
+
+    def process_result_value(self, value: Any, dialect: Dialect):
+        if value is None:
+            return None
+        return self.adapter.validate_python(value)
+        
+    # for alembic
+    def __repr__(self) -> str:
+        return f"JSON()"
