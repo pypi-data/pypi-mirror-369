5203d1ca0551166c82fd79d2c59d0960b67e144c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:05:03 +0000

    Add package visagene/0.3.10/visagene-0.3.10-cp311-cp311-win_amd64.whl

---
+++ packages/visagene/visagene-0.3.10-cp311-cp311-win_amd64.whl/visagene/__init__.py
@@ -0,0 +1,28 @@
+from .detection import BaseFaceDetection, OnnxFaceDetection, TrtFaceDetection
+from .embedding import BaseFaceEmbedding, OnnxFaceEmbedding, TrtFaceEmbedding
+from .enhance import BaseFaceEnhance, OnnxFaceEnhance, TrtFaceEnhance
+from .paste import PasteBack, paste_back
+from .schema import VisageneFace
+from .segmentation import BaseFaceSegmentation, OnnxFaceSegmentation, TrtFaceSegmentation
+from .swap import BaseFaceSwap, OnnxFaceSwap, TrtFaceSwap
+
+__all__ = [
+    "OnnxFaceDetection",
+    "TrtFaceDetection",
+    "OnnxFaceEnhance",
+    "TrtFaceEnhance",
+    "OnnxFaceEmbedding",
+    "TrtFaceEmbedding",
+    "PasteBack",
+    "paste_back",
+    "VisageneFace",
+    "OnnxFaceSegmentation",
+    "TrtFaceSegmentation",
+    "OnnxFaceSwap",
+    "TrtFaceSwap",
+    "BaseFaceDetection",
+    "BaseFaceEnhance",
+    "BaseFaceEmbedding",
+    "BaseFaceSegmentation",
+    "BaseFaceSwap",
+]
