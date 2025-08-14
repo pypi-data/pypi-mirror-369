59facdcadaedd7a24dd9a773a6a4bd3c86ed577b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:06:09 +0000

    Add package test-publish-proj-with-uv/0.1.0/test_publish_proj_with_uv-0.1.0.tar.gz

---
+++ packages/test-publish-proj-with-uv/test_publish_proj_with_uv-0.1.0.tar.gz/test_publish_proj_with_uv-0.1.0/test_publish_pkg/test_publish.py
@@ -0,0 +1,10 @@
+import requests
+import dotenv
+from dotenv import load_dotenv
+from datetime import datetime
+
+def greet_yangq():
+    print("hello yangq")
+
+if __name__ == "__main__":
+    greet_yangq()
