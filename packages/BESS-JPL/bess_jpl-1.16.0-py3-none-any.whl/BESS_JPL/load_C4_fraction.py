2aaf0c4811670f2832c059908a48fac734e4b8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:05:18 +0000

    Add package BESS-JPL/1.16.0/bess_jpl-1.16.0-py3-none-any.whl

---
+++ packages/BESS-JPL/bess_jpl-1.16.0-py3-none-any.whl/BESS_JPL/load_C4_fraction.py
@@ -0,0 +1,14 @@
+from os.path import join, abspath, dirname
+
+import rasters as rt
+from rasters import Raster, RasterGeometry
+
+import numpy as np
+
+def load_C4_fraction(geometry: RasterGeometry = None, resampling: str = "nearest") -> Raster:
+    filename = join(abspath(dirname(__file__)), "C4_fraction.tif")
+    image = Raster.open(filename, geometry=geometry, resampling=resampling, nodata=np.nan)
+    image = rt.clip(image, 0, 100)
+    # TODO: scale C4_fraction to be within 0 and 1?
+
+    return image
