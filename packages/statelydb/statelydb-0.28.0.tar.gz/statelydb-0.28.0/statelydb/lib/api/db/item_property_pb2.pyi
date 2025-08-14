fd1fbd49fe86721c5fffbda868fdca345ce8862f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:06:07 +0000

    Add package statelydb/0.28.0/statelydb-0.28.0.tar.gz

---
+++ packages/statelydb/statelydb-0.28.0.tar.gz/statelydb-0.28.0/statelydb/lib/api/db/item_property_pb2.pyi
@@ -0,0 +1,20 @@
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class SortableProperty(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    SORTABLE_PROPERTY_KEY_PATH: _ClassVar[SortableProperty]
+    SORTABLE_PROPERTY_LAST_MODIFIED_VERSION: _ClassVar[SortableProperty]
+    SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_1: _ClassVar[SortableProperty]
+    SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_2: _ClassVar[SortableProperty]
+    SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_3: _ClassVar[SortableProperty]
+    SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_4: _ClassVar[SortableProperty]
+SORTABLE_PROPERTY_KEY_PATH: SortableProperty
+SORTABLE_PROPERTY_LAST_MODIFIED_VERSION: SortableProperty
+SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_1: SortableProperty
+SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_2: SortableProperty
+SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_3: SortableProperty
+SORTABLE_PROPERTY_GROUP_LOCAL_INDEX_4: SortableProperty
