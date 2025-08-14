695b16841d0cb55c647e0e54b40d3404a1ace1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:06:11 +0000

    Add package greetsub/0.1.2/greetsub-0.1.2-py3-none-any.whl

---
+++ packages/greetsub/greetsub-0.1.2-py3-none-any.whl/greetsub/greetinner.py
@@ -0,0 +1,18 @@
+import requests
+from datetime import datetime
+import dotenv
+
+def greet_yangq():
+    print("inner, hello yang qiang")
+
+def hello_yangq():
+    print("inner, hello, yang qiang")
+
+def hello_world():
+    print("inner, hello world")
+
+
+if __name__ == "__main__":
+    hello_yangq()
+
+
