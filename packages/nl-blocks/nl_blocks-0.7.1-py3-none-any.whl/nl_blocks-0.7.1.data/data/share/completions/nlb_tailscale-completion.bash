0a681182b573453135aa43824521caa649e812e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 07:06:29 +0000

    Add package nl-blocks/0.7.1/nl_blocks-0.7.1-py3-none-any.whl

---
+++ packages/nl-blocks/nl_blocks-0.7.1-py3-none-any.whl/nl_blocks-0.7.1.data/data/share/completions/nlb_tailscale-completion.bash
@@ -0,0 +1,29 @@
+_nlb_tailscale_completion() {
+    local IFS=$'\n'
+    local response
+
+    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _NLB_TAILSCALE_COMPLETE=bash_complete $1)
+
+    for completion in $response; do
+        IFS=',' read type value <<< "$completion"
+
+        if [[ $type == 'dir' ]]; then
+            COMPREPLY=()
+            compopt -o dirnames
+        elif [[ $type == 'file' ]]; then
+            COMPREPLY=()
+            compopt -o default
+        elif [[ $type == 'plain' ]]; then
+            COMPREPLY+=($value)
+        fi
+    done
+
+    return 0
+}
+
+_nlb_tailscale_completion_setup() {
+    complete -o nosort -F _nlb_tailscale_completion nlb_tailscale
+}
+
+_nlb_tailscale_completion_setup;
+
---
+++ packages/nl-blocks/nl_blocks-0.7.1-py3-none-any.whl/nl_blocks-0.7.1.data/data/share/completions/venv_completions-completion.bash
@@ -0,0 +1,29 @@
+_venv_completions_completion() {
+    local IFS=$'\n'
+    local response
+
+    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _VENV_COMPLETIONS_COMPLETE=bash_complete $1)
+
+    for completion in $response; do
+        IFS=',' read type value <<< "$completion"
+
+        if [[ $type == 'dir' ]]; then
+            COMPREPLY=()
+            compopt -o dirnames
+        elif [[ $type == 'file' ]]; then
+            COMPREPLY=()
+            compopt -o default
+        elif [[ $type == 'plain' ]]; then
+            COMPREPLY+=($value)
+        fi
+    done
+
+    return 0
+}
+
+_venv_completions_completion_setup() {
+    complete -o nosort -F _venv_completions_completion venv_completions
+}
+
+_venv_completions_completion_setup;
+
