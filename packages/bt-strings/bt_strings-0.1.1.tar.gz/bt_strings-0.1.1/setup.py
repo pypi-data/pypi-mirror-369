032900440fee9be8c9ff8950eee79a5741721354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 04:11:54 +0000

    Add package bt-strings/0.1.1/bt_strings-0.1.1.tar.gz

---
+++ packages/bt-strings/bt_strings-0.1.1.tar.gz/bt_strings-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bt_strings',
+    version='0.1.1',
+    packages=find_packages(),
+    # description='Một mô tả ngắn về package của bạn',
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    author='Tran Quoc Dat',
+    author_email='dat.tq6141@gmail.com',
+    # url='https://github.com/yourusername/my_package',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
