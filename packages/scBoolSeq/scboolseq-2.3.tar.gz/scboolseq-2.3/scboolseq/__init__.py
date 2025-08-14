4a07a3c73c523292a50fed5477b751527888e672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:05:00 +0000

    Add package scBoolSeq/2.3/scboolseq-2.3.tar.gz

---
+++ packages/scBoolSeq/scboolseq-2.3.tar.gz/scboolseq-2.3/scboolseq/__init__.py
@@ -0,0 +1,17 @@
+"""
+scBoolSeq: Linking scRNA-Seq with Boolean Dynamics
+
+author: "Gustavo Magaña López"
+credits: "BNediction ; Institut Curie"
+"""
+
+__version__ = "0.2.0"
+__author__ = "Gustavo Magaña López"
+__credits__ = "BNeDiction; Institut Curie"
+
+# Packages
+from . import binarization
+from . import meta
+
+# Main Class
+from .binarization import scBoolSeqBinarizer as scBoolSeq
