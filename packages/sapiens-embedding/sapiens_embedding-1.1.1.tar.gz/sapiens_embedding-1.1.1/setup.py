1e75c2871a16fc64e1ba1f7db9355279ff6247ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 04:10:46 +0000

    Add package sapiens-embedding/1.1.1/sapiens_embedding-1.1.1.tar.gz

---
+++ packages/sapiens-embedding/sapiens_embedding-1.1.1.tar.gz/sapiens_embedding-1.1.1/setup.py
@@ -0,0 +1,15 @@
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'sapiens_embedding'
+version = '1.1.1'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    install_requires=['sapiens-tokenizer'],
+    url='https://github.com/sapiens-technology/SapiensEmbedding',
+    license='Proprietary Software',
+    include_package_data=True
+)
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
