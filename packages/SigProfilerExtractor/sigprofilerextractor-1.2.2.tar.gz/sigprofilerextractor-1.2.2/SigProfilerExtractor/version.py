f9e0854e7b8960e07074636ada6218f69f9ba175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:05:03 +0000

    Add package SigProfilerExtractor/1.2.2/sigprofilerextractor-1.2.2.tar.gz

---
+++ packages/SigProfilerExtractor/sigprofilerextractor-1.2.2.tar.gz/sigprofilerextractor-1.2.2/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.2.2'
+version = '1.2.2'
+Update = 'v1.2.2: Add mutation count and stability to 4608 plots in All_Solutions and a stop parameter'
+    
+    
