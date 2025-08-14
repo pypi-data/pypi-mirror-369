6a22bd1249e801ff13acc28582d916e8ee8df5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:42:40 +0000

    Add package spanda-rag-toolkit/0.1.3/spanda_rag_toolkit-0.1.3.tar.gz

---
+++ packages/spanda-rag-toolkit/spanda_rag_toolkit-0.1.3.tar.gz/spanda_rag_toolkit-0.1.3/spanda_rag_toolkit/__init__.py
@@ -0,0 +1,16 @@
+# spanda_rag_toolkit/__init__.py
+
+"""A modular toolkit for decomposed question answering in RAG pipelines."""
+
+__version__ = "0.1.0"
+
+from .decompose import Spanda_DecomposeQuestion, Spanda_QuestionFilter
+from .reranker import Spanda_Decompose_Reranker
+from .final_llm import Spanda_Final_Query
+
+__all__ = [
+    "Spanda_DecomposeQuestion",
+    "Spanda_QuestionFilter", 
+    "Spanda_Decompose_Reranker",
+    "Spanda_Final_Query",
+]
