ddb718653e0a85e00c5f98e2f07cece6f0c3a905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:06:01 +0000

    Add package rust-pgn-reader-python-binding/3.3.0/rust_pgn_reader_python_binding-3.3.0-cp312-cp312-win_amd64.whl

---
+++ packages/rust-pgn-reader-python-binding/rust_pgn_reader_python_binding-3.3.0-cp312-cp312-win_amd64.whl/rust_pgn_reader_python_binding/__init__.py
@@ -0,0 +1,5 @@
+from .rust_pgn_reader_python_binding import *
+
+__doc__ = rust_pgn_reader_python_binding.__doc__
+if hasattr(rust_pgn_reader_python_binding, "__all__"):
+    __all__ = rust_pgn_reader_python_binding.__all__
