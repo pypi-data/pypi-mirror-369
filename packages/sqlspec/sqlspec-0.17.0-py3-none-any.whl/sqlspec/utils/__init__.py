64b4b217394c8d3ba76c2f0f6eef79af817e72b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:05:44 +0000

    Add package sqlspec/0.17.0/sqlspec-0.17.0-py3-none-any.whl

---
+++ packages/sqlspec/sqlspec-0.17.0-py3-none-any.whl/sqlspec/utils/__init__.py
@@ -0,0 +1,3 @@
+from sqlspec.utils import deprecation, fixtures, module_loader, singleton, sync_tools, text, type_guards
+
+__all__ = ("deprecation", "fixtures", "module_loader", "singleton", "sync_tools", "text", "type_guards")
