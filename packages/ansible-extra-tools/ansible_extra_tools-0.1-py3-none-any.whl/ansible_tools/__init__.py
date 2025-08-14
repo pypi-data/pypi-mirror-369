080ec9b2f83ae9a7eec61f67b70441fe96c20dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:05:03 +0000

    Add package ansible-extra-tools/0.1/ansible_extra_tools-0.1-py3-none-any.whl

---
+++ packages/ansible-extra-tools/ansible_extra_tools-0.1-py3-none-any.whl/ansible_tools/__init__.py
@@ -0,0 +1,10 @@
+"""Ansible Tools to expand ansible-core
+
+Copyright (C) 2025 Dan Griffin
+Portions Copyright: (c) Ansible Project
+
+Based on work from ansible
+
+Licensed under the GNU General Public License v3.0+ 
+(see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
