682cdf77c6d47aa323c0295c14ae7d25211ae770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:05:20 +0000

    Add package pymathutils/0.1.21/pymathutils-0.1.21.tar.gz

---
+++ packages/pymathutils/pymathutils-0.1.21.tar.gz/pymathutils-0.1.21/pymathutils/__init__.py
@@ -0,0 +1,15 @@
+"""MathUtils - Fast mathematical utilities."""
+
+# numba jit compiled functions
+from . import jit_funs
+
+# general utility functions
+from .mathutils_backend import (
+    sign,
+    thetaphi_from_xyz,
+    rthetaphi_from_xyz,
+    xyz_from_rthetaphi,
+)
+
+# special functions
+from . import special
