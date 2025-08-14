143354e0857a74c9bdb4dad5061d82f443ec96a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 14:05:11 +0000

    Add package tnbsclean/1.6/tnbsclean-1.6.tar.gz

---
+++ packages/tnbsclean/tnbsclean-1.6.tar.gz/tnbsclean-1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tnbsclean",
+    version="1.06",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "mne",
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="MIT",  # License name, not a dict
+    license_files=["LICENSE"],  # Ensure LICENSE is included
+)
