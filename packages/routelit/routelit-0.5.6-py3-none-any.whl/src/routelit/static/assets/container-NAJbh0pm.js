609aa6f9ddad5f8c7d6e5c01d84740658d36d49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 06:06:18 +0000

    Add package routelit/0.5.6/routelit-0.5.6-py3-none-any.whl

---
+++ packages/routelit/routelit-0.5.6-py3-none-any.whl/src/routelit/static/assets/container-NAJbh0pm.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app--5U4-9K0.js";function e({children:t,...r}){return n.jsx("div",{...r,children:t})}export{e as default};
---
+++ packages/routelit/routelit-0.5.6-py3-none-any.whl/src/routelit/static/assets/expander-CEsXtkeh.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app--5U4-9K0.js";const t=({title:s,open:e,children:a})=>r.jsxs("details",{open:e,children:[r.jsx("summary",{children:s}),a]});t.displayName="Expander";export{t as default};
