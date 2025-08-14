b47ab21344a71a6caa27da9d989d673c33ee760e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 05:05:01 +0000

    Add package SigProfilerAssignment/0.2.5/sigprofilerassignment-0.2.5.tar.gz

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-0.2.5.tar.gz/sigprofilerassignment-0.2.5/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.2.5'
+version = '0.2.5'
+Update = 'v0.2.5:  Support for generating decomposition plots for custom signature sets.'
+
+    
