86621201df0f21d042ac9d84e5fb8a5953b19862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-14 04:11:35 +0000

    Add package cdxbasics/0.2.140/cdxbasics-0.2.140.tar.gz

---
+++ packages/cdxbasics/cdxbasics-0.2.140.tar.gz/cdxbasics-0.2.140/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cdxbasics",
+    version="0.2.140",  # will auto-update via pip_modify_setup.py
+    author="Hans Buehler",
+    author_email="github@buehler.london",
+    description="Basic Python tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hansbuehler/cdxbasics",
+    packages=setuptools.find_packages(),
+    install_requires=[
+         'numpy', 'pandas', 'matplotlib', 'sortedcontainers', 'psutil', 'jsonpickle', 'setuptools', 'numba'
+     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
