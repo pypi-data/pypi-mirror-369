682285685e8c23f7caf793f5c88ba5af5581f96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 14:06:56 +0000

    Add package sense-table/0.0.1rc19/sense_table-0.0.1rc19.tar.gz

---
+++ packages/sense-table/sense_table-0.0.1rc19.tar.gz/sense_table-0.0.1rc19/tests/test_settings.py
@@ -0,0 +1,39 @@
+import unittest
+
+from my_logging import getLogger
+from pydantic import ValidationError
+
+from sense_table.settings import SenseTableSettings
+
+logger = getLogger(__name__)
+
+
+class TestSettings(unittest.TestCase):
+    def test_settings(self):
+        settings = SenseTableSettings()
+        logger.info(settings)
+        self.assertEqual(settings.heatmapNormalizeColor, "none")
+
+    def test_wrong_name_or_value(self):
+        with self.assertRaises(ValidationError):
+            SenseTableSettings(heatmapNormalizeColr="zone")
+
+        with self.assertRaises(ValidationError):
+            SenseTableSettings(heatmapNormalizeColor="unknown")
+
+    def test_out_of_range(self):
+        settings = SenseTableSettings(baseFontSize=14)
+        self.assertEqual(settings.baseFontSize, 14)
+
+        with self.assertRaises(ValidationError):
+            SenseTableSettings(baseFontSize=100)
+
+    def test_immutable(self):
+        settings = SenseTableSettings()
+
+        with self.assertRaises(ValidationError):
+            settings.baseFontSize = 15
+
+
+if __name__ == "__main__":
+    unittest.main()
