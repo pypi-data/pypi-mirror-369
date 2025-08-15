ab0dd2eda8af2af76eb10716b6aec6e94b3a7eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 03:30:15 +0000

    Add package material-fingerprinting/0.0.2/material_fingerprinting-0.0.2.tar.gz

---
+++ packages/material-fingerprinting/material_fingerprinting-0.0.2.tar.gz/material_fingerprinting-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="material_fingerprinting",
+    version="0.0.2",
+    author="Moritz Flaschel",
+    author_email="moritz.flaschel@fau.de",
+    url="https://github.com/Material-Fingerprinting",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={
+        "material_fingerprinting": ["databases/*.npz"],
+    },
+    install_requires=[
+        "matplotlib",
+        "numpy",
+    ],
+)
