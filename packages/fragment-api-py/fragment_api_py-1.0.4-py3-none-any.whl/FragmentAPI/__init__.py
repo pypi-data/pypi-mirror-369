1e43675f48cfad165cc15c844843d8693d92004a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:07:06 +0000

    Add package fragment-api-py/1.0.4/fragment_api_py-1.0.4-py3-none-any.whl

---
+++ packages/fragment-api-py/fragment_api_py-1.0.4-py3-none-any.whl/FragmentAPI/__init__.py
@@ -0,0 +1,16 @@
+from .api import FragmentAPI, FragmentAuth, FragmentGeneral, FragmentPremium, FragmentStars, FragmentTON
+from .models import FragmentSession, FragmentAPIError, FragmentErrorType
+
+__all__ = [
+    'FragmentAPI',
+    'FragmentAuth',
+    'FragmentGeneral',
+    'FragmentPremium',
+    'FragmentStars',
+    'FragmentTON',
+    'FragmentSession',
+    'FragmentAPIError',
+    'FragmentErrorType'
+]
+
+__version__ = "1.0.4"
