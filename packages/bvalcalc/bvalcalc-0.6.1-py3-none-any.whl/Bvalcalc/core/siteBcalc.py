e3a48c4a6a4daab9486e2ba0bc511a55e3bb92c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 14:06:50 +0000

    Add package bvalcalc/0.6.1/bvalcalc-0.6.1-py3-none-any.whl

---
+++ packages/bvalcalc/bvalcalc-0.6.1-py3-none-any.whl/Bvalcalc/core/siteBcalc.py
@@ -0,0 +1,19 @@
+from Bvalcalc.core.helpers.demography_helpers import get_Bcur
+
+def siteBcalc(args):    
+    gene_size, distance, quiet = args.gene_size, args.distance, args.quiet
+
+    import Bvalcalc.utils.dfe_helper as dfe_helper
+    dfe_helper.GAMMA_DFE = args.gamma_dfe # Update DFE if --gamma_dfe
+    dfe_helper.CONSTANT_DFE = args.constant_dfe # Update DFE if --constant_dfe
+    from Bvalcalc.core.calculateB import calculateB_linear
+
+    b_values = calculateB_linear(distance, gene_size)
+    print(f"B for site {distance}bp away from {gene_size}bp region: {b_values}")
+
+    if args.pop_change:
+        if not quiet: print("Demographic change prior to B-calculation", b_values)
+        b_values = get_Bcur(b_values)
+        if not quiet: print("Demographic change applied to B-calculation", b_values)
+    
+    return
