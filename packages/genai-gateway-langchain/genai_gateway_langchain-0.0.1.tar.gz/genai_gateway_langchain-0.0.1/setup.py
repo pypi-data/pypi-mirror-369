3d1503c5fe087f0af5784ee718c07d211fef0938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 07:07:13 +0000

    Add package genai-gateway-langchain/0.0.1/genai_gateway_langchain-0.0.1.tar.gz

---
+++ packages/genai-gateway-langchain/genai_gateway_langchain-0.0.1.tar.gz/genai_gateway_langchain-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='genai-gateway-langchain',
+    packages=find_packages(include=['genai-gateway-langchain']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
