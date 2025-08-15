2e51e91a861cf04caabe2f05aecc14a29c92aecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 05:06:12 +0000

    Add package abm/2.1.4b1/abm-2.1.4b1.tar.gz

---
+++ packages/abm/abm-2.1.4b1.tar.gz/abm-2.1.4b1/src/abm/_sdk/likelihood/distributions/__init__.py
@@ -0,0 +1,30 @@
+from .base import (
+    Distribution,
+    DistributionPrediction,
+    DistributionResidual,
+)
+from .distributions import (
+    DistributionsLikelihoodFunction,
+    DistributionsPredictions,
+    DistributionsResiduals,
+)
+from .loguniform import (
+    LogUniformDistribution,
+    LogUniformPrediction,
+    LogUniformResidual,
+)
+from .multivariate_lognormal import (
+    MultivariateLogNormalDistribution,
+    MultivariateLogNormalPrediction,
+    MultivariateLogNormalResidual,
+)
+from .multivariate_normal import (
+    MultivariateNormalDistribution,
+    MultivariateNormalPrediction,
+    MultivariateNormalResidual,
+)
+from .uniform import (
+    UniformDistribution,
+    UniformPrediction,
+    UniformResidual,
+)
---
+++ packages/abm/abm-2.1.4b1.tar.gz/abm-2.1.4b1/src/abm/_sdk/likelihood/distributions/multivariate_normal.py
@@ -0,0 +1,43 @@
+__all__ = [
+    "MultivariateNormalDistribution",
+    "MultivariateNormalPrediction",
+    "MultivariateNormalResidual",
+]
+
+from dataclasses import dataclass
+
+from serialite import serializable
+
+from ...expression import StaticExpression, Unit
+from .base import Distribution, DistributionPrediction, DistributionResidual
+
+
+@serializable
+@dataclass(frozen=True, kw_only=True, slots=True)
+class MultivariateNormalDistribution(Distribution):
+    times: list[StaticExpression]
+    targets: list[str]
+    variance: list[list[StaticExpression]]
+    measurements: list[StaticExpression] | None = None
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class MultivariateNormalPrediction(DistributionPrediction):
+    times: list[float]
+    time_units: list[Unit]
+    targets: list[str]
+    units: list[Unit]
+    predictions: list[float]
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class MultivariateNormalResidual(DistributionResidual):
+    times: list[float]
+    time_units: list[Unit]
+    targets: list[str]
+    measurements: list[float]
+    units: list[Unit]
+    predictions: list[float]
+    residuals: list[float]
+    normalized_residuals: list[float]
+    log_likelihood: float
