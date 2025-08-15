208ad3c6007998d692431e7f3b70e290e196506a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:06:09 +0000

    Add package django-components/0.141.4/django_components-0.141.4-py3-none-any.whl

---
+++ packages/django-components/django_components-0.141.4-py3-none-any.whl/django_components/commands/upgradecomponent.py
@@ -0,0 +1,11 @@
+from django_components.commands.upgrade import UpgradeCommand
+
+
+# TODO_REMOVE_IN_V1 - No longer needed?
+class UpgradeComponentCommand(UpgradeCommand):
+    """
+    **Deprecated**. Use [`components upgrade`](../commands#components-upgrade) instead.
+    """
+
+    name = "upgradecomponent"
+    help = "Deprecated. Use `components upgrade` instead."
