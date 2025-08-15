4f69486d93183953656e5f124c845e230574b781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:40 +0000

    Add package pypibrunohw/1.0.0/pypibrunohw-1.0.0.tar.gz

---
+++ packages/pypibrunohw/pypibrunohw-1.0.0.tar.gz/pypibrunohw-1.0.0/src/pypibrunohw/__init__.py
@@ -0,0 +1,10 @@
+import click
+
+
+@click.command()
+@click.option("--count", default=1, help="Number of greetings.")
+@click.option("--name", prompt="Your name", help="The person to greet.")
+def main(count: int, name: str) -> None:
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
