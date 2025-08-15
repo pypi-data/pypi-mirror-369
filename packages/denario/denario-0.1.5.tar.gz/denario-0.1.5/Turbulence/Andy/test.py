857b49aa45f44056e006e81169efc469cb68607b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 07:07:10 +0000

    Add package denario/0.1.5/denario-0.1.5.tar.gz

---
+++ packages/denario/denario-0.1.5.tar.gz/denario-0.1.5/Turbulence/Andy/test.py
@@ -0,0 +1,23 @@
+from astropilot import AstroPilot, Journal
+
+folder = "Project1"
+
+# set AstroPilot and input text
+astro_pilot = AstroPilot(project_dir=folder)
+astro_pilot.set_data_description(f"{folder}/input.md")
+
+# Generate a research idea from the input text
+#astro_pilot.get_idea_fast(llm='gemini-2.5-flash')
+
+# Generate a research plan to carry out the idea
+#astro_pilot.get_method_fast(llm="gemini-2.5-pro")
+
+# Follow the research plan, write and execute code, make plots, and summarize the results
+#astro_pilot.get_results(engineer_model='gemini-2.5-pro', researcher_model='gemini-2.5-pro')
+#astro_pilot.get_results()
+
+# Write a paper with [APS (Physical Review Journals)](https://journals.aps.org/) style
+astro_pilot.get_paper(journal=Journal.AAS, llm='gemini-2.5-flash', add_citations=False)
+
+
+
