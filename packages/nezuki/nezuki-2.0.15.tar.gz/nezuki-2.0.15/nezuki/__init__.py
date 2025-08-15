4a9a590f725330458b991a81a19229c8fc27ce36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 03:29:47 +0000

    Add package nezuki/2.0.15/nezuki-2.0.15.tar.gz

---
+++ packages/nezuki/nezuki-2.0.15.tar.gz/nezuki-2.0.15/nezuki/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "1.0.1"
+
+
+# from .Http import Http
+# from .Bot import Bot
+# from .Database import Database
+# from .Http import Http
+# from .JsonManager import JsonManager
+# from .YamlManager import YamlManager
+# from .Logger.Logger import get_nezuki_logger, configure_nezuki_logger
+# # from .commonLoggerSingleTon import commonLoggerSingleTon # Da rivedere la struttura nuova
+# # from .commonLogId import commonLogId # Da rivedere la struttura nuova
+# # from .Documentor.Documentor import Documentor # Da utilizzare un modulo pip adatto già creato
+# # from .LogContext.LogContext import LogContext # Da rivedere la struttura nuova
+# # from .Logger.Logger import Logger # Da rivedere la struttura nuova
+# # from .Mail.Mail import Mail # Fa parte del potenziamento del rilascio potenziamento moduli
+# # from .PassKey import PassKey # Rimuovere perché progetto abbandonato
+# # from .PyModules.PyModules import PyModules # Da rimuovere perché abbandonato
+# # from .QrCode.QrCode import QrCode # Da aggiornare al momento corretto
+# # from .ServerUtils.ServerUtils import ServerUtils # Fa parte del rilascio potenziamento moduli
+# # from .TelegramApi.TelegramApi import TelegramApi # Appartenente al rilascio da pianificare relativo ai servizi web
+# # from .TelegramParser.TelegramParser import TelegramParser # Come da riga precedente
+# # from .Telegram.Telegram import TelegramParser_OLD # Come da riga precedente
+# # from .WebLogsViewer import WebLogsViewer # Come da riga precedente
+
+# __all__ = ['Bot', 'Database', 'Http', 'JsonManager', 'YamlManager', 'get_nezuki_logger', 'configure_nezuki_logger']
