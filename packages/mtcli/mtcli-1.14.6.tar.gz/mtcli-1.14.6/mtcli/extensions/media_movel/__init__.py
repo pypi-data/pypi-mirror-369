d4baeb73a567449bceb8edafe47b9de0687e06e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 03:29:41 +0000

    Add package mtcli/1.14.6/mtcli-1.14.6.tar.gz

---
+++ packages/mtcli/mtcli-1.14.6.tar.gz/mtcli-1.14.6/mtcli/extensions/media_movel/__init__.py
@@ -0,0 +1,4 @@
+from . import command, conf
+from .command import mm
+from .models import model_media_movel
+from .views import view_media_movel
---
+++ packages/mtcli/mtcli-1.14.6.tar.gz/mtcli-1.14.6/mtcli/views/view_volumes.py
@@ -0,0 +1,57 @@
+"""Módulo da classe da view de volumes."""
+
+from mtcli import conf
+from mtcli.models import model_chart
+
+
+class VolumesView:
+    """Classe da view de volumes."""
+
+    def __init__(
+        self,
+        bars,
+        count,
+        period="d1",
+        date="",
+        numerator=False,
+        show_date=False,
+        volume="tick",
+    ):
+        """View de volumes."""
+        self.count = count
+        self.period = period
+        self.date = date
+        self.numerator = numerator
+        self.show_date = show_date
+        self.volume = volume
+        self.chart = model_chart.ChartModel(bars, len(bars), count, date)
+        self.bars = bars[-count:]
+
+    def views(self):
+        """Retorna a lista de views."""
+        views = []
+        n = self.chart.get_n()
+        sequencias = self.chart.consecutive_sequencias()
+        sequencias = sequencias[-self.count :]
+        sequencias_volume = self.chart.consecutive_volumes(self.volume)
+        sequencias_volume = sequencias_volume[-self.count :]
+        for bar, sequencia, sequencia_volume in zip(
+            self.bars, sequencias, sequencias_volume
+        ):
+            n += 1
+            volume = bar.volume if self.volume == "tick" else bar.volume_real
+            if self.numerator or self.show_date:
+                view = "%s "  # numerador ou data
+            else:
+                view = ""
+            view += "%s %s %i"  # volume tick
+            if self.show_date:
+                if self.period == "d1" or self.period == "w1" or self.period == "mn1":
+                    views.append(view % (bar.date, sequencia, sequencia_volume, volume))
+                else:
+                    views.append(view % (bar.time, sequencia, sequencia_volume, volume))
+            elif self.numerator:
+                views.append(view % (n, sequencia, sequencia_volume, volume))
+            else:
+                views.append(view % (sequencia, sequencia_volume, volume))
+        return views
