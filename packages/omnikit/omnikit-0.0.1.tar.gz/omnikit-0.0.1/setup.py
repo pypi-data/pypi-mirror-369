96ff231b30e023b1d31a68616c33bc6fdf144145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:52 +0000

    Add package omnikit/0.0.1/omnikit-0.0.1.tar.gz

---
+++ packages/omnikit/omnikit-0.0.1.tar.gz/omnikit-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='omnikit',
+    version='0.0.1',
+    description=(),
+    long_description=(),
+    author='tongbochen',
+    author_email='bochen.tong@proton.me',
+    url='',
+    license='MIT Licence',
+    packages=find_packages(),
+    install_requires=[],
+)
