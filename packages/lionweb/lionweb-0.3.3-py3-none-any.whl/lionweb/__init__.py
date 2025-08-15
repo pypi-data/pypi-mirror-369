59a96f9fdc1751bcf1c2135fd0c78848f894cd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 03:29:56 +0000

    Add package lionweb/0.3.3/lionweb-0.3.3-py3-none-any.whl

---
+++ packages/lionweb/lionweb-0.3.3-py3-none-any.whl/lionweb/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.3.3"
+from .lionweb_version import LionWebVersion
+
+__all__ = ["LionWebVersion"]
---
+++ packages/lionweb/lionweb-0.3.3-py3-none-any.whl/lionweb/model/__init__.py
@@ -0,0 +1,4 @@
+from .classifier_instance import ClassifierInstance as ClassifierInstance
+from .impl.dynamic_node import DynamicNode as DynamicNode
+from .node import Node as Node
+from .reference_value import ReferenceValue as ReferenceValue
