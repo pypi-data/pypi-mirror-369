fa6708438131ae272aec3b66dfcf788ad7307c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 04:12:11 +0000

    Add package osbot-aws/2.39.4/osbot_aws-2.39.4-py3-none-any.whl

---
+++ packages/osbot-aws/osbot_aws-2.39.4-py3-none-any.whl/osbot_aws/aws/lambda_/schemas/Safe_Str__File__Path__Python_Package.py
@@ -0,0 +1,7 @@
+import re
+from osbot_utils.type_safe.primitives.safe_str.filesystem.Safe_Str__File__Path import Safe_Str__File__Path
+
+TYPE_SAFE_STR__FILE_PATH__PYTHON_PACKAGE__REGEX      = re.compile(r'[^a-zA-Z0-9_\-./\\ =]')  # Allow alphanumerics, underscores, hyphens, dots, slashes, spaces and equal
+
+class Safe_Str__File__Path__Python_Package(Safe_Str__File__Path):
+    regex = TYPE_SAFE_STR__FILE_PATH__PYTHON_PACKAGE__REGEX
