37b8f80c3ef1def477c1c92ee9ebe704b85b3426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:53 +0000

    Add package path-magician/0.1.0/path_magician-0.1.0.tar.gz

---
+++ packages/path-magician/path_magician-0.1.0.tar.gz/path_magician-0.1.0/path_magician/core_functions.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+def add_to_sys_path(abs_path, rel_tool_path):
+    """
+    将根据相对工具路径和绝对路径拼接得到的新绝对路径添加到 sys.path 中。
+
+    参数:
+    abs_path (str): 绝对路径字符串。
+    rel_tool_path (str): 相对工具路径字符串。
+
+    返回:
+    无 - 此函数直接修改 sys.path 列表。
+    """
+
+    # 获取rel_tool_path中的第一个文件夹
+    first_folder = rel_tool_path.split(os.sep)[0]
+    # 查找first_folder在abs_path中的位置
+    index = abs_path.find(first_folder)
+    if index != -1:
+        # 截取abs_path中first_folder之前的部分
+        prefix = abs_path[:index]
+        # 拼接成新的绝对路径
+        new_abs_path = os.path.join(prefix, rel_tool_path)
+        
+        # 将新的绝对路径添加到sys.path中
+        sys.path.append(new_abs_path)
---
+++ packages/path-magician/path_magician-0.1.0.tar.gz/path_magician-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="path_magician",          # 包名（必须唯一，PyPI上不能与现有包重复）
+    version="0.1.0",         # 版本号（遵循语义化版本：主版本.次版本.修订号）
+    author="Jiahuan",      # 作者名
+    author_email="719481125@qq.com",  # 作者邮箱
+    description="make path management easier",  # 包的简短描述
+    packages=find_packages(),  # 自动发现所有Python包（即`my_tool`目录）
+    classifiers=[             # 包的分类标签（帮助用户找到你的包，参考PyPI分类表）
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",  # 支持的Python最低版本
+    install_requires=[],      # 包的依赖（如需要requests，就写["requests>=2.25.0"]）
+)
