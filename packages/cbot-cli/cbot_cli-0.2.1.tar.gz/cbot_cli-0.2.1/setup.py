3885739daddfc545bfa11a8752369f3656a3df03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 07:07:05 +0000

    Add package cbot-cli/0.2.1/cbot_cli-0.2.1.tar.gz

---
+++ packages/cbot-cli/cbot_cli-0.2.1.tar.gz/cbot_cli-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cbot-cli",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pyperclip",
+        "openai",
+        "python-dotenv"
+    ],
+    entry_points={
+        "console_scripts": [
+            "cbot = cbot.__main__:main",
+        ],
+    },
+)
