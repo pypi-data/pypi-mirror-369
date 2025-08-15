eec050e1a72c9890bd5a3d16a9c351cafdf91df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 07:07:13 +0000

    Add package genai-gateway-strands/0.0.1/genai_gateway_strands-0.0.1.tar.gz

---
+++ packages/genai-gateway-strands/genai_gateway_strands-0.0.1.tar.gz/genai_gateway_strands-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='genai-gateway-strands',
+    packages=find_packages(include=['genai-gateway-strands']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
