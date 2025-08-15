0835a14a719c29771dd3002bd854324f0a7ec0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 06:06:42 +0000

    Add package bafser/2.0.0/bafser-2.0.0.tar.gz

---
+++ packages/bafser/bafser-2.0.0.tar.gz/bafser-2.0.0/test/blueprints/index.py
@@ -0,0 +1,47 @@
+from flask import Blueprint
+from flask_jwt_extended import jwt_required  # type: ignore
+
+from bafser import permission_required, response_msg, use_db_session, get_json_values_from_req, use_user
+from sqlalchemy.orm import Session
+from test.data import Operations
+from test.data.img import Img, ImageJson
+from test.data.user import User
+
+
+blueprint = Blueprint("index", __name__)
+
+
+@blueprint.route("/api/user")
+@use_db_session
+def index(db_sess: Session):
+    u = User.get_admin(db_sess)
+    assert u
+    return {"name": u.login}
+
+
+@blueprint.post("/api/post")
+def test_post():  # type: ignore
+    a, b, c = get_json_values_from_req(("a", int), ("b", str, "def"), ("c", bool))
+    return {"a": a, "b": b, "c": c}  # type: ignore
+
+
+@blueprint.post("/api/post2")
+def test_post2():
+    a = get_json_values_from_req(("a", int))
+    return {"a": a}
+
+
+@blueprint.post("/api/img")
+@jwt_required()
+@use_db_session
+@use_user()
+@permission_required(Operations.upload_img)
+def upload_img(db_sess: Session, user: User):
+    img_data = get_json_values_from_req(("img", ImageJson))
+
+    img, image_error = Img.new(user, img_data)
+    if image_error:
+        return response_msg(image_error, 400)
+    assert img
+
+    return {"id": img.id}
