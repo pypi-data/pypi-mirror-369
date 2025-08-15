23b9d6cd4f5a6d467bde2e8eb57a0df1b800de07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:41 +0000

    Add package mipx/0.7.1/mipx-0.7.1.tar.gz

---
+++ packages/mipx/mipx-0.7.1.tar.gz/mipx-0.7.1/tests/test_tuple.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+import mipx
+from mipx.types import TupleDict
+
+
+def test_key_pattern_set():
+    model = mipx.Model()
+    x: TupleDict[int, int] = model.addVars(1, 2)
+    s = x.keyset("-", "*")
+    assert len(s) == 2
+    y: TupleDict[int, int] = model.addVars(3, 2)
+    z = model.addVars(10)
+    res = y.key_pattern_set("-", "*")
+    res = z.key_pattern_set("*")
+    for a in res:
+        model.addConstr(z.quicksum(*a) == 1)
+
+
+if __name__ == "__main__":
+    test_key_pattern_set()
