e6fdb7bcbd91f87997ccae1795b076c87a021dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 07:07:10 +0000

    Add package raglight/2.3.0/raglight-2.3.0.tar.gz

---
+++ packages/raglight/raglight-2.3.0.tar.gz/raglight-2.3.0/src/raglight/config/agentic_rag_config.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass, field
+from ..config.settings import Settings
+from typing import List
+from ..models.data_source_model import DataSource
+
+
+@dataclass(kw_only=True)
+class AgenticRAGConfig:
+    api_key: str = field(default="")
+    api_base: str = field(default=Settings.DEFAULT_OLLAMA_CLIENT)
+    provider: str = field(default=Settings.OLLAMA.lower())
+    model: str = field(default=Settings.DEFAULT_LLM)
+    num_ctx: int = field(default=8192)
+    k: int = field(default=5)
+    verbosity_level: int = field(default=2)
+    max_steps: int = field(default=4)
+    system_prompt: str = field(default=Settings.DEFAULT_AGENT_PROMPT)
+    knowledge_base: List[DataSource] = field(default=None)
+    ignore_folders: list = field(
+        default_factory=lambda: list(Settings.DEFAULT_IGNORE_FOLDERS)
+    )
+
+
+@dataclass(kw_only=True)
+class SimpleAgenticRAGConfig:
+    api_key: str = field(default="")
+    api_base: str = field(default=Settings.DEFAULT_OLLAMA_CLIENT)
+    provider: str = field(default=Settings.OLLAMA.lower())
+    model: str = field(default=Settings.DEFAULT_LLM)
+    num_ctx: int = field(default=8192)
+    k: int = field(default=2)
+    verbosity_level: int = field(default=2)
+    max_steps: int = field(default=4)
+    system_prompt: str = field(default=Settings.DEFAULT_AGENT_PROMPT)
+    ignore_folders: list = field(
+        default_factory=lambda: list(Settings.DEFAULT_IGNORE_FOLDERS)
+    )
