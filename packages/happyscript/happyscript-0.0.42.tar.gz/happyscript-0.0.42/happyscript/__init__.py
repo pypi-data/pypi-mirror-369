940f604b7d3aa652f007d342afab6b3962a009b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 04:12:12 +0000

    Add package happyscript/0.0.42/happyscript-0.0.42.tar.gz

---
+++ packages/happyscript/happyscript-0.0.42.tar.gz/happyscript-0.0.42/happyscript/__init__.py
@@ -0,0 +1,15 @@
+"""HappyScript - handy script launcher"""
+
+__version__ = "0.0.42"
+
+# With these statements, after doing 'import happyscript' you can do 'xxx = happyscript.ScriptManager()'
+#from .scriptmanager import ScriptManager
+#from .pytestsupport import *
+
+from .scriptmanager import ScriptManager
+from .staticscriptmanager import StaticScriptManager
+from .splashmenu.splashmenumain import SplashMenu
+
+from .scriptcontrol import ScriptControl as Ctrl
+from .scriptgui import ScriptGui as Gui
+from .scriptinfo import ScriptInfo as Info
