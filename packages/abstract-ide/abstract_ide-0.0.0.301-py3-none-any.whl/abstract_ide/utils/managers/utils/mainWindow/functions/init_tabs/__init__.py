6a2fee2730340df8ed81803723ec4129e586a9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:05:55 +0000

    Add package abstract-ide/0.0.0.301/abstract_ide-0.0.0.301-py3-none-any.whl

---
+++ packages/abstract-ide/abstract_ide-0.0.0.301-py3-none-any.whl/abstract_ide/utils/managers/utils/mainWindow/functions/init_tabs/__init__.py
@@ -0,0 +1,8 @@
+from .runner import *
+from .functions_page import *
+from .initialize_init import *
+from .finder import *
+from .apigui import *
+def getManagerFuncs(self):
+    self.getRunner = getRunner
+    self.initializeInit = initializeInit
