3ecdabd21b083ab03fb2934eecde2c5ca15169dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:02 +0000

    Add package zstdlib/0.1.0/zstdlib-0.1.0.tar.gz

---
+++ packages/zstdlib/zstdlib-0.1.0.tar.gz/zstdlib-0.1.0/zstdlib/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+from .not_set import NotSetType, NotSet
+from .frozen import Freezable, frozen
+from .singleton import Singleton
+from .enum import Enum
+from . import log, io
