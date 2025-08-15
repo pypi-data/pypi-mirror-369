a624ed8c80ba8902b48c080acb33efd04e82a4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:24 +0000

    Add package jneqsim/3.0.38b0/jneqsim-3.0.38b0.tar.gz

---
+++ packages/jneqsim/jneqsim-3.0.38b0.tar.gz/jneqsim-3.0.38b0/jneqsim/neqsim/physicalproperties/methods/liquidphysicalproperties/density/__init__.pyi
@@ -0,0 +1,30 @@
+
+import sys
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    from typing_extensions import Protocol
+
+import jneqsim.neqsim.physicalproperties.methods.liquidphysicalproperties
+import jneqsim.neqsim.physicalproperties.methods.methodinterface
+import jneqsim.neqsim.physicalproperties.system
+import typing
+
+
+
+class Costald(jneqsim.neqsim.physicalproperties.methods.liquidphysicalproperties.LiquidPhysicalPropertyMethod, jneqsim.neqsim.physicalproperties.methods.methodinterface.DensityInterface):
+    def __init__(self, physicalProperties: jneqsim.neqsim.physicalproperties.system.PhysicalProperties): ...
+    def calcDensity(self) -> float: ...
+    def clone(self) -> 'Costald': ...
+
+class Density(jneqsim.neqsim.physicalproperties.methods.liquidphysicalproperties.LiquidPhysicalPropertyMethod, jneqsim.neqsim.physicalproperties.methods.methodinterface.DensityInterface):
+    def __init__(self, physicalProperties: jneqsim.neqsim.physicalproperties.system.PhysicalProperties): ...
+    def calcDensity(self) -> float: ...
+    def clone(self) -> 'Density': ...
+
+
+class __module_protocol__(Protocol):
+    # A module protocol which reflects the result of ``jp.JPackage("jneqsim.neqsim.physicalproperties.methods.liquidphysicalproperties.density")``.
+
+    Costald: typing.Type[Costald]
+    Density: typing.Type[Density]
