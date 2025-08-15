5c01c4d9cf5f977267d02b166e319df6b557e683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 14:06:54 +0000

    Add package connecpy/2.1.0/connecpy-2.1.0.tar.gz

---
+++ packages/connecpy/connecpy-2.1.0.tar.gz/connecpy-2.1.0/src/connecpy/exceptions.py
@@ -0,0 +1,44 @@
+__all__ = ["ConnecpyException"]
+
+
+from collections.abc import Iterable, Sequence
+
+from google.protobuf.any import Any, pack
+from google.protobuf.message import Message
+
+from .code import Code
+
+
+class ConnecpyException(Exception):
+    def __init__(self, code: Code, message: str, details: Iterable[Message] = ()):
+        """
+        Initializes a new Connecpy exception. If raised in a server, the same exception
+        will be raised in the client.
+
+        Args:
+            code: The error code.
+            message: The error message.
+            details: Additional details about the error.
+        """
+        super().__init__(message)
+        try:
+            self._code = Code(code)
+        except ValueError:
+            self._code = Code.UNKNOWN
+        self._message = message
+
+        self._details = (
+            [m if isinstance(m, Any) else pack(m) for m in details] if details else ()
+        )
+
+    @property
+    def code(self):
+        return self._code
+
+    @property
+    def message(self):
+        return self._message
+
+    @property
+    def details(self) -> Sequence[Any]:
+        return self._details
