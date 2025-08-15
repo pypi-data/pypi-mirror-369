46f778a05338deb5498d0f35d5737c763d4ccec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 04:11:40 +0000

    Add package rlupat.toyrobot/0.2.2/rlupat_toyrobot-0.2.2.tar.gz

---
+++ packages/rlupat.toyrobot/rlupat_toyrobot-0.2.2.tar.gz/rlupat_toyrobot-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# read the contents of README.md file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="rlupat.toyrobot",
+    version="0.2.2",
+    description="ToyRobot Example Implementation",
+    author="Richard Lupat",
+    license="MIT",
+    packages=["toyrobot"]
+    + ["toyrobot." + p for p in sorted(find_packages("./toyrobot"))],
+    install_requires=[
+        "wheel",
+        "setuptools",
+        "twine"
+    ],
+    entry_points={"console_scripts": ["toyrobot=toyrobot.app:main"]},
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
