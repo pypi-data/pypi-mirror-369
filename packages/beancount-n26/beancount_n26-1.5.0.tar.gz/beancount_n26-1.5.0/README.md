21a795369138f48aa70f6b1167e88df30616b314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 06:06:45 +0000

    Add package beancount-n26/1.5.0/beancount_n26-1.5.0.tar.gz

---
+++ packages/beancount-n26/beancount_n26-1.5.0.tar.gz/beancount_n26-1.5.0/README.md
@@ -0,0 +1,125 @@
+# Beancount N26 Importer
+
+[![image](https://github.com/siddhantgoel/beancount-n26/workflows/beancount-n26/badge.svg)](https://github.com/siddhantgoel/beancount-n26/workflows/beancount-n26/badge.svg)
+[![image](https://img.shields.io/pypi/v/beancount-n26.svg)](https://pypi.python.org/pypi/beancount-n26)
+[![image](https://img.shields.io/pypi/pyversions/beancount-n26.svg)](https://pypi.python.org/pypi/beancount-n26)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+`beancount-n26` provides a [Beancount] Importer for converting CSV exports of
+[N26] account summaries to the Beancount format.
+
+## Installation
+
+```sh
+$ pip install beancount-n26
+```
+
+In case you prefer installing from the Github repository, please note that `main` is the
+development branch so `stable` is what you should be installing from.
+
+## Usage
+
+### Beancount 3.x
+
+Beancount 3.x has replaced the `config.py` file based workflow in favor of having a
+script based workflow, as per the [changes documented here]. The `beangulp` examples
+suggest using a Python script based on `beangulp.Ingest`. Here's an example of how that
+might work:
+
+Add an `import.py` script in your project root with the following contents:
+
+```python
+from beancount_n26 import N26Importer
+from beangulp import Ingest
+
+importers = (
+    N26Importer(
+        IBAN_NUMBER,
+        'Assets:N26',
+        language='en',
+        file_encoding='utf-8',
+    ),
+)
+
+if __name__ == "__main__":
+    ingest = Ingest(importer)
+    ingest()
+```
+
+... and run it directly using `python import.py extract`.
+
+### Beancount 2.x
+
+Add the following to your `config.py`.
+
+```python
+from beancount_n26 import N26Importer
+
+CONFIG = [
+    N26Importer(
+        IBAN_NUMBER,
+        'Assets:N26',
+        language='en',
+        file_encoding='utf-8',
+    ),
+]
+```
+
+### Classification
+
+To classify specific recurring transactions automatically, you can specify an
+`account_patterns` parameter. The key should be the account name and the items in the
+list are regular expressions that should match a `payee`.
+
+A few helper functions have been provided in
+`beancount_n26/utils/patterns_generation.py` to help you generate this dictionnary.
+
+#### Beancount 3.x
+
+```python
+from beancount_n26 import N26Importer
+from beangulp import Ingest
+
+importers = (
+    N26Importer(
+        IBAN_NUMBER,
+        'Assets:N26',
+        account_patterns={"Expenses:Supermarket": ["REWE", "ALDI"]}
+    ),
+)
+
+if __name__ == "__main__":
+    ingest = Ingest(importer)
+    ingest()
+```
+
+#### Beancount 2.x
+
+```python
+from beancount_n26 import N26Importer
+
+CONFIG = [
+    N26Importer(
+        ...
+        account_patterns={"Expenses:Supermarket": ["REWE", "ALDI"]}
+    ),
+]
+```
+
+## Contributing
+
+Please make sure you have Python 3.9+ and [Poetry] installed.
+
+1. Git clone the repository -
+   `git clone https://github.com/siddhantgoel/beancount-n26`
+
+2. Install the packages required for development -
+   `poetry install`
+
+3. That's basically it. You should now be able to run the test suite -
+   `poetry run task test`.
+
+[Beancount]: http://furius.ca/beancount/
+[N26]: https://n26.com/
+[Poetry]: https://python-poetry.org/
+[changes documented here]: https://docs.google.com/document/d/1O42HgYQBQEna6YpobTqszSgTGnbRX7RdjmzR2xumfjs/edit#heading=h.hjzt0c6v8pfs
---
+++ packages/beancount-n26/beancount_n26-1.5.0.tar.gz/beancount_n26-1.5.0/beancount_n26/__init__.py
@@ -0,0 +1,335 @@
+import csv
+import re
+from collections import OrderedDict, namedtuple
+from datetime import datetime
+from typing import Dict, List, Optional
+
+from beancount.core import data, flags
+from beancount.core.amount import Amount
+from beancount.core.number import Decimal
+from beangulp.importer import Importer
+
+HeaderField = namedtuple("HeaderField", ["label", "optional"])
+
+HEADER_FIELDS = {
+    "en": (
+        OrderedDict(
+            {
+                "date": HeaderField("Date", False),
+                "payee": HeaderField("Payee", False),
+                "account_number": HeaderField("Account number", False),
+                "transaction_type": HeaderField("Transaction type", False),
+                "payment_reference": HeaderField("Payment reference", False),
+                "category": HeaderField("Category", True),
+                "amount_eur": HeaderField("Amount (EUR)", False),
+                "amount_foreign_currency": HeaderField(
+                    "Amount (Foreign Currency)", False
+                ),
+                "type_foreign_currency": HeaderField("Type Foreign Currency", False),
+                "exchange_rate": HeaderField("Exchange Rate", False),
+            }
+        ),
+        OrderedDict(
+            {
+                "date": HeaderField("Booking Date", False),
+                "value_date": HeaderField("Value Date", False),
+                "payee": HeaderField("Partner Name", False),
+                "account_number": HeaderField("Partner Iban", False),
+                "transaction_type": HeaderField("Type", False),
+                "payment_reference": HeaderField("Payment Reference", False),
+                "category": HeaderField("Category", True),
+                "account_name": HeaderField("Account Name", False),
+                "amount_eur": HeaderField("Amount (EUR)", False),
+                "amount_foreign_currency": HeaderField("Original Amount", False),
+                "type_foreign_currency": HeaderField("Original Currency", False),
+                "exchange_rate": HeaderField("Exchange Rate", False),
+            }
+        ),
+    ),
+    "de": (
+        OrderedDict(
+            {
+                "date": HeaderField("Datum", False),
+                "payee": HeaderField("Empfänger", False),
+                "account_number": HeaderField("Kontonummer", False),
+                "transaction_type": HeaderField("Transaktionstyp", False),
+                "payment_reference": HeaderField("Verwendungszweck", False),
+                "category": HeaderField("Kategorie", True),
+                "amount_eur": HeaderField("Betrag (EUR)", False),
+                "amount_foreign_currency": HeaderField("Betrag (Fremdwährung)", False),
+                "type_foreign_currency": HeaderField("Fremdwährung", False),
+                "exchange_rate": HeaderField("Wechselkurs", False),
+            }
+        ),
+    ),
+    "fr": (
+        OrderedDict(
+            {
+                "date": HeaderField("Date", False),
+                "payee": HeaderField("Bénéficiaire", False),
+                "account_number": HeaderField("Numéro de compte", False),
+                "transaction_type": HeaderField("Type de transaction", False),
+                "payment_reference": HeaderField("Référence de paiement", False),
+                "category": HeaderField("Catégorie", True),
+                "amount_eur": HeaderField("Montant (EUR)", False),
+                "amount_foreign_currency": HeaderField(
+                    "Montant (Devise étrangère)", False
+                ),
+                "type_foreign_currency": HeaderField(
+                    "Sélectionnez la devise étrangère", False
+                ),
+                "exchange_rate": HeaderField("Taux de conversion", False),
+            }
+        ),
+    ),
+}
+
+
+def _is_language_supported(language: str) -> bool:
+    return language in HEADER_FIELDS
+
+
+def _header_values_for(language: str, include_optional: bool = True) -> List[List[str]]:
+    result = []
+    translations = HEADER_FIELDS[language]
+
+    for translation in translations:
+        if include_optional:
+            entries = OrderedDict(
+                (key, value.label) for (key, value) in translation.items()
+            )
+        else:
+            entries = OrderedDict(
+                (key, value.label)
+                for (key, value) in translation.items()
+                if not value.optional
+            )
+        result.append(list(entries.values()))
+
+    return result
+
+
+class InvalidFormatError(Exception):
+    pass
+
+
+PayeePattern = namedtuple("PayeePattern", ["regex", "account"])
+
+
+class N26Importer(Importer):
+    def __init__(
+        self,
+        iban: str,
+        account_name: str,
+        language: str = "en",
+        file_encoding: str = "utf-8",
+        account_patterns: Dict[str, List[str]] = {},
+    ):
+        self.iban = iban
+        self.account_name = account_name
+        self.language = language
+        self.file_encoding = file_encoding
+        self.payee_patterns = set()
+
+        self._filepath = None
+        self._translation_strings = None
+
+        if not _is_language_supported(language):
+            raise InvalidFormatError(
+                "Language {} is not supported (yet)".format(language)
+            )
+
+        # Compile account and payee pattern regular expressions
+
+        seen_patterns = set()
+
+        for account, patterns in account_patterns.items():
+            for pattern in patterns:
+                assert (
+                    pattern not in seen_patterns
+                ), f"{pattern} defined in multiple accounts"
+
+                seen_patterns.add(pattern)
+                self.payee_patterns.add(
+                    PayeePattern(
+                        regex=re.compile(pattern, flags=re.IGNORECASE),
+                        account=account,
+                    )
+                )
+
+    def account(self, _) -> data.Account:
+        return data.Account(self.account_name)
+
+    def _update_translations(self, filepath: str):
+        with open(filepath, encoding=self.file_encoding) as fd:
+            line = fd.readline().strip()
+
+        actual_header = [column.strip('"') for column in line.split(",")]
+
+        translations = HEADER_FIELDS[self.language]
+
+        for translation in translations:
+            fields = translation.values()
+            header_with_optional = [field.label for field in fields]
+            header_without_optional = [
+                field.label for field in fields if not field.optional
+            ]
+
+            if (
+                actual_header == header_with_optional
+                or actual_header == header_without_optional
+            ):
+                self._translation_strings = {
+                    key: value.label for (key, value) in translation.items()
+                }
+                return
+
+        raise InvalidFormatError(
+            "File {} does not contain any of the expected headers".format(filepath)
+        )
+
+    def _translate(self, key):
+        return self._translation_strings[key]
+
+    def _parse_date(self, entry):
+        return datetime.strptime(entry[self._translate("date")], "%Y-%m-%d").date()
+
+    def date(self, filepath: str) -> Optional[datetime.date]:
+        if not self.identify(filepath):
+            return None
+
+        self._update_translations(filepath)
+
+        date = None
+
+        with open(filepath, encoding=self.file_encoding) as fd:
+            reader = csv.DictReader(
+                fd, delimiter=",", quoting=csv.QUOTE_MINIMAL, quotechar='"'
+            )
+
+            for line in reader:
+                date_tmp = self._parse_date(line)
+
+                if not date or date_tmp > date:
+                    date = date_tmp
+
+        return date
+
+    def identify(self, filepath: str) -> bool:
+        try:
+            with open(filepath, encoding=self.file_encoding) as fd:
+                line = fd.readline().strip()
+
+            expected_headers = _header_values_for(self.language) + _header_values_for(
+                self.language, include_optional=False
+            )
+            actual_header = [column.strip('"') for column in line.split(",")]
+
+            for expected_header in expected_headers:
+                if expected_header != actual_header:
+                    continue
+                return True
+
+            return False
+        except ValueError:
+            return False
+
+    def extract(self, filepath: str, existing: data.Entries = None) -> data.Entries:
+        entries = []
+
+        if not self.identify(filepath):
+            return []
+
+        self._update_translations(filepath)
+
+        s_amount_eur = self._translate("amount_eur")
+        s_amount_foreign_currency = self._translate("amount_foreign_currency")
+        s_payee = self._translate("payee")
+        s_payment_reference = self._translate("payment_reference")
+        s_type_foreign_currency = self._translate("type_foreign_currency")
+        s_exchange_rate = self._translate("exchange_rate")
+
+        with open(filepath, encoding=self.file_encoding) as fd:
+            lines = [line.strip() for line in fd.readlines()]
+            reader = csv.DictReader(
+                lines, delimiter=",", quoting=csv.QUOTE_MINIMAL, quotechar='"'
+            )
+
+            for index, line in enumerate(reader):
+                meta = data.new_metadata(filepath, index)
+                meta["__source__"] = lines[index]
+
+                postings = []
+
+                eur_amount = line[s_amount_eur]
+                foreign_amount = line[s_amount_foreign_currency]
+                foreign_currency = line[s_type_foreign_currency]
+
+                if foreign_amount and foreign_currency != "EUR":
+                    # According to the documentation:
+                    # https://beancount.github.io/docs/command_line_accounting_cookbook.html#currency-transfers-conversions
+                    # For the following posting
+                    #     Assets:US:BofA:Checking      10000.00 USD @ 0.90 CHF
+                    # > The balance amount of the second posting is calculated as
+                    # > 10,000.00 USD x 0.90 CHF/USD = 9,000 CHF ...
+                    #
+                    # The exchange rate from the N26 CSV output seems to be the EUR
+                    # amount divided by the amount in the foreign currency. So to keep
+                    # things as transparent as possible (i.e. avoid decimal divisions)
+                    # we output the main amount in the foreign currency and attach the
+                    # EUR exchange rate as the price of the transaction.
+
+                    exchange_rate = Decimal(line[s_exchange_rate])
+
+                    postings += [
+                        data.Posting(
+                            self.account(filepath),
+                            Amount(Decimal(eur_amount), "EUR"),
+                            None,
+                            Amount(1 / exchange_rate, foreign_currency),
+                            None,
+                            None,
+                        ),
+                    ]
+                else:
+                    postings += [
+                        data.Posting(
+                            self.account(filepath),
+                            Amount(Decimal(eur_amount), "EUR"),
+                            None,
+                            None,
+                            None,
+                            None,
+                        ),
+                    ]
+
+                match = None
+                for pattern in self.payee_patterns:
+                    if pattern.regex.match(line[s_payee]):
+                        match = pattern.account
+                if match:
+                    postings += [
+                        data.Posting(
+                            match,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                        ),
+                    ]
+
+                entries.append(
+                    data.Transaction(
+                        meta,
+                        self._parse_date(line),
+                        flags.FLAG_OKAY,
+                        line[s_payee],
+                        line[s_payment_reference],
+                        data.EMPTY_SET,
+                        data.EMPTY_SET,
+                        postings,
+                    )
+                )
+
+        return entries
