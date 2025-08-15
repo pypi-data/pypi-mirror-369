b74d5bc976a5167bc2b7f5068769bd0d18f32f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:41 +0000

    Add package pyarccli/0.0.4/pyarccli-0.0.4.tar.gz

---
+++ packages/pyarccli/pyarccli-0.0.4.tar.gz/pyarccli-0.0.4/pyarccli/__main__.py
@@ -0,0 +1,8 @@
+def main():
+    import cmd_generate_module
+    import cmd_generate_config_file
+    
+    # Utilisez cmd_generate_module.config et cmd_generate_config_file.config
+
+if __name__ == "__main__":
+    main()
