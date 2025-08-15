dda577b0ec32bf1bd9c70938a12a10b5c57b581a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 06:06:18 +0000

    Add package emailify/0.1.5/emailify-0.1.5-py3-none-any.whl

---
+++ packages/emailify/emailify-0.1.5-py3-none-any.whl/emailify/renderers/fill.py
@@ -0,0 +1,11 @@
+from emailify.models import Fill
+from emailify.renderers.core import _render
+from emailify.renderers.style import render_extra_props
+
+
+def render_fill(fill: Fill) -> str:
+    return _render(
+        "fill",
+        fill=fill,
+        extra_props=render_extra_props("fill", fill.style),
+    )
