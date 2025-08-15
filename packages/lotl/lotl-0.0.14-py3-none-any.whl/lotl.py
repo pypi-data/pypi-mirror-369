dab37ec16a4866f620bec62710629efbdd88034d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 14:07:19 +0000

    Add package lotl/0.0.14/lotl-0.0.14-py3-none-any.whl

---
+++ packages/lotl/lotl-0.0.14-py3-none-any.whl/lotl.py
@@ -0,0 +1,73 @@
+class lotl:
+    def __init__(self,data,nth=-1):
+        self.data = data
+        self.nth = nth
+
+    def chain(self):
+        hits = []
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                hits.append(self.data[i][j])
+        return hits
+
+    
+    def fill(self):
+        if isinstance(self.data, int):
+            return [self.nth for i in range(self.data)]
+        elif isinstance(self.data,list) or isinstance(self.data,tuple):
+            if len(self.data) == 2:
+                return [[self.nth for j in range(self.data[0])] for i in range(self.data[1])]
+    
+    def flatten(self):
+        new_data = self.data
+        if self.nth == -1:
+            while True:
+                if any([True if isinstance(i,list) or isinstance(i,tuple) else False for i in new_data]):
+                    add = []
+                    for i in new_data:
+                        if isinstance(i,list) or isinstance(i,tuple):
+                            add += i
+                        else:
+                            add.append(i)
+                    new_data = list(add[:])
+                else:
+                    break
+        else:
+            for _ in range(self.nth):
+                if any([True if isinstance(i,list) or isinstance(i,tuple) else False for i in new_data]):
+                    add = []
+                    for i in new_data:
+                        if isinstance(i,list) or isinstance(i,tuple):
+                            add += i
+                        else:
+                            add.append(i)
+                    new_data = list(add[:])
+                else:
+                    break
+        return new_data
+
+    def mean(self):
+        return sum(self.data) / len(self.data)
+
+    def nested(self):
+        count = 0
+        new_data = self.data
+        if self.nth == -1:
+            while True:
+                if any([True if isinstance(i,list) or isinstance(i,tuple) else False for i in new_data]):
+                    count += 1
+                    add = []
+                    for i in new_data:
+                        if isinstance(i,list) or isinstance(i,tuple):
+                            add += i
+                        else:
+                            add.append(i)
+                    new_data = list(add[:])
+                else:
+                    break
+        return count
+
+    def slope(self):
+        x = [i for i in range(1,len(self.data)+1)]
+        hits = lotl([(self.data[i+1]- self.data[i]) / (x[i+1] - x[i])  for i in range(len(self.data)-1)]).mean()
+        return hits
