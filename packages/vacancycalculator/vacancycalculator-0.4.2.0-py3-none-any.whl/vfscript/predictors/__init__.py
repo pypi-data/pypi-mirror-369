2f4cdf18c943dab362df93be64e3fda02862cf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 05:05:56 +0000

    Add package vacancycalculator/0.4.2.0/vacancycalculator-0.4.2.0-py3-none-any.whl

---
+++ packages/vacancycalculator/vacancycalculator-0.4.2.0-py3-none-any.whl/vfscript/predictors/__init__.py
@@ -0,0 +1,6 @@
+from . import vacancy_predictors_classified
+from . import vacancy_predictors
+from . import mach_finger
+from . import coeff_vacancy
+from . import vancy_keras
+from . import csv_vancy_keras
