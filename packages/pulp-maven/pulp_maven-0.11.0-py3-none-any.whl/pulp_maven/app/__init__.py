854f61b34b91b07bb8e8fa6be1db533655a7f277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 04:11:39 +0000

    Add package pulp-maven/0.11.0/pulp_maven-0.11.0-py3-none-any.whl

---
+++ packages/pulp-maven/pulp_maven-0.11.0-py3-none-any.whl/pulp_maven/app/__init__.py
@@ -0,0 +1,11 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpMavenPluginAppConfig(PulpPluginAppConfig):
+    """Entry point for the maven plugin."""
+
+    name = "pulp_maven.app"
+    label = "maven"
+    version = "0.11.0"
+    python_package_name = "pulp-maven"
+    domain_compatible = True
