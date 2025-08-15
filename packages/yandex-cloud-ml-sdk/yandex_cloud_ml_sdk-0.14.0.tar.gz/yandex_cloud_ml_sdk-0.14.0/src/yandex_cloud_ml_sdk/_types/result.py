68d4817ad1fc9e4a2eb106ded7f4be4c4f063784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 04:11:57 +0000

    Add package yandex-cloud-ml-sdk/0.14.0/yandex_cloud_ml_sdk-0.14.0.tar.gz

---
+++ packages/yandex-cloud-ml-sdk/yandex_cloud_ml_sdk-0.14.0.tar.gz/yandex_cloud_ml_sdk-0.14.0/src/yandex_cloud_ml_sdk/_types/result.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .proto import ProtoBased, ProtoMessage, ProtoMessageTypeT_contra, SDKType
+
+# it is left here until further refactoring
+__all__ = ['ProtoMessage', 'BaseProtoResult', 'SDKType']
+
+
+class BaseProtoResult(ProtoBased[ProtoMessageTypeT_contra]):
+    pass
