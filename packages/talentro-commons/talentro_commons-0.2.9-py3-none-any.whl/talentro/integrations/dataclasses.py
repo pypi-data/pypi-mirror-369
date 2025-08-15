afc209f7ab81884131ae21abccb22456125c5596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 04:11:34 +0000

    Add package talentro-commons/0.2.9/talentro_commons-0.2.9-py3-none-any.whl

---
+++ packages/talentro-commons/talentro_commons-0.2.9-py3-none-any.whl/talentro/integrations/dataclasses.py
@@ -0,0 +1,57 @@
+from uuid import UUID
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from .models import Link as LinkModel, Integration as IntegrationModel
+
+
+class Integration(BaseModel):
+    id: UUID
+    created_at: datetime
+    updated_at: Optional[datetime]
+
+    name: str
+    icon: str
+    type: str
+    tag: str
+    enabled: bool
+    description: str
+
+    @classmethod
+    def from_model(cls: "Integration", model: IntegrationModel) -> "Integration":
+        return cls(
+            id=model.id,
+            created_at=model.created_at,
+            updated_at=model.updated_at,
+            name=model.name,
+            icon=model.icon,
+            type=model.type,
+            tag=model.tag,
+            enabled=model.enabled,
+            description=model.description,
+        )
+
+
+class Link(BaseModel):
+    id: UUID
+    created_at: datetime
+    updated_at: Optional[datetime]
+    organization: UUID
+
+    name: str
+    status: str
+    integration: Integration
+
+    @classmethod
+    def from_model(cls: "Link", model: LinkModel, integration: Integration) -> "Link":
+        return cls(
+            id=model.id,
+            created_at=model.created_at,
+            updated_at=model.updated_at,
+            organization=model.organization,
+            name=model.name,
+            status=model.status,
+            integration=integration,
+        )
