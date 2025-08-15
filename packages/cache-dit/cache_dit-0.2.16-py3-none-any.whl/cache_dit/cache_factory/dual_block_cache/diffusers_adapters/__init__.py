d26abb7d99b0cf1ef85a361c8882cbc7bce38d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:20 +0000

    Add package cache-dit/0.2.16/cache_dit-0.2.16-py3-none-any.whl

---
+++ packages/cache-dit/cache_dit-0.2.16-py3-none-any.whl/cache_dit/cache_factory/dual_block_cache/diffusers_adapters/__init__.py
@@ -0,0 +1,55 @@
+import importlib
+
+from diffusers import DiffusionPipeline
+
+
+def apply_db_cache_on_transformer(transformer, *args, **kwargs):
+    transformer_cls_name: str = transformer.__class__.__name__
+    if transformer_cls_name.startswith("Flux"):
+        adapter_name = "flux"
+    elif transformer_cls_name.startswith("Mochi"):
+        adapter_name = "mochi"
+    elif transformer_cls_name.startswith("CogVideoX"):
+        adapter_name = "cogvideox"
+    elif transformer_cls_name.startswith("Wan"):
+        adapter_name = "wan"
+    elif transformer_cls_name.startswith("HunyuanVideo"):
+        adapter_name = "hunyuan_video"
+    elif transformer_cls_name.startswith("QwenImage"):
+        adapter_name = "qwen_image"
+    else:
+        raise ValueError(
+            f"Unknown transformer class name: {transformer_cls_name}"
+        )
+
+    adapter_module = importlib.import_module(f".{adapter_name}", __package__)
+    apply_db_cache_on_transformer_fn = getattr(
+        adapter_module, "apply_db_cache_on_transformer"
+    )
+    return apply_db_cache_on_transformer_fn(transformer, *args, **kwargs)
+
+
+def apply_db_cache_on_pipe(pipe: DiffusionPipeline, *args, **kwargs):
+    assert isinstance(pipe, DiffusionPipeline)
+
+    pipe_cls_name: str = pipe.__class__.__name__
+    if pipe_cls_name.startswith("Flux"):
+        adapter_name = "flux"
+    elif pipe_cls_name.startswith("Mochi"):
+        adapter_name = "mochi"
+    elif pipe_cls_name.startswith("CogVideoX"):
+        adapter_name = "cogvideox"
+    elif pipe_cls_name.startswith("Wan"):
+        adapter_name = "wan"
+    elif pipe_cls_name.startswith("HunyuanVideo"):
+        adapter_name = "hunyuan_video"
+    elif pipe_cls_name.startswith("QwenImage"):
+        adapter_name = "qwen_image"
+    else:
+        raise ValueError(f"Unknown pipeline class name: {pipe_cls_name}")
+
+    adapter_module = importlib.import_module(f".{adapter_name}", __package__)
+    apply_db_cache_on_pipe_fn = getattr(
+        adapter_module, "apply_db_cache_on_pipe"
+    )
+    return apply_db_cache_on_pipe_fn(pipe, *args, **kwargs)
---
+++ packages/cache-dit/cache_dit-0.2.16-py3-none-any.whl/cache_dit/cache_factory/dynamic_block_prune/diffusers_adapters/wan.py
@@ -0,0 +1,100 @@
+import functools
+import unittest
+
+import torch
+from diffusers import DiffusionPipeline, WanTransformer3DModel
+
+from cache_dit.cache_factory.dynamic_block_prune import (
+    prune_context,
+    DBPrunedTransformerBlocks,
+)
+
+
+def apply_db_prune_on_transformer(
+    transformer: WanTransformer3DModel,
+):
+    if getattr(transformer, "_is_pruned", False):
+        return transformer
+
+    blocks = torch.nn.ModuleList(
+        [
+            DBPrunedTransformerBlocks(
+                transformer.blocks,
+                transformer=transformer,
+                return_hidden_states_only=True,
+            )
+        ]
+    )
+
+    original_forward = transformer.forward
+
+    @functools.wraps(transformer.__class__.forward)
+    def new_forward(
+        self,
+        *args,
+        **kwargs,
+    ):
+        with unittest.mock.patch.object(
+            self,
+            "blocks",
+            blocks,
+        ):
+            return original_forward(
+                *args,
+                **kwargs,
+            )
+
+    transformer.forward = new_forward.__get__(transformer)
+
+    transformer._is_pruned = True
+
+    return transformer
+
+
+def apply_cache_on_pipe(
+    pipe: DiffusionPipeline,
+    *,
+    shallow_patch: bool = False,
+    residual_diff_threshold=0.03,
+    downsample_factor=1,
+    # SLG is not supported in WAN with DBPrune yet
+    # slg_layers=None,
+    # slg_start: float = 0.0,
+    # slg_end: float = 0.1,
+    warmup_steps=0,
+    max_cached_steps=-1,
+    **kwargs,
+):
+    cache_kwargs, kwargs = prune_context.collect_prune_kwargs(
+        default_attrs={
+            "residual_diff_threshold": residual_diff_threshold,
+            "downsample_factor": downsample_factor,
+            # "enable_alter_cache": True,
+            # "slg_layers": slg_layers,
+            # "slg_start": slg_start,
+            # "slg_end": slg_end,
+            "num_inference_steps": kwargs.get("num_inference_steps", 50),
+            "warmup_steps": warmup_steps,
+            "max_cached_steps": max_cached_steps,
+        },
+        **kwargs,
+    )
+    if not getattr(pipe, "_is_pruned", False):
+        original_call = pipe.__class__.__call__
+
+        @functools.wraps(original_call)
+        def new_call(self, *args, **kwargs):
+            with prune_context.prune_context(
+                prune_context.create_prune_context(
+                    **cache_kwargs,
+                )
+            ):
+                return original_call(self, *args, **kwargs)
+
+        pipe.__class__.__call__ = new_call
+        pipe.__class__._is_pruned = True
+
+    if not shallow_patch:
+        apply_db_prune_on_transformer(pipe.transformer, **kwargs)
+
+    return pipe
