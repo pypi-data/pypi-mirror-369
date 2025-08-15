9889cb0ceeb323880c100b7e765cc090ec0290bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 14:06:56 +0000

    Add package lmmy/0.0.1/lmmy-0.0.1.tar.gz

---
+++ packages/lmmy/lmmy-0.0.1.tar.gz/lmmy-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='lmmy',
+    version='0.0.1',
+    description='lmmy',
+    author='LM Studio',
+    author_email='team@lmstudio.ai',
+    zip_safe=False
+)
