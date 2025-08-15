0387f31ad5f61b6f8ace104686cadaa1f3110f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:59 +0000

    Add package neurocaps/0.35.2/neurocaps-0.35.2-py3-none-any.whl

---
+++ packages/neurocaps/neurocaps-0.35.2-py3-none-any.whl/neurocaps/__init__.py
@@ -0,0 +1,7 @@
+from . import analysis, extraction, exceptions, utils
+
+# Don't include "typing"
+__all__ = ["analysis", "extraction", "exceptions", "utils"]
+
+# Version in single place
+__version__ = "0.35.2"
---
+++ packages/neurocaps/neurocaps-0.35.2-py3-none-any.whl/neurocaps/utils/_helpers.py
@@ -0,0 +1,27 @@
+"""Internal helper functions."""
+
+from copy import deepcopy
+from typing import Any
+
+from ._logging import setup_logger
+
+LG = setup_logger(__name__)
+
+
+def resolve_kwargs(defaults: dict[str, Any], **kwargs) -> dict[str, Any]:
+    plot_dict = deepcopy(defaults)
+    plot_dict.update({k: v for k, v in kwargs.items() if k in plot_dict})
+
+    if kwargs:
+        invalid_kwargs = {k: v for k, v in kwargs.items() if k not in plot_dict}
+        if invalid_kwargs:
+            LG.info(
+                f"The following invalid kwargs arguments used and will be ignored: {invalid_kwargs}"
+            )
+
+    return plot_dict
+
+
+def list_to_str(str_list: list[str]) -> None:
+    """Converts a list containing strings to a string."""
+    return ", ".join(["'{a}'".format(a=x) for x in str_list])
