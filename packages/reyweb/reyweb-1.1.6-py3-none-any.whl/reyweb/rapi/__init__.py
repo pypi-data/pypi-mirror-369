f6bd42846fcb053b299d552d3643cc2b4acc52d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:52 +0000

    Add package reyweb/1.1.6/reyweb-1.1.6-py3-none-any.whl

---
+++ packages/reyweb/reyweb-1.1.6-py3-none-any.whl/reyweb/rapi/__init__.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024-01-11 21:55:48
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : External API methods.
+
+Modules
+-------
+rbaidu : Baidu API methods.
+"""
+
+
+from .rali import *
+from .rbaidu import *
