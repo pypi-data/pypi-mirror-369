351aa95aa18c0949b26f8be5ac8f2f6d1345b8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 03:29:43 +0000

    Add package plotmath/0.2.28/plotmath-0.2.28.tar.gz

---
+++ packages/plotmath/plotmath-0.2.28.tar.gz/plotmath-0.2.28/plotmath/__init__.py
@@ -0,0 +1,21 @@
+from .plotmath import (
+    plot,
+    show,
+    savefig,
+    histogram,
+    plot_polygon,
+    multiplot,
+    gca,
+    COLORS,
+)
+
+__all__ = [
+    "plot",
+    "savefig",
+    "show",
+    "histogram",
+    "plot_polygon",
+    "multiplot",
+    "gca",
+    "COLORS",
+]
