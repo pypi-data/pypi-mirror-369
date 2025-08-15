4a470950c13d9ce0292b1b160fa574a832b7ebc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 03:29:36 +0000

    Add package moneyball/0.0.204/moneyball-0.0.204.tar.gz

---
+++ packages/moneyball/moneyball-0.0.204.tar.gz/moneyball-0.0.204/moneyball/portfolio/bet.py
@@ -0,0 +1,20 @@
+"""The bet dictionary format."""
+
+from typing import Any, TypedDict
+
+from .team import Team
+
+Bet = TypedDict(
+    "Bet",
+    {
+        "strategy": str,
+        "league": str,
+        "kelly": float,
+        "weight": float,
+        "amount": float,
+        "teams": list[Team],
+        "dt": str,
+        "row": dict[str, Any],
+        "importances": dict[str, float],
+    },
+)
