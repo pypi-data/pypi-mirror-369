ec567bc1188f7842aae35d3e802fd6f3418b87f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:57 +0000

    Add package PySrDaliGateway/0.6.0/pysrdaligateway-0.6.0-py3-none-any.whl

---
+++ packages/PySrDaliGateway/pysrdaligateway-0.6.0-py3-none-any.whl/PySrDaliGateway/__init__.py
@@ -0,0 +1,22 @@
+"""Dali Gateway"""
+# pylint: disable=invalid-name
+
+from .__version__ import __version__
+from .gateway import DaliGateway
+from .device import Device
+from .group import Group
+from .scene import Scene
+from .types import SceneType, GroupType, DeviceType, DaliGatewayType
+
+
+__all__ = [
+    "__version__",
+    "DaliGateway",
+    "Device",
+    "Group",
+    "Scene",
+    "DeviceType",
+    "GroupType",
+    "SceneType",
+    "DaliGatewayType",
+]
