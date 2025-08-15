c4f8547c58178997cf31be1f68a7b6a51971a3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:22 +0000

    Add package cucll-learn/0.1.1/cucll_learn-0.1.1-py3-none-any.whl

---
+++ packages/cucll-learn/cucll_learn-0.1.1-py3-none-any.whl/cucll/learn/_imports.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+
+OUTLIER_THRESHOLD = 3.0
+k = 1.5
+
+
+__all__ = []  # private
