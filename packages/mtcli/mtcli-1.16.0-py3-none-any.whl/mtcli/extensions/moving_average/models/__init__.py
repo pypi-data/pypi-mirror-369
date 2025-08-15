9d98ededfb6bd122b69e2dd52fd6f27b7681670f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:06:10 +0000

    Add package mtcli/1.16.0/mtcli-1.16.0-py3-none-any.whl

---
+++ packages/mtcli/mtcli-1.16.0-py3-none-any.whl/mtcli/extensions/moving_average/models/__init__.py
@@ -0,0 +1,1 @@
+from . import model_ma, model_mas, model_rates
---
+++ packages/mtcli/mtcli-1.16.0-py3-none-any.whl/mtcli/views/view_high.py
@@ -0,0 +1,42 @@
+"""Módulo da classe da view das máximas."""
+
+from mtcli import conf
+from mtcli.models import model_chart
+
+
+class HighView:
+    """Classe da view das máximas."""
+
+    def __init__(
+        self, bars, count, period="d1", date="", numerator=False, show_date=False
+    ):
+        """View das máximas."""
+        self.count = count
+        self.period = period
+        self.date = date
+        self.numerator = numerator
+        self.show_date = show_date
+        self.chart = model_chart.ChartModel(bars, len(bars), count, date)
+        self.bars = bars[-count:]
+
+    def views(self):
+        """Lista das views de máximas."""
+        views = []
+        n = self.chart.get_n()
+        for bar in self.bars:
+            n += 1
+            if self.numerator or self.show_date:
+                view = "%s "  # numerador ou data
+            else:
+                view = ""
+            view += "%." + str(conf.digitos) + "f"  # máxima
+            if self.show_date:
+                if self.period == "d1" or self.period == "w1" or self.period == "mn1":
+                    views.append(view % (bar.date, bar.high))
+                else:
+                    views.append(view % (bar.time, bar.high))
+            elif self.numerator:
+                views.append(view % (n, bar.high))
+            else:
+                views.append(view % (bar.high))
+        return views
