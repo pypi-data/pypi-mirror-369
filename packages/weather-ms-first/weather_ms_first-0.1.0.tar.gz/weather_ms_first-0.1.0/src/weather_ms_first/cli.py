1d8313abfda223dd7cb0a2edf9cd114a2fa2ed50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:06:09 +0000

    Add package weather-ms-first/0.1.0/weather_ms_first-0.1.0.tar.gz

---
+++ packages/weather-ms-first/weather_ms_first-0.1.0.tar.gz/weather_ms_first-0.1.0/src/weather_ms_first/cli.py
@@ -0,0 +1,4 @@
+from .server import add
+
+def main():
+    print(f"add(1, 2) = {add(1, 2)}")
---
+++ packages/weather-ms-first/weather_ms_first-0.1.0.tar.gz/weather_ms_first-0.1.0/src/weather_ms_first/server.py
@@ -0,0 +1,14 @@
+
+
+def add(a: int, b: int) -> int:
+    """
+    Add two numbers together.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of the two numbers.
+    """
+    return a + b
