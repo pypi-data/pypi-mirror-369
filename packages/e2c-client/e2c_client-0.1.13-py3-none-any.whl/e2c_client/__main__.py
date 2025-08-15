6cf081ab02108f6c42d0c8e8ec953c5378e92096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:49 +0000

    Add package e2c-client/0.1.13/e2c_client-0.1.13-py3-none-any.whl

---
+++ packages/e2c-client/e2c_client-0.1.13-py3-none-any.whl/e2c_client/__main__.py
@@ -0,0 +1,46 @@
+import typer
+from typing import Optional
+
+from e2c_client import __version__
+
+# Commands and subcommands import
+from e2c_client.cli import monitor
+from e2c_client.cli import control
+
+# Command-line application based on Typer
+app = typer.Typer(add_completion=False)
+
+# Commands and subcommands definition divided by subsystem or functionality set
+app.add_typer(monitor.monitor_app, name="monitor")
+app.add_typer(control.control_app, name="control")
+
+
+@app.callback()
+def callback():
+    """
+    E2C client commands
+
+    Usage example (not all commands shown. Use --help for complete list):
+
+    \b
+    e2c-client --help
+    e2c-client version
+    e2c-client monitor --help
+    e2c-client monitor request --host <host> --channel <channel> --node <node> --rca <RCA>
+    e2c-client monitor --help
+    e2c-client control request --host <host> --channel <channel> --node <node> --rca <RCA> --data <data>
+
+    """
+
+
+@app.command(short_help="Show current version")
+def version():
+    print(__version__)
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
