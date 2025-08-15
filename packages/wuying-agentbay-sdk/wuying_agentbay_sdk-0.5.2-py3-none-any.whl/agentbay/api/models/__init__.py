7b11cc53903e6cd357f2aeb0e4e6cc3618f1290c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:59 +0000

    Add package wuying-agentbay-sdk/0.5.2/wuying_agentbay_sdk-0.5.2-py3-none-any.whl

---
+++ packages/wuying-agentbay-sdk/wuying_agentbay_sdk-0.5.2-py3-none-any.whl/agentbay/api/models/__init__.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from __future__ import annotations
+
+
+
+from ._call_mcp_tool_request import CallMcpToolRequest
+from ._call_mcp_tool_response_body import CallMcpToolResponseBody
+from ._call_mcp_tool_response import CallMcpToolResponse
+from ._create_mcp_session_request import CreateMcpSessionRequest
+from ._create_mcp_session_shrink_request import CreateMcpSessionShrinkRequest
+from ._create_mcp_session_response_body import CreateMcpSessionResponseBody
+from ._create_mcp_session_response import CreateMcpSessionResponse
+from ._delete_context_request import DeleteContextRequest
+from ._delete_context_response_body import DeleteContextResponseBody
+from ._delete_context_response import DeleteContextResponse
+from ._get_context_request import GetContextRequest
+from ._get_context_response_body import GetContextResponseBody
+from ._get_context_response import GetContextResponse
+from ._get_context_info_request import GetContextInfoRequest
+from ._get_context_info_response_body import GetContextInfoResponseBody
+from ._get_context_info_response import GetContextInfoResponse
+from ._get_label_request import GetLabelRequest
+from ._get_label_response_body import GetLabelResponseBody
+from ._get_label_response import GetLabelResponse
+from ._get_link_request import GetLinkRequest
+from ._get_link_response_body import GetLinkResponseBody
+from ._get_link_response import GetLinkResponse
+from ._get_mcp_resource_request import GetMcpResourceRequest
+from ._get_mcp_resource_response_body import GetMcpResourceResponseBody
+from ._get_mcp_resource_response import GetMcpResourceResponse
+from ._init_browser_request import InitBrowserRequest
+from ._init_browser_response_body import InitBrowserResponseBody
+from ._init_browser_response import InitBrowserResponse
+from ._list_contexts_request import ListContextsRequest
+from ._list_contexts_response_body import ListContextsResponseBody
+from ._list_contexts_response import ListContextsResponse
+from ._list_mcp_tools_request import ListMcpToolsRequest
+from ._list_mcp_tools_response_body import ListMcpToolsResponseBody
+from ._list_mcp_tools_response import ListMcpToolsResponse
+from ._list_session_request import ListSessionRequest
+from ._list_session_response_body import ListSessionResponseBody
+from ._list_session_response import ListSessionResponse
+from ._modify_context_request import ModifyContextRequest
+from ._modify_context_response_body import ModifyContextResponseBody
+from ._modify_context_response import ModifyContextResponse
+from ._release_mcp_session_request import ReleaseMcpSessionRequest
+from ._release_mcp_session_response_body import ReleaseMcpSessionResponseBody
+from ._release_mcp_session_response import ReleaseMcpSessionResponse
+from ._set_label_request import SetLabelRequest
+from ._set_label_response_body import SetLabelResponseBody
+from ._set_label_response import SetLabelResponse
+from ._sync_context_request import SyncContextRequest
+from ._sync_context_response_body import SyncContextResponseBody
+from ._sync_context_response import SyncContextResponse
+from ._create_mcp_session_request import CreateMcpSessionRequestPersistenceDataList
+from ._create_mcp_session_response_body import CreateMcpSessionResponseBodyData
+from ._get_context_response_body import GetContextResponseBodyData
+from ._get_context_info_response_body import GetContextInfoResponseBodyData
+from ._get_label_response_body import GetLabelResponseBodyData
+from ._get_link_response_body import GetLinkResponseBodyData
+from ._get_mcp_resource_response_body import GetMcpResourceResponseBodyDataDesktopInfo
+from ._get_mcp_resource_response_body import GetMcpResourceResponseBodyData
+from ._init_browser_response_body import InitBrowserResponseBodyData
+from ._list_contexts_response_body import ListContextsResponseBodyData
+from ._list_session_response_body import ListSessionResponseBodyData
+
+__all__ = [
+    CallMcpToolRequest,
+    CallMcpToolResponseBody,
+    CallMcpToolResponse,
+    CreateMcpSessionRequest,
+    CreateMcpSessionShrinkRequest,
+    CreateMcpSessionResponseBody,
+    CreateMcpSessionResponse,
+    DeleteContextRequest,
+    DeleteContextResponseBody,
+    DeleteContextResponse,
+    GetContextRequest,
+    GetContextResponseBody,
+    GetContextResponse,
+    GetContextInfoRequest,
+    GetContextInfoResponseBody,
+    GetContextInfoResponse,
+    GetLabelRequest,
+    GetLabelResponseBody,
+    GetLabelResponse,
+    GetLinkRequest,
+    GetLinkResponseBody,
+    GetLinkResponse,
+    GetMcpResourceRequest,
+    GetMcpResourceResponseBody,
+    GetMcpResourceResponse,
+    InitBrowserRequest,
+    InitBrowserResponseBody,
+    InitBrowserResponse,
+    ListContextsRequest,
+    ListContextsResponseBody,
+    ListContextsResponse,
+    ListMcpToolsRequest,
+    ListMcpToolsResponseBody,
+    ListMcpToolsResponse,
+    ListSessionRequest,
+    ListSessionResponseBody,
+    ListSessionResponse,
+    ModifyContextRequest,
+    ModifyContextResponseBody,
+    ModifyContextResponse,
+    ReleaseMcpSessionRequest,
+    ReleaseMcpSessionResponseBody,
+    ReleaseMcpSessionResponse,
+    SetLabelRequest,
+    SetLabelResponseBody,
+    SetLabelResponse,
+    SyncContextRequest,
+    SyncContextResponseBody,
+    SyncContextResponse,
+    CreateMcpSessionRequestPersistenceDataList,
+    CreateMcpSessionResponseBodyData,
+    GetContextResponseBodyData,
+    GetContextInfoResponseBodyData,
+    GetLabelResponseBodyData,
+    GetLinkResponseBodyData,
+    GetMcpResourceResponseBodyDataDesktopInfo,
+    GetMcpResourceResponseBodyData,
+    InitBrowserResponseBodyData,
+    ListContextsResponseBodyData,
+    ListSessionResponseBodyData
+]
