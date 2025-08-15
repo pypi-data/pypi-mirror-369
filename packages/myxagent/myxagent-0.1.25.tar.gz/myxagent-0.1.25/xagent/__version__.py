b523e346e93c2f6ccf7ad3907c448c0bef736429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 15:06:39 +0000

    Add package myxagent/0.1.25/myxagent-0.1.25.tar.gz

---
+++ packages/myxagent/myxagent-0.1.25.tar.gz/myxagent-0.1.25/xagent/__version__.py
@@ -0,0 +1,6 @@
+"""Version information for xAgent package."""
+
+__version__ = "0.1.15"
+__author__ = "xAgent Team"
+__email__ = "zhangjun310@live.com"
+__description__ = "Multi-Modal AI Agent System"
