01d0c5d9904e4194e829e79eaa41830579f9cbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:06:04 +0000

    Add package VegasAfterglow/0.2.8/vegasafterglow-0.2.8-cp38-cp38-win_amd64.whl

---
+++ packages/VegasAfterglow/vegasafterglow-0.2.8-cp38-cp38-win_amd64.whl/VegasAfterglow/__init__.py
@@ -0,0 +1,25 @@
+from .types import ModelParams, Setups, ObsData, VegasMC, FitResult, ParamDef, Scale, TophatJet, GaussianJet, PowerLawJet, TwoComponentJet, ISM, Wind, Medium, Ejecta, Model, Radiation, Observer, Magnetar
+from .runner import Fitter
+
+__all__ = [
+    "ModelParams",
+    "Setups",
+    "ObsData",
+    "VegasMC",
+    "FitResult",
+    "Fitter",
+    "ParamDef",
+    "Scale",
+    "ISM",
+    "Wind",
+    "Medium",
+    "TophatJet",
+    "GaussianJet",
+    "PowerLawJet",
+    "TwoComponentJet",
+    "Ejecta",
+    "Model",
+    "Radiation",
+    "Observer",
+    "Magnetar",
+]
