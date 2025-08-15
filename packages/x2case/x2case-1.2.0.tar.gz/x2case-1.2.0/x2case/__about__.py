712b03f120d56892867cbc0c5b30216a3bfc7d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 14:06:30 +0000

    Add package x2case/1.2.0/x2case-1.2.0.tar.gz

---
+++ packages/x2case/x2case-1.2.0.tar.gz/x2case-1.2.0/x2case/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'XMind2Jira'
+__description__ = 'XMind2Jira 基于Python实现， Xmind 文件转jira 用例'
+__keywords__ = 'xmind2jira, testcase, xmind, 思维导图, XMind思维导图',
+__url__ = 'https://github.com/Allenzzz/x2case'
+__author__ = 'Zeno'
+__author_email__ = 'z_ok@me.com'
+__version__ = '1.1.0'
+__license__ = 'MIT'
