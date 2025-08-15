8a4ff235a4fc245954442e6dacc319f20a852347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 07:07:16 +0000

    Add package omlish/0.0.0.dev396/omlish-0.0.0.dev396-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev396-py3-none-any.whl/omlish/specs/jsonrpc/__init__.py
@@ -0,0 +1,41 @@
+from .errors import (  # noqa
+    KnownError,
+    KnownErrors,
+
+    CUSTOM_ERROR_BASE,
+)
+
+from .types import (  # noqa
+    NUMBER_TYPES,
+    Number,
+    Object,
+    ID_TYPES,
+    Id,
+
+    VERSION,
+
+    NotSpecified,
+    is_not_specified,
+    check_not_not_specified,
+
+    Request,
+    request,
+    notification,
+
+    Response,
+    result,
+
+    Error,
+    error,
+
+    Message,
+    detect_message_type,
+)
+
+
+##
+
+
+from ...lang.imports import _register_conditional_import  # noqa
+
+_register_conditional_import('...marshal', '.marshal', __package__)
