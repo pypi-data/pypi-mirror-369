e32d5d1afb5c1f26cd67c26ebb05632ac0815774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 06:06:46 +0000

    Add package async-pyserial/0.2.4/async_pyserial-0.2.4-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/async-pyserial/async_pyserial-0.2.4-cp313-cp313-macosx_11_0_arm64.whl/async_pyserial/__init__.py
@@ -0,0 +1,24 @@
+import sys
+
+from async_pyserial.common import *
+
+from async_pyserial.backend import set_async_worker
+
+__version__ = '0.2.4'
+
+VERSION = __version__
+
+__all__ = ["SerialPort", "SerialPortOptions", "SerialPortEvent", 
+           "SerialPortParity", "set_async_worker", "SerialPortError"]
+
+sys_platform = sys.platform
+    
+if sys_platform  == 'win32' or \
+    sys_platform == 'linux' or \
+    sys_platform == 'darwin' or \
+    'bsd' in sys_platform.lower():
+
+    from async_pyserial.native_serialport import SerialPort
+
+else:
+    raise PlatformNotSupported()
