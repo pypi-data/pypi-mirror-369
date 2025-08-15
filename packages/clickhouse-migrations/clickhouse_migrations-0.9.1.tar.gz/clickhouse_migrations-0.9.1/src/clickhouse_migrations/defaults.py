924fdd548fa8247b812a079ebe57a4db3bec208b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 16:05:45 +0000

    Add package clickhouse-migrations/0.9.1/clickhouse_migrations-0.9.1.tar.gz

---
+++ packages/clickhouse-migrations/clickhouse_migrations-0.9.1.tar.gz/clickhouse_migrations-0.9.1/src/clickhouse_migrations/defaults.py
@@ -0,0 +1,12 @@
+import os
+from os.path import abspath
+from pathlib import Path
+
+DB_HOST = "localhost"
+DB_PORT = 9000
+DB_NAME = "default"
+DB_USER = "default"
+DB_PASSWORD = ""
+MIGRATIONS_DIR = abspath(Path(os.getcwd()) / "migrations")
+
+DB_URL = "clickhouse://default:@localhost:9000/db_placeholder"
