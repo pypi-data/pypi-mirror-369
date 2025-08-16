d61e7b2694e9267d79b6c05f6e31ba2941b50c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:06:30 +0000

    Add package doppler-secrets/0.4.0/doppler_secrets-0.4.0.tar.gz

---
+++ packages/doppler-secrets/doppler_secrets-0.4.0.tar.gz/doppler_secrets-0.4.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['doppler']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cachetools>=6.1.0,<7.0.0', 'requests>=2.32.4,<3.0.0']
+
+setup_kwargs = {
+    'name': 'doppler-secrets',
+    'version': '0.4.0',
+    'description': 'Dead simple Doppler client',
+    'long_description': 'doppler-secrets\n===============\n\n\n',
+    'author': 'Fede Calendino',
+    'author_email': 'fede@calendino.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/fedecalendino/doppler-secrets',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
