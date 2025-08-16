f97ca1ed666e37a7f3a877d7af8cf4d194d2e69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:06:22 +0000

    Add package types-aiobotocore-sesv2/2.24.1/types_aiobotocore_sesv2-2.24.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-sesv2/types_aiobotocore_sesv2-2.24.1-py3-none-any.whl/types_aiobotocore_sesv2/__init__.py
@@ -0,0 +1,34 @@
+"""
+Main interface for sesv2 service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sesv2/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sesv2 import (
+        Client,
+        ListMultiRegionEndpointsPaginator,
+        SESV2Client,
+    )
+
+    session = get_session()
+    async with session.create_client("sesv2") as client:
+        client: SESV2Client
+        ...
+
+
+    list_multi_region_endpoints_paginator: ListMultiRegionEndpointsPaginator = client.get_paginator("list_multi_region_endpoints")
+    ```
+"""
+
+from .client import SESV2Client
+from .paginator import ListMultiRegionEndpointsPaginator
+
+Client = SESV2Client
+
+
+__all__ = ("Client", "ListMultiRegionEndpointsPaginator", "SESV2Client")
