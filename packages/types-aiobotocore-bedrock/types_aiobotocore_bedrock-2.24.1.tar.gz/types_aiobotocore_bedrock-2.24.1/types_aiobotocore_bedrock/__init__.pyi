04e24d21d932739dcbae8a60f88fcb577e0f9aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 14:06:14 +0000

    Add package types-aiobotocore-bedrock/2.24.1/types_aiobotocore_bedrock-2.24.1.tar.gz

---
+++ packages/types-aiobotocore-bedrock/types_aiobotocore_bedrock-2.24.1.tar.gz/types_aiobotocore_bedrock-2.24.1/types_aiobotocore_bedrock/__init__.pyi
@@ -0,0 +1,87 @@
+"""
+Main interface for bedrock service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_bedrock import (
+        BedrockClient,
+        Client,
+        ListCustomModelDeploymentsPaginator,
+        ListCustomModelsPaginator,
+        ListEvaluationJobsPaginator,
+        ListGuardrailsPaginator,
+        ListImportedModelsPaginator,
+        ListInferenceProfilesPaginator,
+        ListMarketplaceModelEndpointsPaginator,
+        ListModelCopyJobsPaginator,
+        ListModelCustomizationJobsPaginator,
+        ListModelImportJobsPaginator,
+        ListModelInvocationJobsPaginator,
+        ListPromptRoutersPaginator,
+        ListProvisionedModelThroughputsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("bedrock") as client:
+        client: BedrockClient
+        ...
+
+
+    list_custom_model_deployments_paginator: ListCustomModelDeploymentsPaginator = client.get_paginator("list_custom_model_deployments")
+    list_custom_models_paginator: ListCustomModelsPaginator = client.get_paginator("list_custom_models")
+    list_evaluation_jobs_paginator: ListEvaluationJobsPaginator = client.get_paginator("list_evaluation_jobs")
+    list_guardrails_paginator: ListGuardrailsPaginator = client.get_paginator("list_guardrails")
+    list_imported_models_paginator: ListImportedModelsPaginator = client.get_paginator("list_imported_models")
+    list_inference_profiles_paginator: ListInferenceProfilesPaginator = client.get_paginator("list_inference_profiles")
+    list_marketplace_model_endpoints_paginator: ListMarketplaceModelEndpointsPaginator = client.get_paginator("list_marketplace_model_endpoints")
+    list_model_copy_jobs_paginator: ListModelCopyJobsPaginator = client.get_paginator("list_model_copy_jobs")
+    list_model_customization_jobs_paginator: ListModelCustomizationJobsPaginator = client.get_paginator("list_model_customization_jobs")
+    list_model_import_jobs_paginator: ListModelImportJobsPaginator = client.get_paginator("list_model_import_jobs")
+    list_model_invocation_jobs_paginator: ListModelInvocationJobsPaginator = client.get_paginator("list_model_invocation_jobs")
+    list_prompt_routers_paginator: ListPromptRoutersPaginator = client.get_paginator("list_prompt_routers")
+    list_provisioned_model_throughputs_paginator: ListProvisionedModelThroughputsPaginator = client.get_paginator("list_provisioned_model_throughputs")
+    ```
+"""
+
+from .client import BedrockClient
+from .paginator import (
+    ListCustomModelDeploymentsPaginator,
+    ListCustomModelsPaginator,
+    ListEvaluationJobsPaginator,
+    ListGuardrailsPaginator,
+    ListImportedModelsPaginator,
+    ListInferenceProfilesPaginator,
+    ListMarketplaceModelEndpointsPaginator,
+    ListModelCopyJobsPaginator,
+    ListModelCustomizationJobsPaginator,
+    ListModelImportJobsPaginator,
+    ListModelInvocationJobsPaginator,
+    ListPromptRoutersPaginator,
+    ListProvisionedModelThroughputsPaginator,
+)
+
+Client = BedrockClient
+
+__all__ = (
+    "BedrockClient",
+    "Client",
+    "ListCustomModelDeploymentsPaginator",
+    "ListCustomModelsPaginator",
+    "ListEvaluationJobsPaginator",
+    "ListGuardrailsPaginator",
+    "ListImportedModelsPaginator",
+    "ListInferenceProfilesPaginator",
+    "ListMarketplaceModelEndpointsPaginator",
+    "ListModelCopyJobsPaginator",
+    "ListModelCustomizationJobsPaginator",
+    "ListModelImportJobsPaginator",
+    "ListModelInvocationJobsPaginator",
+    "ListPromptRoutersPaginator",
+    "ListProvisionedModelThroughputsPaginator",
+)
