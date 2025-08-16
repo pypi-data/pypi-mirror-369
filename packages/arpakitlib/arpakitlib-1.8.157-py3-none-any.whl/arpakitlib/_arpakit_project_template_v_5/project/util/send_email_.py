1f796536f7db0c34a0175eb7ea3dc29cc7a0328d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:24 +0000

    Add package arpakitlib/1.8.157/arpakitlib-1.8.157-py3-none-any.whl

---
+++ packages/arpakitlib/arpakitlib-1.8.157-py3-none-any.whl/arpakitlib/_arpakit_project_template_v_5/project/util/send_email_.py
@@ -0,0 +1,90 @@
+import asyncio
+import datetime as dt
+import logging
+import smtplib
+from email.message import EmailMessage
+
+import aiosmtplib
+
+from project.core.settings import get_cached_settings
+from project.core.util import setup_logging
+
+_logger = logging.getLogger(__name__)
+
+
+def sync_send_email(
+        *,
+        to_email: str,
+        subject: str = "Gamer.Market",
+        html_content: str,
+        emulate: bool = False
+):
+    to_email = to_email.strip()
+
+    if emulate:
+        _logger.info(f"emulate email sending, {to_email=}, {subject=}, {html_content=}")
+        return
+
+    message = EmailMessage()
+    message["From"] = get_cached_settings().email_smtp_user
+    message["To"] = to_email
+    message["Subject"] = subject
+    message.add_alternative(html_content, subtype="html")
+
+    with smtplib.SMTP_SSL(
+            host=get_cached_settings().email_smtp_hostname,
+            port=get_cached_settings().email_smtp_port,
+            timeout=dt.timedelta(seconds=15).total_seconds()
+
+    ) as server:
+        server.login(
+            get_cached_settings().email_smtp_user,
+            get_cached_settings().email_smtp_password
+        )
+        server.send_message(message)
+
+    _logger.info(f"email was send, {to_email=}")
+
+
+async def async_send_email(
+        *,
+        to_email: str,
+        subject: str = "Gamer.Market",
+        html_content: str,
+        emulate: bool = False
+):
+    to_email = to_email.strip()
+
+    if emulate:
+        _logger.info(f"emulate email sending, {to_email=}, {subject=}, {html_content=}")
+        return
+
+    message = EmailMessage()
+    message["From"] = get_cached_settings().email_smtp_user
+    message["To"] = to_email
+    message["Subject"] = subject
+    message.add_alternative(html_content, subtype="html")
+
+    await aiosmtplib.send(
+        message,
+        hostname=get_cached_settings().email_smtp_hostname,
+        port=get_cached_settings().email_smtp_port,
+        username=get_cached_settings().email_smtp_user,
+        password=get_cached_settings().email_smtp_password,
+        use_tls=True,
+        timeout=dt.timedelta(seconds=15).total_seconds()
+    )
+
+    _logger.info(f"email was send, {to_email=}")
+
+
+async def __async_example():
+    setup_logging()
+    await async_send_email(
+        to_email="arpakit@gmail.com",
+        html_content="Hello world 2"
+    )
+
+
+if __name__ == '__main__':
+    asyncio.run(__async_example())
