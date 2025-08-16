2a412fc17dbb0ae580f11df00c380a51936881b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:06:04 +0000

    Add package scfeatureprofiler/1.1.1/scfeatureprofiler-1.1.1-py3-none-any.whl

---
+++ packages/scfeatureprofiler/scfeatureprofiler-1.1.1-py3-none-any.whl/scfeatureprofiler/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+SingleCellFeatureProfiler: A Python package for single-cell feature expression profiling.
+"""
+
+from .api import (
+    get_feature_profiles,
+    get_feature_activity,
+    find_marker_features,
+)
+
+__all__ = [
+    "get_feature_profiles",
+    "get_feature_activity",
+    "find_marker_features",
+]
+
+__version__ = "1.1.0"
