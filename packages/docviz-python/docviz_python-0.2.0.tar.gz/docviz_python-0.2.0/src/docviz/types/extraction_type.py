c16cfa284b96840b7bd3537b21cf62d102301d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:17 +0000

    Add package docviz-python/0.2.0/docviz_python-0.2.0.tar.gz

---
+++ packages/docviz-python/docviz_python-0.2.0.tar.gz/docviz_python-0.2.0/src/docviz/types/extraction_type.py
@@ -0,0 +1,17 @@
+import enum
+
+
+class ExtractionType(enum.Enum):
+    TABLE = "table"
+    TEXT = "text"
+    FIGURE = "figure"
+    EQUATION = "equation"
+    CODE = "code"
+    REFERENCE = "reference"
+    OTHER = "other"
+
+    def __str__(self):
+        return self.value
+
+    def get_all(self):
+        return [e for e in ExtractionType]
