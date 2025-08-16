8c6c7aaa2bad5de2b7c75a95d12c677d6ca28926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:05:45 +0000

    Add package kernel/0.9.1/kernel-0.9.1-py3-none-any.whl

---
+++ packages/kernel/kernel-0.9.1-py3-none-any.whl/kernel/resources/browsers/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .fs import (
+    FsResource,
+    AsyncFsResource,
+    FsResourceWithRawResponse,
+    AsyncFsResourceWithRawResponse,
+    FsResourceWithStreamingResponse,
+    AsyncFsResourceWithStreamingResponse,
+)
+from .replays import (
+    ReplaysResource,
+    AsyncReplaysResource,
+    ReplaysResourceWithRawResponse,
+    AsyncReplaysResourceWithRawResponse,
+    ReplaysResourceWithStreamingResponse,
+    AsyncReplaysResourceWithStreamingResponse,
+)
+from .browsers import (
+    BrowsersResource,
+    AsyncBrowsersResource,
+    BrowsersResourceWithRawResponse,
+    AsyncBrowsersResourceWithRawResponse,
+    BrowsersResourceWithStreamingResponse,
+    AsyncBrowsersResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ReplaysResource",
+    "AsyncReplaysResource",
+    "ReplaysResourceWithRawResponse",
+    "AsyncReplaysResourceWithRawResponse",
+    "ReplaysResourceWithStreamingResponse",
+    "AsyncReplaysResourceWithStreamingResponse",
+    "FsResource",
+    "AsyncFsResource",
+    "FsResourceWithRawResponse",
+    "AsyncFsResourceWithRawResponse",
+    "FsResourceWithStreamingResponse",
+    "AsyncFsResourceWithStreamingResponse",
+    "BrowsersResource",
+    "AsyncBrowsersResource",
+    "BrowsersResourceWithRawResponse",
+    "AsyncBrowsersResourceWithRawResponse",
+    "BrowsersResourceWithStreamingResponse",
+    "AsyncBrowsersResourceWithStreamingResponse",
+]
---
+++ packages/kernel/kernel-0.9.1-py3-none-any.whl/kernel/types/browsers/__init__.py
@@ -0,0 +1,18 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .f_move_params import FMoveParams as FMoveParams
+from .f_file_info_params import FFileInfoParams as FFileInfoParams
+from .f_read_file_params import FReadFileParams as FReadFileParams
+from .f_list_files_params import FListFilesParams as FListFilesParams
+from .f_write_file_params import FWriteFileParams as FWriteFileParams
+from .replay_start_params import ReplayStartParams as ReplayStartParams
+from .f_delete_file_params import FDeleteFileParams as FDeleteFileParams
+from .f_file_info_response import FFileInfoResponse as FFileInfoResponse
+from .replay_list_response import ReplayListResponse as ReplayListResponse
+from .f_list_files_response import FListFilesResponse as FListFilesResponse
+from .replay_start_response import ReplayStartResponse as ReplayStartResponse
+from .f_create_directory_params import FCreateDirectoryParams as FCreateDirectoryParams
+from .f_delete_directory_params import FDeleteDirectoryParams as FDeleteDirectoryParams
+from .f_set_file_permissions_params import FSetFilePermissionsParams as FSetFilePermissionsParams
