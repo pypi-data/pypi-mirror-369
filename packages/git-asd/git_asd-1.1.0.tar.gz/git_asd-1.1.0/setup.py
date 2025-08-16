d599c8d36fb27bc9a073767a6b56f0a4686596c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 16:07:10 +0000

    Add package git-asd/1.1.0/git_asd-1.1.0.tar.gz

---
+++ packages/git-asd/git_asd-1.1.0.tar.gz/git_asd-1.1.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+setuptools.setup(
+    name="git-asd",
+    version="1.1.0",
+    author="Aditya Kumar",
+    description="a natural language git assistant for the terminal",
+    url="https://github.com/adikuma/asd",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "langchain-core",
+        "langchain-openai",
+        "langchain-google-genai",
+        "langgraph",
+        "typer[all]",
+        "rich",
+        "rich-gradient",
+        "ruff",
+        "python-dotenv",
+        "IPython",
+        "questionary",
+    ],
+    entry_points={
+        "console_scripts": [
+            "asd=asd.cli:run",
+        ],
+    },
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
