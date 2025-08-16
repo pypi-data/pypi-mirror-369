bb046bec8035de9ee384c1b90428cb5a955318dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:40 +0000

    Add package tdrpa.tdworker/1.2.17.6/tdrpa_tdworker-1.2.17.6-py39-none-win_amd64.whl

---
+++ packages/tdrpa.tdworker/tdrpa_tdworker-1.2.17.6-py39-none-win_amd64.whl/tdrpa/tdworker/__init__.pyi
@@ -0,0 +1,4 @@
+from ._excel import Excel as Excel
+from ._other import App as App, Clipboard as Clipboard, Dialog as Dialog, File as File, HTTP as HTTP, INI as INI, Image as Image, Log as Log, Mail as Mail, PrintToScreen as PrintToScreen, retry as retry
+from ._web import Web as Web
+from ._win import WinElement as WinElement, WinKeyboard as WinKeyboard, WinMouse as WinMouse, Window as Window
