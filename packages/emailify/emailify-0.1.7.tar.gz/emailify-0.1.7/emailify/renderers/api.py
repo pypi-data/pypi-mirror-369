fb7c346439c9a74fa7a43d30c76a83c1351a39b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:58 +0000

    Add package emailify/0.1.7/emailify-0.1.7.tar.gz

---
+++ packages/emailify/emailify-0.1.7.tar.gz/emailify-0.1.7/emailify/renderers/api.py
@@ -0,0 +1,69 @@
+import json
+from email.mime.application import MIMEApplication
+from functools import lru_cache
+from importlib.resources import files
+
+from quickjs import Context
+
+from emailify.models import Component
+from emailify.renderers.core import _render
+from emailify.renderers.render import render_component
+
+
+def _read_bundle_text() -> str:
+    return (
+        files("emailify")
+        .joinpath("resources", "js", "mjml-browser.js")
+        .read_text(encoding="utf-8")
+    )
+
+
+def _read_js_text(name: str) -> str:
+    return (
+        files("emailify").joinpath("resources", "js", name).read_text(encoding="utf-8")
+    )
+
+
+def _build_ctx() -> Context:
+    ctx = Context()
+    ctx.eval(_read_js_text("setup_shim.js"))
+    bundle_js = _read_bundle_text()
+    ctx.eval(bundle_js)
+    ctx.eval(_read_js_text("capture_export.js"))
+    return ctx
+
+
+@lru_cache(maxsize=1)
+def _get_ctx() -> Context:
+    return _build_ctx()
+
+
+def mjml2html(
+    mjml: str,
+    **options,
+) -> str:
+    template = _read_js_text("call_mjml.js")
+    js = template.replace("__MJML__", json.dumps(mjml)).replace(
+        "__OPTIONS__", json.dumps(options or {})
+    )
+    try:
+        return _get_ctx().eval(js)
+    except Exception as e:
+        raise RuntimeError(str(e)) from None
+
+
+def render(
+    *components: Component,
+) -> tuple[str, list[MIMEApplication]]:
+    parts: list[str] = []
+    attachments: list[MIMEApplication] = []
+    for component in components:
+        body, cur_attachments = render_component(component)
+        parts.append(body)
+        attachments.extend(cur_attachments)
+    body_str = _render(
+        "index",
+        content="".join(parts),
+    )
+    html = mjml2html(body_str)
+    return html, attachments
