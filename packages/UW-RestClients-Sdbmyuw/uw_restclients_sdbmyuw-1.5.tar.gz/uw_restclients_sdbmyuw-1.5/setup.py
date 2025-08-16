665ddf2e3d5c1ea0f82bb5194df23de0492155f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:06:14 +0000

    Add package UW-RestClients-Sdbmyuw/1.5/uw_restclients_sdbmyuw-1.5.tar.gz

---
+++ packages/UW-RestClients-Sdbmyuw/uw_restclients_sdbmyuw-1.5.tar.gz/uw_restclients_sdbmyuw-1.5/setup.py
@@ -0,0 +1,37 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from setuptools import setup
+
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/uw-restclients-sdbmyuw>`_.
+"""
+
+version_path = 'uw_sdbmyuw/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='UW-RestClients-Sdbmyuw',
+    version=VERSION,
+    packages=['uw_sdbmyuw'],
+    author="UW-IT T&LS",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=['UW-RestClients-Core'],
+    license='Apache License, Version 2.0',
+    description=('A library for connecting to the Sdbmyuw API'),
+    long_description=README,
+    url="https://github.com/uw-it-aca/uw-restclients-sdbmyuw",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
---
+++ packages/UW-RestClients-Sdbmyuw/uw_restclients_sdbmyuw-1.5.tar.gz/uw_restclients_sdbmyuw-1.5/uw_sdbmyuw/util.py
@@ -0,0 +1,8 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from restclients_core.util.decorators import use_mock
+from uw_sdbmyuw.dao import Sdbmyuw_DAO
+
+
+fdao_sdbmyuw_override = use_mock(Sdbmyuw_DAO())
