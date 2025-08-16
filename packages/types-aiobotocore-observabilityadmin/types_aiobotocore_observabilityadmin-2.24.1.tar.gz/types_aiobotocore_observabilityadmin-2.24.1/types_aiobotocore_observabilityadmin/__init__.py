8aa1664a9be1467c3f2e19f6a549113fa5d2c800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:05:55 +0000

    Add package types-aiobotocore-observabilityadmin/2.24.1/types_aiobotocore_observabilityadmin-2.24.1.tar.gz

---
+++ packages/types-aiobotocore-observabilityadmin/types_aiobotocore_observabilityadmin-2.24.1.tar.gz/types_aiobotocore_observabilityadmin-2.24.1/types_aiobotocore_observabilityadmin/__init__.py
@@ -0,0 +1,41 @@
+"""
+Main interface for observabilityadmin service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_observabilityadmin/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_observabilityadmin import (
+        Client,
+        CloudWatchObservabilityAdminServiceClient,
+        ListResourceTelemetryForOrganizationPaginator,
+        ListResourceTelemetryPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("observabilityadmin") as client:
+        client: CloudWatchObservabilityAdminServiceClient
+        ...
+
+
+    list_resource_telemetry_for_organization_paginator: ListResourceTelemetryForOrganizationPaginator = client.get_paginator("list_resource_telemetry_for_organization")
+    list_resource_telemetry_paginator: ListResourceTelemetryPaginator = client.get_paginator("list_resource_telemetry")
+    ```
+"""
+
+from .client import CloudWatchObservabilityAdminServiceClient
+from .paginator import ListResourceTelemetryForOrganizationPaginator, ListResourceTelemetryPaginator
+
+Client = CloudWatchObservabilityAdminServiceClient
+
+
+__all__ = (
+    "Client",
+    "CloudWatchObservabilityAdminServiceClient",
+    "ListResourceTelemetryForOrganizationPaginator",
+    "ListResourceTelemetryPaginator",
+)
