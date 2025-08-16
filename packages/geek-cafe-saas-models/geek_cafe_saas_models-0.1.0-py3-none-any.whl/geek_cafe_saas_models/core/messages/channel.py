55c733738e38f54ed49f83f5bf8874610f8606d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 06:06:32 +0000

    Add package geek-cafe-saas-models/0.1.0/geek_cafe_saas_models-0.1.0-py3-none-any.whl

---
+++ packages/geek-cafe-saas-models/geek_cafe_saas_models-0.1.0-py3-none-any.whl/geek_cafe_saas_models/core/messages/channel.py
@@ -0,0 +1,17 @@
+
+
+
+from typing import Optional
+from ..._base_tenant_user_model import BaseTenantUserDBModel as BaseDBModel
+# from .indexing.channel_indexes import Indexes
+
+
+class MessageChannel(BaseDBModel):
+    """
+    A message "channel"
+    """
+    def __init__(self):
+        super().__init__()
+        self.name = Optional[str] = None
+        self.owner_id = Optional[str] = None
+        
