24e3c79bc0f4b0e1b3cc2837078f2a7b96bcdab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 14:06:41 +0000

    Add package mug/0.0.1/mug-0.0.1-py3-none-any.whl

---
+++ packages/mug/mug-0.0.1-py3-none-any.whl/mug/__init__.py
@@ -0,0 +1,13 @@
+"""
+mug package
+----------
+
+CLI to capture, tag, and query project documents with user-defined XML/tags.
+"""
+
+__version__ = "0.0.1"
+
+
+def get_version() -> str:
+    """Return the current version of mug."""
+    return __version__
