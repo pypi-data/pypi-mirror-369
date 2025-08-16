c48ec410046a7c03cb020fc62f067de19eeb2c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:06:25 +0000

    Add package nonebot_plugin_suggarchat/3.4.0.dev1/nonebot_plugin_suggarchat-3.4.0.dev1-py3-none-any.whl

---
+++ packages/nonebot_plugin_suggarchat/nonebot_plugin_suggarchat-3.4.0.dev1-py3-none-any.whl/nonebot_plugin_suggarchat/utils/lock.py
@@ -0,0 +1,12 @@
+import asyncio
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1024)
+def get_group_lock(_: int) -> asyncio.Lock:
+    return asyncio.Lock()
+
+
+@lru_cache(maxsize=1024)
+def get_private_lock(_: int) -> asyncio.Lock:
+    return asyncio.Lock()
