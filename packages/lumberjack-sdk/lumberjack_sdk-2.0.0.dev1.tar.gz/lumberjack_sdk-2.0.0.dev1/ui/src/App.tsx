da9584499a3c09b64f35751184e6df7cd3e643bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 06:06:33 +0000

    Add package lumberjack-sdk/2.0.0.dev1/lumberjack_sdk-2.0.0.dev1.tar.gz

---
+++ packages/lumberjack-sdk/lumberjack_sdk-2.0.0.dev1.tar.gz/lumberjack_sdk-2.0.0.dev1/ui/src/App.tsx
@@ -0,0 +1,11 @@
+import { LogViewer } from './components/LogViewer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <LogViewer />
+    </div>
+  )
+}
+
+export default App
