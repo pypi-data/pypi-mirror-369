c3b53854056398202bd8bf49870edca56ac84f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:27 +0000

    Add package scikit-plots/0.4.0.post2/scikit_plots-0.4.0.post2-cp312-cp312-macosx_12_0_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post2-cp312-cp312-macosx_12_0_arm64.whl/scikitplot/stats/__init__.py
@@ -0,0 +1,13 @@
+"""
+Elegant statistical tools for intuitive and insightful data visualization and interpretation.
+
+The :py:mod:`~scikitplot.stats` module offers a wide range of probability distributions, summary
+and frequency statistics, correlation functions, statistical tests,
+masked statistics, and additional tools.
+"""
+
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+from ..cexternals._astropy.stats import *  # noqa: F401,F403
+from ..externals._tweedie import *  # noqa: F401,F403
