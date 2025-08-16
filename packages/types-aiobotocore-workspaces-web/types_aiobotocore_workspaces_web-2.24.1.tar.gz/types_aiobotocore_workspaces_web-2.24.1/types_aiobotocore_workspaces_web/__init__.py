749864c2721ffc26360904dd760558ee53a81a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:06:29 +0000

    Add package types-aiobotocore-workspaces-web/2.24.1/types_aiobotocore_workspaces_web-2.24.1.tar.gz

---
+++ packages/types-aiobotocore-workspaces-web/types_aiobotocore_workspaces_web-2.24.1.tar.gz/types_aiobotocore_workspaces_web-2.24.1/types_aiobotocore_workspaces_web/__init__.py
@@ -0,0 +1,41 @@
+"""
+Main interface for workspaces-web service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_workspaces_web/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_workspaces_web import (
+        Client,
+        ListDataProtectionSettingsPaginator,
+        ListSessionsPaginator,
+        WorkSpacesWebClient,
+    )
+
+    session = get_session()
+    async with session.create_client("workspaces-web") as client:
+        client: WorkSpacesWebClient
+        ...
+
+
+    list_data_protection_settings_paginator: ListDataProtectionSettingsPaginator = client.get_paginator("list_data_protection_settings")
+    list_sessions_paginator: ListSessionsPaginator = client.get_paginator("list_sessions")
+    ```
+"""
+
+from .client import WorkSpacesWebClient
+from .paginator import ListDataProtectionSettingsPaginator, ListSessionsPaginator
+
+Client = WorkSpacesWebClient
+
+
+__all__ = (
+    "Client",
+    "ListDataProtectionSettingsPaginator",
+    "ListSessionsPaginator",
+    "WorkSpacesWebClient",
+)
---
+++ packages/types-aiobotocore-workspaces-web/types_aiobotocore_workspaces_web-2.24.1.tar.gz/types_aiobotocore_workspaces_web-2.24.1/types_aiobotocore_workspaces_web/paginator.pyi
@@ -0,0 +1,85 @@
+"""
+Type annotations for workspaces-web service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_workspaces_web/paginators/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_workspaces_web.client import WorkSpacesWebClient
+    from types_aiobotocore_workspaces_web.paginator import (
+        ListDataProtectionSettingsPaginator,
+        ListSessionsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("workspaces-web") as client:
+        client: WorkSpacesWebClient
+
+        list_data_protection_settings_paginator: ListDataProtectionSettingsPaginator = client.get_paginator("list_data_protection_settings")
+        list_sessions_paginator: ListSessionsPaginator = client.get_paginator("list_sessions")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListDataProtectionSettingsRequestPaginateTypeDef,
+    ListDataProtectionSettingsResponseTypeDef,
+    ListSessionsRequestPaginateTypeDef,
+    ListSessionsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("ListDataProtectionSettingsPaginator", "ListSessionsPaginator")
+
+if TYPE_CHECKING:
+    _ListDataProtectionSettingsPaginatorBase = AioPaginator[
+        ListDataProtectionSettingsResponseTypeDef
+    ]
+else:
+    _ListDataProtectionSettingsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListDataProtectionSettingsPaginator(_ListDataProtectionSettingsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/workspaces-web/paginator/ListDataProtectionSettings.html#WorkSpacesWeb.Paginator.ListDataProtectionSettings)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_workspaces_web/paginators/#listdataprotectionsettingspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListDataProtectionSettingsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListDataProtectionSettingsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/workspaces-web/paginator/ListDataProtectionSettings.html#WorkSpacesWeb.Paginator.ListDataProtectionSettings.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_workspaces_web/paginators/#listdataprotectionsettingspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListSessionsPaginatorBase = AioPaginator[ListSessionsResponseTypeDef]
+else:
+    _ListSessionsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListSessionsPaginator(_ListSessionsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/workspaces-web/paginator/ListSessions.html#WorkSpacesWeb.Paginator.ListSessions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_workspaces_web/paginators/#listsessionspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListSessionsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListSessionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/workspaces-web/paginator/ListSessions.html#WorkSpacesWeb.Paginator.ListSessions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_workspaces_web/paginators/#listsessionspaginator)
+        """
