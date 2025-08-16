55c416ca2ce606e17858dc40aae56b7c242fe55a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 16:08:15 +0000

    Add package fafpdf/1.1/fafpdf-1.1-py3-none-any.whl

---
+++ packages/fafpdf/fafpdf-1.1-py3-none-any.whl/fafpdf/pdf2text.py
@@ -0,0 +1,17 @@
+''' This module provides functions to convert a PDF  to text. Faf, Goel and Inacio'''
+
+class Converter:
+    ''' A simple converter for converting PDFs to text.'''
+    def convert(self, path):
+        ''' 
+        Convert the given pdf to text
+
+    Parameters
+    path (str): The path to a PDF file.
+
+
+    Returns:
+    str: The content of the PDF file as tesxt.
+    '''
+    
+    print('pdf2text, Faf, Goel and Inacio')
