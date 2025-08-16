d51490004c335462b802d9922b4d4a63edd28770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:06:18 +0000

    Add package sarathii/0.3.0/sarathii-0.3.0.tar.gz

---
+++ packages/sarathii/sarathii-0.3.0.tar.gz/sarathii-0.3.0/sarathii/main.py
@@ -0,0 +1,20 @@
+def age():
+    from datetime import date
+
+    # Enter your birth year
+    birth_year = 2004  # change this to your year of        birth
+
+    # Get the current year
+    current_year = date.today().year
+
+    # Calculate age
+    age = current_year - birth_year
+
+    print(f"Your age is: {age}")
+    
+def full_name():
+	print("VIJAYASARATHI.A")
+def dob():
+	print("02/MAY/2004")
+def degree():
+	print("B.tech-AI&DS")
