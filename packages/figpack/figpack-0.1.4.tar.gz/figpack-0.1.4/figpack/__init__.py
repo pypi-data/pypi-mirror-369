13be51256821a43ba99f3355a81c8eaa4cb39d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 06:06:16 +0000

    Add package figpack/0.1.4/figpack-0.1.4.tar.gz

---
+++ packages/figpack/figpack-0.1.4.tar.gz/figpack-0.1.4/figpack/__init__.py
@@ -0,0 +1,9 @@
+"""
+figpack - A Python package for creating shareable, interactive visualizations in the browser
+"""
+
+__version__ = "0.1.4"
+__author__ = "Jeremy Magland"
+__email__ = "jmagland@flatironinstitute.org"
+
+from . import spike_sorting
---
+++ packages/figpack/figpack-0.1.4.tar.gz/figpack-0.1.4/figpack/spike_sorting/views/__init__.py
@@ -0,0 +1,23 @@
+"""
+Spike sorting views for figpack
+"""
+
+from .AutocorrelogramItem import AutocorrelogramItem
+from .Autocorrelograms import Autocorrelograms
+from .CrossCorrelogramItem import CrossCorrelogramItem
+from .CrossCorrelograms import CrossCorrelograms
+from .UnitsTableColumn import UnitsTableColumn
+from .UnitsTableRow import UnitsTableRow
+from .UnitSimilarityScore import UnitSimilarityScore
+from .UnitsTable import UnitsTable
+
+__all__ = [
+    "AutocorrelogramItem",
+    "Autocorrelograms",
+    "CrossCorrelogramItem",
+    "CrossCorrelograms",
+    "UnitsTableColumn",
+    "UnitsTableRow",
+    "UnitSimilarityScore",
+    "UnitsTable",
+]
