4dbed9f0b14e70f02f68bee9c59836c0a9272fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:39 +0000

    Add package fathom-global-client-sdk/1.1.0/fathom_global_client_sdk-1.1.0-py3-none-any.whl

---
+++ packages/fathom-global-client-sdk/fathom_global_client_sdk-1.1.0-py3-none-any.whl/fathom/sdk/clients/__init__.py
@@ -0,0 +1,11 @@
+from .geo import GeoClient
+from .geojson import GeoJSONClient
+from .portfolio import PortfolioClient
+from .vector_file import VectorFileClient
+
+__all__ = [
+    "GeoClient",
+    "GeoJSONClient",
+    "PortfolioClient",
+    "VectorFileClient",
+]
