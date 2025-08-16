591b496cd9c5c6813918bad58b1191cb14126d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:09:29 +0000

    Add package dataverse-utils/0.17.2/dataverse_utils-0.17.2.tar.gz

---
+++ packages/dataverse-utils/dataverse_utils-0.17.2.tar.gz/dataverse_utils-0.17.2/src/dataverse_utils/__init__.py
@@ -0,0 +1,9 @@
+'''
+Generalized dataverse utilities. Note that
+`import dataverse_utils` is the equivalent of
+`import dataverse_utils.dataverse_utils`
+'''
+from dataverse_utils.dataverse_utils import *
+
+VERSION = (0,17,2)
+__version__ = '.'.join([str(x) for x in VERSION])
