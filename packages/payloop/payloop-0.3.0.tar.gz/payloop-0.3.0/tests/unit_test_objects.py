489f2e43ae3ee84b797e6c1a574d5f6d8111b4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:06:54 +0000

    Add package payloop/0.3.0/payloop-0.3.0.tar.gz

---
+++ packages/payloop/payloop-0.3.0.tar.gz/payloop-0.3.0/tests/unit_test_objects.py
@@ -0,0 +1,10 @@
+class unit_test_x:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+
+
+class unit_test_y:
+    def __init__(self):
+        self.c = 3
+        self.d = unit_test_x()
