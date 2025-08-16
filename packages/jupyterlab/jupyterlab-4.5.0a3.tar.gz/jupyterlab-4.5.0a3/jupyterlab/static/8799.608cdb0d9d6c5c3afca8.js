7ae0b3e6e21a42085eb5af4ef9c87d7f39be735e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:07 +0000

    Add package jupyterlab/4.5.0a3/jupyterlab-4.5.0a3.tar.gz

---
+++ packages/jupyterlab/jupyterlab-4.5.0a3.tar.gz/jupyterlab-4.5.0a3/jupyterlab/static/8799.608cdb0d9d6c5c3afca8.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunk_jupyterlab_application_top"]=self["webpackChunk_jupyterlab_application_top"]||[]).push([[8799],{88799:(e,a,p)=>{p.d(a,{createPieServices:()=>t.f});var t=p(34588);var c=p(26829)}}]);
