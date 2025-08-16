e7e9ea0ca612d8bc1b2da52641fd2dfc8438d1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:09:47 +0000

    Add package python-terminusgps/40.1.4/python_terminusgps-40.1.4.tar.gz

---
+++ packages/python-terminusgps/python_terminusgps-40.1.4.tar.gz/python_terminusgps-40.1.4/docs/build/html/_sources/twilio/caller.rst.txt
@@ -0,0 +1,9 @@
+Caller object
+=============
+
+
+.. currentmodule:: terminusgps.twilio.caller
+
+.. autoclass:: TwilioCaller
+   :members:
+   :autoclasstoc:
---
+++ packages/python-terminusgps/python_terminusgps-40.1.4.tar.gz/python_terminusgps-40.1.4/terminusgps/wialon/items/route.py
@@ -0,0 +1,36 @@
+from terminusgps.wialon import flags
+from terminusgps.wialon.items.base import WialonBase
+
+
+class WialonRoute(WialonBase):
+    """A Wialon `route <https://help.wialon.com/en/wialon-hosting/user-guide/monitoring-system/routes>`_."""
+
+    def create(self, creator_id: str | int, name: str) -> int | None:
+        """
+        Creates a new Wialon route.
+
+        :param creator_id: A Wialon user id.
+        :type creator_id: :py:obj:`str` | :py:obj:`int`
+        :param name: A name for the route.
+        :type name: :py:obj:`str`
+        :raises ValueError: If ``creator_id`` is not a digit.
+        :raises WialonError: If something goes wrong with Wialon.
+        :returns: The Wialon id for the new route, if it was created.
+        :rtype: :py:obj:`int` | :py:obj:`None`
+
+        """
+        if isinstance(creator_id, str) and not creator_id.isdigit():
+            raise ValueError(f"'creator_id' must be a digit, got '{creator_id}'.")
+
+        response = self.session.wialon_api.core_create_route(
+            **{
+                "creatorId": creator_id,
+                "name": name,
+                "dataFlags": flags.DataFlag.ROUTE_BASE.value,
+            }
+        )
+        return (
+            int(response.get("item", {}).get("id"))
+            if response and response.get("item")
+            else None
+        )
