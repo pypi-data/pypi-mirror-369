7a2e7f2eaaa8080d398f1221c12a1157e4db38d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:27 +0000

    Add package magic-case/0.1.0/magic_case-0.1.0.tar.gz

---
+++ packages/magic-case/magic_case-0.1.0.tar.gz/magic_case-0.1.0/magic_case/__init__.py
@@ -0,0 +1,19 @@
+from .base import BaseCase
+from .snake import SnakeCase
+from .camel import CamelCase
+from .pascal import PascalCase
+from .kebab import KebabCase
+from .upper import UpperCase
+from .sentence import SentenceCase
+from .title import TitleCase
+
+__all__ = [
+    "BaseCase",
+    "SnakeCase",
+    "CamelCase",
+    "PascalCase",
+    "KebabCase",
+    "UpperCase",
+    "SentenceCase",
+    "TitleCase",
+]
