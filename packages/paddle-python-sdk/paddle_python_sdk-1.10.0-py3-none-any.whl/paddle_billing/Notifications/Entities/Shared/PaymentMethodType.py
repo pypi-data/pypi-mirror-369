7cd8f2cc0d80b99a4a471e745b5116dbe5f9caaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:24 +0000

    Add package paddle-python-sdk/1.10.0/paddle_python_sdk-1.10.0-py3-none-any.whl

---
+++ packages/paddle-python-sdk/paddle_python_sdk-1.10.0-py3-none-any.whl/paddle_billing/Notifications/Entities/Shared/PaymentMethodType.py
@@ -0,0 +1,15 @@
+from paddle_billing.PaddleStrEnum import PaddleStrEnum, PaddleStrEnumMeta
+
+
+class PaymentMethodType(PaddleStrEnum, metaclass=PaddleStrEnumMeta):
+    Alipay: "PaymentMethodType" = "alipay"
+    ApplePay: "PaymentMethodType" = "apple_pay"
+    Bancontact: "PaymentMethodType" = "bancontact"
+    Card: "PaymentMethodType" = "card"
+    GooglePay: "PaymentMethodType" = "google_pay"
+    Ideal: "PaymentMethodType" = "ideal"
+    KoreaLocal: "PaymentMethodType" = "korea_local"
+    Offline: "PaymentMethodType" = "offline"
+    Paypal: "PaymentMethodType" = "paypal"
+    Unknown: "PaymentMethodType" = "unknown"
+    WireTransfer: "PaymentMethodType" = "wire_transfer"
