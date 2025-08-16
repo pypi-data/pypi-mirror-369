5f265f010e7b559b5df7492a120a799b9849f595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:07:06 +0000

    Add package Invoker-CLI/1.0.0a30/invoker_cli-1.0.0a30.tar.gz

---
+++ packages/Invoker-CLI/invoker_cli-1.0.0a30.tar.gz/invoker_cli-1.0.0a30/setup.py
@@ -0,0 +1,13 @@
+"""setup.py for Invoker-CLI"""
+
+__author__ = "Mohsen Ghorbani"
+__email__ = "m.ghorbani2357@gmail.com"
+__copyright__ = "Copyright 2025, Mohsen Ghorbani"
+
+from setuptools import setup, find_packages
+
+
+REQUIREMENTS = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+)
