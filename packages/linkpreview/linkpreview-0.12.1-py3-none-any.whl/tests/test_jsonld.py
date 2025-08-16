6df2cd0f6f58a85db163194a8a18732a2c2ce19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:05:53 +0000

    Add package linkpreview/0.12.1/linkpreview-0.12.1-py3-none-any.whl

---
+++ packages/linkpreview/linkpreview-0.12.1-py3-none-any.whl/tests/test_jsonld.py
@@ -0,0 +1,127 @@
+import pytest
+
+
+from linkpreview import Link
+from linkpreview.preview import JsonLd
+
+from tests.helpers import get_sample
+
+
+@pytest.mark.parametrize(
+    "tin, tout",
+    (
+        (
+            "available.html",
+            {
+                "title": "a title",
+                "description": "لورم ایپزوم",
+                "image": None,
+                "site_name": "a title",
+            },
+        ),
+        (
+            "unavailable.html",
+            {
+                "title": None,
+                "description": None,
+                "image": None,
+                "site_name": None,
+            },
+        ),
+        (
+            "with-image.html",
+            {
+                "title": "a title",
+                "description": None,
+                "image": "/img/heck.jpg",
+                "site_name": None,
+            },
+        ),
+        (
+            "with-thumbnail.html",
+            {
+                "title": "a title",
+                "description": None,
+                "image": "/img/heck.jpg",
+                "site_name": None,
+            },
+        ),
+        (
+            "invalid.html",
+            {
+                "title": None,
+                "description": None,
+                "image": None,
+                "site_name": None,
+            },
+        ),
+        (
+            "article-website.html",
+            {
+                "title": "The Article",
+                "description": None,
+                "image": None,
+                "site_name": "The Website",
+            },
+        ),
+        (
+            "website-article.html",
+            {
+                "title": "The Article",
+                "description": None,
+                "image": None,
+                "site_name": "The Website",
+            },
+        ),
+        (
+            "sitename.html",
+            {
+                "title": "The Website",
+                "description": None,
+                "image": None,
+                "site_name": "The Website",
+            },
+        ),
+        (
+            "image-object.html",
+            {
+                "title": None,
+                "description": "no more desc",
+                "image": "https://site.com/example.jpg",
+                "site_name": None,
+            },
+        ),
+        (
+            "image-object-empty.html",
+            {
+                "title": None,
+                "description": "no more desc",
+                "image": None,
+                "site_name": None,
+            },
+        ),
+        (
+            "image-object-empty2.html",
+            {
+                "title": None,
+                "description": "no more desc",
+                "image": None,
+                "site_name": None,
+            },
+        ),
+        (
+            "image-object-null.html",
+            {
+                "title": None,
+                "description": "no more desc",
+                "image": None,
+                "site_name": None,
+            },
+        ),
+    ),
+)
+def test_jsonld(tin, tout):
+    link = Link("http://localhost", content=get_sample("jsonld/%s" % tin))
+    preview = JsonLd(link, parser="html.parser")
+    for key in tout.keys():
+        assert getattr(preview, key) == tout[key]
