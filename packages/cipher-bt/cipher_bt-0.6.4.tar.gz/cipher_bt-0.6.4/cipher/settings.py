dc321a1d099ec3d3dce023dfa6633627ffd47b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:16 +0000

    Add package cipher-bt/0.6.4/cipher_bt-0.6.4.tar.gz

---
+++ packages/cipher-bt/cipher_bt-0.6.4.tar.gz/cipher_bt-0.6.4/cipher/settings.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from cipher.models import LogLevel
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    cache_root: Path = ".cache"
+    log_level: LogLevel = LogLevel.INFO
