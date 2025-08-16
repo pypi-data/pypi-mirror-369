ae6b4f84ceb621a174d79f7b5a138e7e0ea091c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:06:11 +0000

    Add package indent/0.1.10/indent-0.1.10.tar.gz

---
+++ packages/indent/indent-0.1.10.tar.gz/indent-0.1.10/exponent/core/remote_execution/system_context.py
@@ -0,0 +1,27 @@
+import getpass
+import os
+import platform
+
+from anyio import Path as AsyncPath
+
+from exponent.core.remote_execution.git import get_git_info
+from exponent.core.remote_execution.languages import python_execution
+from exponent.core.remote_execution.types import (
+    SystemInfo,
+)
+from exponent.core.remote_execution.utils import safe_read_file
+
+
+async def get_system_info(working_directory: str) -> SystemInfo:
+    return SystemInfo(
+        name=getpass.getuser(),
+        cwd=working_directory,
+        os=platform.system(),
+        shell=_get_user_shell(),
+        git=await get_git_info(working_directory),
+        python_env=python_execution.get_python_env_info(),
+    )
+
+
+def _get_user_shell() -> str:
+    return os.environ.get("SHELL", "bash")
