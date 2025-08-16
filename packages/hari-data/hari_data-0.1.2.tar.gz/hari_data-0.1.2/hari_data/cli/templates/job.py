69ab6be40f33ad05bbd09361300eeafcd319de89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 14:06:18 +0000

    Add package hari-data/0.1.2/hari_data-0.1.2.tar.gz

---
+++ packages/hari-data/hari_data-0.1.2.tar.gz/hari_data-0.1.2/hari_data/cli/templates/job.py
@@ -0,0 +1,15 @@
+# This is a job.py template
+# Used to define job configurations and execution logic
+# in data processing projects
+
+
+def main():
+    """
+    Main function to execute the job logic.
+    This function should be implemented with the specific job requirements.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
