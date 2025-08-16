ec8d26fb5c8885c48b9accedab810ea686a2ac86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:12 +0000

    Add package agg-python-bindings/0.1.1/agg_python_bindings-0.1.1-cp38-cp38-manylinux_2_34_x86_64.whl

---
+++ packages/agg-python-bindings/agg_python_bindings-0.1.1-cp38-cp38-manylinux_2_34_x86_64.whl/agg_python_bindings/__init__.pyi
@@ -0,0 +1,15 @@
+# view example type annotation at: https://github.com/PyO3/maturin/blob/main/test-crates/pyo3-pure
+
+# TODO: ingest arbitrary terminal sequence (like the one from tmux) and produce png output
+
+# TODO: create a virtual terminal class with state, can take a screenshot at arbitrary state
+
+# TODO: implement the function "load_asciicast_and_return_iterator", return iterator of events (str) with timestamp (float)
+
+def load_asciicast_and_save_png_screenshots(cast_file_loadpath: str, png_write_dir:str=".",png_filename_prefix:str="screenshot", frame_time_min_spacing:float=1.0) -> None:
+    """
+    Load asciicast file from path, save terminal screenshots separated by frame_time_min_spacing (seconds)
+
+    Output png filename format: "{png_filename_prefix}_{screenshot_timestamp}.png"
+    """
+    ...
