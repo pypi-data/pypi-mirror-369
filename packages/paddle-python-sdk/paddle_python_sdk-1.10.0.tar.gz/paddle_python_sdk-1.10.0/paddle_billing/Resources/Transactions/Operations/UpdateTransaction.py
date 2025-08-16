058cb824cd131ab8b57ee6214ce4002a6204cf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:24 +0000

    Add package paddle-python-sdk/1.10.0/paddle_python_sdk-1.10.0.tar.gz

---
+++ packages/paddle-python-sdk/paddle_python_sdk-1.10.0.tar.gz/paddle_python_sdk-1.10.0/paddle_billing/Resources/Transactions/Operations/UpdateTransaction.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+from paddle_billing.Operation import Operation
+from paddle_billing.Undefined import Undefined
+from paddle_billing.Entities.Shared import (
+    Checkout,
+    CollectionMode,
+    CurrencyCode,
+    CustomData,
+    TimePeriod,
+    TransactionStatus,
+)
+from paddle_billing.Resources.Transactions.Operations.Update import (
+    TransactionUpdateItem,
+    TransactionUpdateItemWithPrice,
+    UpdateBillingDetails,
+)
+
+
+@dataclass
+class UpdateTransaction(Operation):
+    items: list[TransactionUpdateItem | TransactionUpdateItemWithPrice] = Undefined()
+    status: TransactionStatus | Undefined = Undefined()
+    customer_id: str | None | Undefined = Undefined()
+    address_id: str | None | Undefined = Undefined()
+    business_id: str | None | Undefined = Undefined()
+    custom_data: CustomData | None | Undefined = Undefined()
+    currency_code: CurrencyCode | Undefined = Undefined()
+    collection_mode: CollectionMode | Undefined = Undefined()
+    discount_id: str | None | Undefined = Undefined()
+    billing_details: UpdateBillingDetails | None | Undefined = Undefined()
+    billing_period: TimePeriod | None | Undefined = Undefined()
+    checkout: Checkout | None | Undefined = Undefined()
