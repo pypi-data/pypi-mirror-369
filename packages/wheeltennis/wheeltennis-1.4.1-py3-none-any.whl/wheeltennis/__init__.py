567b546e1e8c73adbcf574d81327b28949ef4f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:41 +0000

    Add package wheeltennis/1.4.1/wheeltennis-1.4.1-py3-none-any.whl

---
+++ packages/wheeltennis/wheeltennis-1.4.1-py3-none-any.whl/wheeltennis/__init__.py
@@ -0,0 +1,9 @@
+"""Wheelchair tennis data processing package"""
+
+__version__ = "1.4.1"
+
+__all__ = ['match', 'plots', 'testing']
+
+from . import match
+from . import plots
+from . import testing
