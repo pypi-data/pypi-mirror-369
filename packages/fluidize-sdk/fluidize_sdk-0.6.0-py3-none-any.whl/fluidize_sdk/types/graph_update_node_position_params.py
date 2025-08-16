f57059e0bd0b24919d5eaba89e765d190b3fb710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 16:06:54 +0000

    Add package fluidize-sdk/0.6.0/fluidize_sdk-0.6.0-py3-none-any.whl

---
+++ packages/fluidize-sdk/fluidize_sdk-0.6.0-py3-none-any.whl/fluidize_sdk/types/graph_update_node_position_params.py
@@ -0,0 +1,43 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["GraphUpdateNodePositionParams", "Data", "Position"]
+
+
+class GraphUpdateNodePositionParams(TypedDict, total=False):
+    query_id: Required[Annotated[str, PropertyInfo(alias="id")]]
+
+    body_id: Required[Annotated[str, PropertyInfo(alias="id")]]
+
+    data: Required[Data]
+
+    position: Required[Position]
+
+    type: Required[str]
+
+    description: str
+
+    label: str
+
+    location: str
+
+    metadata_version: str
+
+    status: str
+
+
+class Data(TypedDict, total=False):
+    label: Required[str]
+
+    simulation_id: Required[str]
+
+
+class Position(TypedDict, total=False):
+    x: Required[float]
+
+    y: Required[float]
