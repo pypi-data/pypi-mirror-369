7fd347d47f174076707309871f8d8e1bebc6e53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:26 +0000

    Add package scaleinvariance/0.3.1/scaleinvariance-0.3.1.tar.gz

---
+++ packages/scaleinvariance/scaleinvariance-0.3.1.tar.gz/scaleinvariance-0.3.1/scaleinvariance/__init__.py
@@ -0,0 +1,28 @@
+"""
+ScaleInvariance: Simulation and analysis of multifractal fields
+
+This package provides tools for:
+- Hurst exponent estimation using multiple methods
+- Fractionally integrated flux simulation
+- Fractional Brownian motion simulation
+"""
+
+__version__ = "0.3.1"
+__author__ = "Thomas DeWitt"
+
+import torch
+import os
+
+# Set threading for both PyTorch and BLAS
+torch.set_num_threads(8)
+os.environ['OMP_NUM_THREADS'] = '8'
+os.environ['MKL_NUM_THREADS'] = '8'
+os.environ['NUMEXPR_NUM_THREADS'] = '8'
+
+device = torch.device('cpu')
+
+from . import analysis, simulation
+
+# Easy access to main functions
+from .analysis import structure_function_hurst, haar_fluctuation_hurst, spectral_hurst, haar_fluctuation_analysis, structure_function_analysis, spectral_analysis
+from .simulation import FIF_1D, FIF_2D, acausal_fBm_1D, acausal_fBm_2D
