6302927e22c04d2d9dddad2e7ef79b4c0eba7f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 14:06:23 +0000

    Add package sheshe/0.1.2/sheshe-0.1.2-py3-none-any.whl

---
+++ packages/sheshe/sheshe-0.1.2-py3-none-any.whl/sheshe/__init__.py
@@ -0,0 +1,5 @@
+from .sheshe import ModalBoundaryClustering, ClusterRegion
+from .subspace_scout import SubspaceScout
+
+__all__ = ["ModalBoundaryClustering", "ClusterRegion", "SubspaceScout"]
+__version__ = "0.1.1"
