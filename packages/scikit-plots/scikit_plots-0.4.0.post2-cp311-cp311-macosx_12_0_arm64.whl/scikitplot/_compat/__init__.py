48f452d959f6a89495a1fa25d7419f299d431e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:25 +0000

    Add package scikit-plots/0.4.0.post2/scikit_plots-0.4.0.post2-cp311-cp311-macosx_12_0_arm64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post2-cp311-cp311-macosx_12_0_arm64.whl/scikitplot/_compat/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+The content of this module is solely for internal use of ``scikitplot``.
+
+Subject to changes without deprecations. Do not use it in external
+packages or code.
+"""
+
+# Importing this module will also install monkey-patches defined in it
+
+from ..externals._seaborn._compat import *  # noqa: F403
+from .numpycompat import *  # noqa: F403
