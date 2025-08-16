00baf7a548e5be583f75fc625c28303db9539923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:05:49 +0000

    Add package bluer-algo/4.364.1/bluer_algo-4.364.1-py3-none-any.whl

---
+++ packages/bluer-algo/bluer_algo-4.364.1-py3-none-any.whl/bluer_algo/README/yolo.py
@@ -0,0 +1,6 @@
+docs = [
+    {"path": "../docs/yolo"},
+    {"path": "../docs/yolo/dataset"},
+    {"path": "../docs/yolo/dataset/ingest-and-review.md"},
+    {"path": "../docs/yolo/dataset/train.md"},
+]
