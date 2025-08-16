fc05bfb51320799314da4ebf2d72dd8ebcbb0587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:15 +0000

    Add package whisperx-nemo-pipeline/1.0.4/whisperx_nemo_pipeline-1.0.4.tar.gz

---
+++ packages/whisperx-nemo-pipeline/whisperx_nemo_pipeline-1.0.4.tar.gz/whisperx_nemo_pipeline-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+import sys
+
+ext_modules = [
+    Pybind11Extension(
+        "whisperx_nemo_pipeline.vendor.ctc-forced-aligner.ctc_forced_aligner.ctc_forced_aligner",
+        ["whisperx_nemo_pipeline/vendor/ctc-forced-aligner/ctc_forced_aligner/forced_align_impl.cpp"],
+        extra_compile_args=["/O2"] if sys.platform == "win32" else ["-O3"],
+    )
+]
+
+setup(
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)
