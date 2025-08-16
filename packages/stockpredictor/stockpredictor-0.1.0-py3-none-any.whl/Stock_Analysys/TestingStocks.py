bc980318d494a4b3cafa77a54881b4ac9c87c80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 16:07:13 +0000

    Add package stockpredictor/0.1.0/stockpredictor-0.1.0-py3-none-any.whl

---
+++ packages/stockpredictor/stockpredictor-0.1.0-py3-none-any.whl/Stock_Analysys/TestingStocks.py
@@ -0,0 +1,8 @@
+# from stock_predictor import StockPredictor
+#
+# sp = StockPredictor(ticker="^NSEI", start="2023-01-01", end="2024-01-01")
+# sp.load_data()
+# sp.prepare_features()
+# sp.train_logistic()
+# sp.evaluate()
+# sp.plot()
