2f02b30906aae66987c257cd7b854863673e0ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:05:43 +0000

    Add package audiopod/1.0.0/audiopod-1.0.0-py3-none-any.whl

---
+++ packages/audiopod/audiopod-1.0.0-py3-none-any.whl/audiopod/services/__init__.py
@@ -0,0 +1,24 @@
+"""
+AudioPod API Services
+Service classes for different API endpoints
+"""
+
+from .voice import VoiceService
+from .music import MusicService
+from .transcription import TranscriptionService
+from .translation import TranslationService
+from .speaker import SpeakerService
+from .denoiser import DenoiserService
+from .karaoke import KaraokeService
+from .credits import CreditService
+
+__all__ = [
+    "VoiceService",
+    "MusicService", 
+    "TranscriptionService",
+    "TranslationService",
+    "SpeakerService",
+    "DenoiserService",
+    "KaraokeService",
+    "CreditService"
+]
