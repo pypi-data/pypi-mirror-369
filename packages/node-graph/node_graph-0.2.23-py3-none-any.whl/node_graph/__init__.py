9a32485fd1c29e58eaf9d6df68d09a9a2696f83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:14 +0000

    Add package node-graph/0.2.23/node_graph-0.2.23-py3-none-any.whl

---
+++ packages/node-graph/node_graph-0.2.23-py3-none-any.whl/node_graph/__init__.py
@@ -0,0 +1,20 @@
+from .node_graph import NodeGraph
+from .node import Node
+from .decorator import node
+from .executor import NodeExecutor
+from .nodes import NodePool
+from .collection import group
+from . import spec
+
+__version__ = "0.2.23"
+
+
+__all__ = [
+    "NodeGraph",
+    "Node",
+    "node",
+    "NodeExecutor",
+    "NodePool",
+    "group",
+    "spec",
+]
