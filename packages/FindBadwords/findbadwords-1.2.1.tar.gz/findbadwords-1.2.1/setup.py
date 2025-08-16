b638b1eb73e3b59dc834958b74aa5c3615297e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:16 +0000

    Add package FindBadwords/1.2.1/findbadwords-1.2.1.tar.gz

---
+++ packages/FindBadwords/findbadwords-1.2.1.tar.gz/findbadwords-1.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="FindBadwords",
+    version="1.2.1",
+    description="Find any word in a sentence",
+    long_description="Find any word in a sentence, especially if contain special character ;)",
+    author="BOXER",
+    author_email="vagabonwalybi@gmail.com",
+    maintainer="BOXER",
+    maintainer_email="vagabonwalybi@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+        "Operating System :: Microsoft :: Windows :: Windows 10"
+    ],
+    install_requires=[
+        "immutable-Python-Type",
+        "regex"
+    ],
+
+    packages=['FindBadwords'],
+    python_requires=">=3.9",
+    include_package_data=True,
+)
