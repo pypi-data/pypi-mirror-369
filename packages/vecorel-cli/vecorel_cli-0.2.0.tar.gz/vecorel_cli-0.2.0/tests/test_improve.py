27a10e639f3b233964f40bae5871c60cbf74b025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:06:25 +0000

    Add package vecorel-cli/0.2.0/vecorel_cli-0.2.0.tar.gz

---
+++ packages/vecorel-cli/vecorel_cli-0.2.0.tar.gz/vecorel_cli-0.2.0/tests/test_improve.py
@@ -0,0 +1,57 @@
+import pytest
+
+from vecorel_cli.encoding.geoparquet import GeoParquet
+from vecorel_cli.improve import ImproveData
+
+# todo: add test for fix-geometries and explode-geometries
+
+
+def test_improve_compression(tmp_parquet_file):
+    source = "tests/data-files/inspire.parquet"
+    improve = ImproveData()
+    improve.improve_file(source, tmp_parquet_file, compression="zstd")
+
+    gp = GeoParquet(tmp_parquet_file)
+    assert gp.get_compression() == "zstd"
+
+
+def test_improve_rename(tmp_parquet_file):
+    source = "tests/data-files/admin.json"
+    improve = ImproveData()
+    improve.improve_file(source, tmp_parquet_file, rename={"foo": "test"})
+
+    gp = GeoParquet(tmp_parquet_file)
+    props = gp.get_properties()
+    assert "test" in props
+    assert "foo" not in props
+
+
+def test_improve_add_sizes(tmp_parquet_file):
+    source = "tests/data-files/inspire.parquet"
+    improve = ImproveData()
+    improve.improve_file(source, tmp_parquet_file, add_sizes=True)
+
+    gp = GeoParquet(tmp_parquet_file)
+    props = gp.get_properties()
+    assert "area" in props
+    assert "perimeter" in props
+
+    data = gp.read()
+    assert all(data["area"] > 0)
+    assert all(data["perimeter"] > 0)
+
+
+def test_improve_crs(tmp_parquet_file):
+    source = "tests/data-files/inspire.parquet"
+    improve = ImproveData()
+    improve.improve_file(source, tmp_parquet_file, crs="EPSG:3857")
+
+    gp = GeoParquet(tmp_parquet_file)
+    data = gp.read()
+    assert data.crs == "EPSG:3857"
+
+
+def test_improve_invalid_file():
+    improve = ImproveData()
+    with pytest.raises(FileNotFoundError):
+        improve.improve_file("invalid.parquet")
