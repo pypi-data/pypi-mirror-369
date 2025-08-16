e5ec4d208049f42ae07b008720eafe1947d45c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:46 +0000

    Add package aiorocket2/2.0.1/aiorocket2-2.0.1.tar.gz

---
+++ packages/aiorocket2/aiorocket2-2.0.1.tar.gz/aiorocket2-2.0.1/aiorocket2/version.py
@@ -0,0 +1,30 @@
+#  aiorocket2 - Asynchronous Python client for xRocket Pay API
+#  Copyright (C) 2025-present RimMirK
+#
+#  This file is part of aiorocket2.
+#
+#  aiorocket2 is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3 of the License.
+#
+#  aiorocket2 is an independent, unofficial client library.
+#  It is a near one-to-one reflection of the xRocket Pay API:
+#  all methods, parameters, objects and enums are implemented.
+#  If something does not work as expected, please open an issue.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with aiorocket2.  If not, see the LICENSE file.
+#
+#  Repository: https://github.com/RimMirK/aiorocket2
+#  Documentation: https://aiorocket2.rimmirk.pp.ua
+#  Telegram: @RimMirK
+
+"""
+version of aiorocket2
+"""
+
+__all__ = [
+    '__version__'
+]
+
+__version__ = "2.0.1"
