84993876496356d76076633520798fb6c2412fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:06:23 +0000

    Add package types-aiobotocore-socialmessaging/2.24.1/types_aiobotocore_socialmessaging-2.24.1.tar.gz

---
+++ packages/types-aiobotocore-socialmessaging/types_aiobotocore_socialmessaging-2.24.1.tar.gz/types_aiobotocore_socialmessaging-2.24.1/types_aiobotocore_socialmessaging/__init__.py
@@ -0,0 +1,34 @@
+"""
+Main interface for socialmessaging service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_socialmessaging/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_socialmessaging import (
+        Client,
+        EndUserMessagingSocialClient,
+        ListLinkedWhatsAppBusinessAccountsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("socialmessaging") as client:
+        client: EndUserMessagingSocialClient
+        ...
+
+
+    list_linked_whatsapp_business_accounts_paginator: ListLinkedWhatsAppBusinessAccountsPaginator = client.get_paginator("list_linked_whatsapp_business_accounts")
+    ```
+"""
+
+from .client import EndUserMessagingSocialClient
+from .paginator import ListLinkedWhatsAppBusinessAccountsPaginator
+
+Client = EndUserMessagingSocialClient
+
+
+__all__ = ("Client", "EndUserMessagingSocialClient", "ListLinkedWhatsAppBusinessAccountsPaginator")
