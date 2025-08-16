938e4d116979450bae5b5cf28d18476dece05b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:06:13 +0000

    Add package ffmpeg-quality-metrics/3.6.2/ffmpeg_quality_metrics-3.6.2-py3-none-any.whl

---
+++ packages/ffmpeg-quality-metrics/ffmpeg_quality_metrics-3.6.2-py3-none-any.whl/ffmpeg_quality_metrics/__init__.py
@@ -0,0 +1,23 @@
+from .ffmpeg_quality_metrics import (
+    FfmpegQualityMetrics,
+    FfmpegQualityMetricsError,
+    GlobalStats,
+    GlobalStatsData,
+    MetricData,
+    MetricName,
+    SingleMetricData,
+    VmafOptions,
+)
+
+__version__ = "3.6.2"
+__all__ = [
+    "FfmpegQualityMetrics",
+    "FfmpegQualityMetricsError",
+    "VmafOptions",
+    "MetricName",
+    "SingleMetricData",
+    "GlobalStatsData",
+    "GlobalStats",
+    "MetricData",
+    "__version__",
+]
