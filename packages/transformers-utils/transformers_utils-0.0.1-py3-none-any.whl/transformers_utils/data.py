9205af86986ac22e8f0108be6581d472f742d3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 15:08:00 +0000

    Add package transformers-utils/0.0.1/transformers_utils-0.0.1-py3-none-any.whl

---
+++ packages/transformers-utils/transformers_utils-0.0.1-py3-none-any.whl/transformers_utils/data.py
@@ -0,0 +1,24 @@
+def read_json():
+    pass
+
+
+def read_parquet():
+    pass
+
+
+def read_txt():
+    pass
+
+
+def read_csv():
+    pass
+
+
+def read_excel():
+    pass
+
+
+def dump_to_file():
+    pass
+
+
