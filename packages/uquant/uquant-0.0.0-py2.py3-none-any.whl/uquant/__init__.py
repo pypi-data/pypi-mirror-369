678f6673a4c80338691374c604b489a4820df2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 05:07:32 +0000

    Add package uquant/0.0.0/uquant-0.0.0-py2.py3-none-any.whl

---
+++ packages/uquant/uquant-0.0.0-py2.py3-none-any.whl/uquant/__init__.py
@@ -0,0 +1,24 @@
+"""
+# uquant
+A quant library.
+
+## See Also
+### Github repository
+* https://github.com/Chitaoji/uquant/
+
+### PyPI project
+* https://pypi.org/project/uquant/
+
+## License
+This project falls under the BSD 3-Clause License.
+
+"""
+
+from typing import List
+
+from . import core
+from .__version__ import __version__
+from .core import *
+
+__all__: List[str] = []
+__all__.extend(core.__all__)
---
+++ packages/uquant/uquant-0.0.0-py2.py3-none-any.whl/uquant/core.py
@@ -0,0 +1,7 @@
+"""
+Contains the core of $package: ... , etc.
+
+NOTE: this module is private. All functions and objects are available in the main
+`$package` namespace - use that instead.
+
+"""
