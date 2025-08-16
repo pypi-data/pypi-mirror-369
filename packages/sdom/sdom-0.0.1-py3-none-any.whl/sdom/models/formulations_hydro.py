8ba54488abfe473ff50041849f87f5954f35d9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 14:07:11 +0000

    Add package sdom/0.0.1/sdom-0.0.1-py3-none-any.whl

---
+++ packages/sdom/sdom-0.0.1-py3-none-any.whl/sdom/models/formulations_hydro.py
@@ -0,0 +1,14 @@
+from pyomo.core import Var, Constraint
+from pyomo.environ import *
+
+####################################################################################|
+# ----------------------------------- Parameters -----------------------------------|
+####################################################################################|
+
+def add_large_hydro_parameters(model, data):
+    # Control for large hydro generation
+    model.AlphaLargHy = Param( initialize = float(data["scalars"].loc["AlphaLargHy"].Value) )
+    # Large hydro data initialization
+    large_hydro_data = data["large_hydro_data"].set_index('*Hour')['LargeHydro'].to_dict()
+    filtered_large_hydro_data = {h: large_hydro_data[h] for h in model.h if h in large_hydro_data}
+    model.LargeHydro = Param( model.h, initialize = filtered_large_hydro_data)
