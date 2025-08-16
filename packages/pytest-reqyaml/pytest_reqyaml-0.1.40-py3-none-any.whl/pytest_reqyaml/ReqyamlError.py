828dcffe7f6cc0bad776bfc6e9978a24d078d3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 16:06:38 +0000

    Add package pytest-reqyaml/0.1.40/pytest_reqyaml-0.1.40-py3-none-any.whl

---
+++ packages/pytest-reqyaml/pytest_reqyaml-0.1.40-py3-none-any.whl/pytest_reqyaml/ReqyamlError.py
@@ -0,0 +1,17 @@
+class JsonpathError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
+
+class NameMissingError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
+
+class MyAssertionError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
