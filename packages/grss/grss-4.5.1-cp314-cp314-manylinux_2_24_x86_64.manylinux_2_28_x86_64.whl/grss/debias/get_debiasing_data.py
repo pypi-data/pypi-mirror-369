874eb33e8870d5971788c746dc1090b80a7eec0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 03:26:38 +0000

    Add package grss/4.5.1/grss-4.5.1-cp314-cp314-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/grss/grss-4.5.1-cp314-cp314-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/grss/debias/get_debiasing_data.py
@@ -0,0 +1,34 @@
+"""Download the debiasing data files from the JPL Solar System Dynamics Group's FTP server"""
+import os
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+FTP_SITE = 'ftp://ssd.jpl.nasa.gov/pub/ssd/debias'
+# get debiasing data files from the JPL Solar System Dynamics Group's FTP server
+# if lowres_data.tgz or lowres_data/ already exist, do not download
+if not os.path.exists(f'{cwd}/lowres_data.tgz') and not os.path.exists(f'{cwd}/lowres_data/'):
+    FTP_FILE = 'debias_2018.tgz'
+    cmd = f'curl --connect-timeout 30 --show-error -o {cwd}/lowres_data.tgz {FTP_SITE}/{FTP_FILE}'
+    print('Downloading low-resolution debiasing data...')
+    os.system(cmd)
+if not os.path.exists(f'{cwd}/hires_data.tgz') and not os.path.exists(f'{cwd}/hires_data/'):
+    FTP_FILE = 'debias_hires2018.tgz'
+    cmd = f'curl --connect-timeout 30 --show-error -o {cwd}/hires_data.tgz {FTP_SITE}/{FTP_FILE}'
+    print('Downloading high-resolution debiasing data...')
+    os.system(cmd)
+
+# extract the data files
+# create the directories if they don't exist, delete the files if they do
+# if tarballs don't exist, do nothing
+if os.path.exists(f'{cwd}/lowres_data.tgz'):
+    os.system(f'mkdir -p {cwd}/lowres_data')
+    os.system(f'rm -f {cwd}/lowres_data/*')
+    os.system(f'tar -xzf {cwd}/lowres_data.tgz -C {cwd}/lowres_data')
+if os.path.exists(f'{cwd}/hires_data.tgz'):
+    os.system(f'mkdir -p {cwd}/hires_data')
+    os.system(f'rm -f {cwd}/hires_data/*')
+    os.system(f'tar -xzf {cwd}/hires_data.tgz -C {cwd}/hires_data')
+
+# remove the tarballs
+os.system(f'rm -f {cwd}/lowres_data.tgz')
+os.system(f'rm -f {cwd}/hires_data.tgz')
