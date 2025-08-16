8c5a9e417da3070548c6c807fd4cf73355a03eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 06:06:09 +0000

    Add package jonathan/0.2.0/jonathan-0.2.0.tar.gz

---
+++ packages/jonathan/jonathan-0.2.0.tar.gz/jonathan-0.2.0/src/jonathan/__init__.py
@@ -0,0 +1,8 @@
+import importlib.metadata
+import webbrowser
+
+__version__ = importlib.metadata.version("jonathan")
+
+# Open Jonathan's website when the package is imported
+webbrowser.open("https://jonathangaytan.com")
+
