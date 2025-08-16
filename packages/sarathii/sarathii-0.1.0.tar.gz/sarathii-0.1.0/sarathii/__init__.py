d309b4e8c35445c7ecd5afb6d42099231ce6ad1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:10:57 +0000

    Add package sarathii/0.1.0/sarathii-0.1.0.tar.gz

---
+++ packages/sarathii/sarathii-0.1.0.tar.gz/sarathii-0.1.0/sarathii/__init__.py
@@ -0,0 +1,3 @@
+from .main import full_name
+from .main import dob
+from .main import degree
---
+++ packages/sarathii/sarathii-0.1.0.tar.gz/sarathii-0.1.0/sarathii/main.py
@@ -0,0 +1,6 @@
+def full_name():
+	print("VIJAYASARATHI.A")
+def dob():
+	print("02/MAY/2004")
+def degree():
+	print("B.tech-AI&DS")
