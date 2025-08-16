dc8a86ac256cf0c4ab40c786e8abfa73a82a9a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:18 +0000

    Add package f3dasm/2.0.2/f3dasm-2.0.2-py3-none-any.whl

---
+++ packages/f3dasm/f3dasm-2.0.2-py3-none-any.whl/f3dasm/_src/optimization/__init__.py
@@ -0,0 +1,41 @@
+"""
+Module for optimization
+"""
+#                                                                       Modules
+# =============================================================================
+
+# Standard
+from functools import partial
+
+from ._imports import try_import
+
+# Local
+from .errors import faulty_optimizer
+from .optimizer_factory import create_optimizer
+
+with try_import() as _optuna_imports:
+    from .optuna_implementations import tpesampler
+
+with try_import() as _scipy_imports:
+    from .scipy_implementations import cg, lbfgsb, nelder_mead
+
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+
+
+# =============================================================================
+
+__all__ = [
+    'create_optimizer',
+    "OptunaOptimizer",
+    'tpesampler',
+    'cg',
+    'lbfgsb',
+    'nelder_mead',
+]
