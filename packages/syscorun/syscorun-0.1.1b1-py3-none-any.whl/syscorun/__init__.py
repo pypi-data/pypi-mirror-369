d0640ffe9bf0c8e0b7224f22b41be49ba32e7a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-16 04:08:45 +0000

    Add package syscorun/0.1.1b1/syscorun-0.1.1b1-py3-none-any.whl

---
+++ packages/syscorun/syscorun-0.1.1b1-py3-none-any.whl/syscorun/__init__.py
@@ -0,0 +1,34 @@
+import sysco
+from syscovis import plotter
+from sysco import BaseController as Controller
+from loguru import logger
+
+# from rich.markdown import Markdown
+from rich.console import Console
+
+console = Console()
+
+logger.debug(f"Importing {__name__}")
+
+
+def main() -> None:
+    console.print(
+        # Markdown(
+        f"# {sysco.hello_from_rust()}   ",
+        style="bold white on cyan",
+        # style="bold cyan on white",
+        justify="center",
+        # )
+    )
+    controller = Controller()
+    try:
+        controller.get_input()
+    except NotImplementedError as e:
+        logger.warning(f"NotImplementedError for {controller.__repr__()}")
+        logger.warning(e)
+        console.print(
+            "You would have been better off implementing something than sleeping in class...",
+            style="magenta",
+        )
+        pass
+    plotter.plot()
